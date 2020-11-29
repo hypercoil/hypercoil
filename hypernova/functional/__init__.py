901a6c0fde8d18093b6943ef608231d5e52b2b2c
rciric <rastko@stanford.edu>
2020-11-29 00:19:31 -0800

    (MAINT) add init files

---
+++ hypernova/functional/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Functional interfaces
+"""
+from .cov import (
+    cov, corr, partialcorr, pairedcov, conditionalcov, conditionalcorr,
+    precision, invert_spd, covariance, correlation, corrcoef, pcorr, ccov,
+    ccorr
+)
