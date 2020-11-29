04398c0e177e69c624f9edd0d658385c1892442c
rciric <rastko@stanford.edu>
2020-11-29 00:23:08 -0800

    (TEST) validate Schur complement equivalence with confound regression

--- hypernova/functional/test/test_cov.py
+++ hypernova/functional/test/test_cov.py
@@ -103,6 +103,28 @@ def test_pcorr():
 
 
 def test_ccov():
+    """
+    Verify equivalence of the Schur complement approach and fit-based confound
+    regression.
+    """
     out = conditionalcov(Xt, Yt).numpy()
     ref = torch.pinverse(precision(torch.cat([Xt ,Yt], -2))[:7, :7]).numpy()
+    assert testf(out, ref)
+    Y_intercept = np.concatenate([Y, np.ones((1, 100))])
+    ref = np.cov(
+        X - np.linalg.lstsq(Y_intercept.T, X.T, rcond=None)[0].T
+        @ Y_intercept)
+    assert testf(out, ref)
+
+
+def test_ccorr():
+    """
+    Verify equivalence of the Schur complement approach and fit-based confound
+    regression.
+    """
+    Y_intercept = np.concatenate([Y, np.ones((1, 100))])
+    out = conditionalcorr(Xt, Yt).numpy()
+    ref = np.corrcoef(
+        X - np.linalg.lstsq(Y_intercept.T, X.T, rcond=None)[0].T
+        @ Y_intercept)
     assert testf(out, ref)
