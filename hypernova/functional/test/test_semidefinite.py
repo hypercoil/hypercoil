e44f88c28d99e2707160cac3808f4368a2bd064a
rciric <rastko@stanford.edu>
2020-11-30 01:47:34 -0800

    (TEST) unit tests for projection between semidefinite cone and tangent spaces

---
+++ hypernova/functional/test/test_semidefinite.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Unit tests for operations in the positive semidefinite cone
+"""
+import numpy as np
+import torch
+from nilearn.connectome.connectivity_matrices import (
+    _form_symmetric, _map_eigenvalues
+)
+from hypernova import (
+    tangent_project_spd, cone_project_spd
+)
+
+
+tol = 1e-6
+rtol = 1e-5
+testf = lambda out, ref: np.allclose(out, ref, atol=tol, rtol=rtol)
+
+
+A = np.random.rand(10, 10)
+AM = np.random.rand(200, 10, 10)
+A = A @ A.T
+R = AM.mean(0)
+AM = AM @ np.swapaxes(AM, -1, -2)
+At = torch.Tensor(A)
+AMt = torch.Tensor(AM)
+Rt = torch.Tensor(R)
+
+
+def nilearn_tangent_project(input, ref):
+    """nilearn's method for projecting into tangent space"""
+    vals, vecs = np.linalg.eigh(ref)
+    inv_sqrt = _form_symmetric(np.sqrt, 1. / vals, vecs)
+    whitened_matrices = [inv_sqrt.dot(matrix).dot(inv_sqrt)
+                         for matrix in input]
+    return np.stack([_map_eigenvalues(np.log, w_mat)
+                    for w_mat in whitened_matrices])
+
+
+def nilearn_cone_project(input, ref):
+    """nilearn's method for projecting into PSD cone"""
+    rvals, rvecs = np.linalg.eigh(ref)
+    ivals, ivecs = np.linalg.eigh(input)
+    sqrt = _form_symmetric(np.sqrt, rvals, rvecs)
+    # Move along the geodesic
+    return sqrt.dot(_form_symmetric(np.exp, ivals, ivecs)).dot(sqrt)
+
+
+def test_tangent_project():
+    out = tangent_project_spd(At, Rt).numpy()
+    ref = nilearn_tangent_project([A], R)
+    assert testf(out, ref)
+    out = tangent_project_spd(AMt, Rt).numpy()
+    ref = nilearn_tangent_project(AM, R)
+    # Note that this is a very weak condition! This would likely
+    # experience major improvement if pytorch develops a proper
+    # logm function.
+    assert np.allclose(out, ref, atol=1e-1, rtol=1e-1)
+
+
+def test_cone_project():
+    V = nilearn_tangent_project([A], R).squeeze()
+    Vt = torch.Tensor(V)
+    out = cone_project_spd(Vt, Rt).numpy()
+    ref = nilearn_cone_project(V, R)
+    assert testf(out, ref)
+    out = cone_project_spd(AMt, Rt).numpy()
+    ref = np.stack([nilearn_cone_project(AMi, R) for AMi in AM])
+    assert testf(out, ref)
+
+
+def test_proper_inverse():
+    Vt = tangent_project_spd(AMt, Rt, recondition=1e-4)
+    AM_rec = cone_project_spd(Vt, Rt, recondition=1e-4).numpy()
+    assert testf(out, ref)
