1dea606d5c8d22a4a92caa8e0dc5dc4bc3574f4d
rciric <rastko@stanford.edu>
2020-11-30 01:01:55 -0800

    (ENH) projection from tangent subspace into positive semidefinite cone

--- hypernova/functional/semidefinite.py
+++ hypernova/functional/semidefinite.py
@@ -56,9 +56,71 @@ def tangent_project_spd(input, reference, recondition=0):
     -------
     output : Tensor
         Batch of matrices transformed via projection into the tangent subspace.
+
+    See also
+    --------
+    cone_project_spd: The inverse projection, into the semidefinite cone.
     """
     ref_sri = symxfm(reference, torch.rsqrt, psi=recondition)
     return symlog(ref_sri @ input @ ref_sri, recondition)
+
+
+def cone_project_spd(input, reference, recondition=0):
+    """
+    Project a batch of symmetric matrices from a tangent subspace into the
+    positive semidefinite cone.
+
+    Given a tangency point :math:`\Omega`, each input :math:`\Theta` is
+    projected as:
+
+    :math:`\bar{Theta} = \Omega^{1/2} \exp_M \Theta \Omega^{1/2}`
+
+    where :math:`\Omega^{1/2}` denotes the matrix square root of :math:`\Omega`
+    and :math:`exp_M` denotes the matrix-valued exponential.
+
+    Dimension
+    ---------
+    - Input: :math:`(N, *, D, D)`
+      N denotes batch size, `*` denotes any number of intervening dimensions,
+      D denotes matrix row and column dimension
+    - Reference: :math:`(*, D, D)`
+    - Output: :math:`(N, *, D, D)`
+
+    Parameters
+    ----------
+    input : Tensor
+        Batch of matrices to project into the positive semidefinite cone.
+    reference : Tensor
+        Point of tangency. This is an element of the positive semidefinite
+        cone at which the projection occurs. It should be representative of
+        the sample being projected (for instance, some form of mean).
+    recondition : float in [0, 1]
+        Conditioning factor to promote positive definiteness. If this is in
+        (0, 1], the original input will be replaced with a convex combination
+        of the input and an identity matrix.
+
+        :math:`\hat{X} = (1 - \psi) X + \psi I`
+
+        A suitable value can be used to ensure that all eigenvalues are
+        positive and therefore guarantee that the matrix is in the domain.
+
+    Returns
+    -------
+    output : Tensor
+        Batch of matrices transformed via projection into the positive
+        semidefinite cone.
+
+    See also
+    --------
+    tangent_project_spd: The inverse projection, into a tangent subspace.
+    """
+    ref_sr = symsqrt(reference, recondition)
+    # Note that we recondition the exponential as well to ensure that this is
+    # a well-formed inverse of `tangent_project_spd`.
+    if recondition > 0:
+        recondition * input + (1 - recondition) * torch.eye(input.size(-1))
+    # Note that we must use the much slower torch.matrix_exp for stability.
+    return ref_sr @ torch.matrix_exp(input) @ ref_sr
 
 
 def mean_euc_spd(input):
