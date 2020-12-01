eb2e270870cfd8259215577e3f9e2523e1eca945
rciric <rastko@stanford.edu>
2020-12-01 02:10:30 -0800

    (DOC) add documentation for several means in the semidefinite cone

--- hypernova/functional/semidefinite.py
+++ hypernova/functional/semidefinite.py
@@ -123,18 +123,145 @@ def cone_project_spd(input, reference, recondition=0):
 
 
 def mean_euc_spd(input):
+    """
+    Batch-wise Euclidean mean of tensors in the positive semidefinite cone.
+
+    This is the familiar arithmetic mean:
+
+    :math:`\frac{1}{N}\sum_{i=1}^N X_{i}`
+
+    Dimension
+    ---------
+    - Input: :math:`(N, *, D, D)`
+      N denotes batch size, `*` denotes any number of intervening dimensions,
+      D denotes matrix row and column dimension
+    - Output: :math:`(*, D, D)`
+
+    Parameters
+    ----------
+    input : Tensor
+        Batch of matrices over which the Euclidean mean is to be computed.
+
+    Returns
+    -------
+    output : Tensor
+        Euclidean mean of the input batch.
+    """
     return input.mean(0)
 
 
 def mean_harm_spd(input):
+    """
+    Batch-wise harmonic mean of tensors in the positive semidefinite cone.
+
+    The harmonic mean is computed as the matrix inverse of the Euclidean mean
+    of matrix inverses:
+
+    :math:`\left(\frac{1}{N}\sum_{i=1}^N X_{i}^{-1}\right)^{-1}`
+
+    Dimension
+    ---------
+    - Input: :math:`(N, *, D, D)`
+      N denotes batch size, `*` denotes any number of intervening dimensions,
+      D denotes matrix row and column dimension
+    - Output: :math:`(*, D, D)`
+
+    Parameters
+    ----------
+    input : Tensor
+        Batch of matrices over which the harmonic mean is to be computed.
+
+    Returns
+    -------
+    output : Tensor
+        Harmonic mean of the input batch.
+    """
     return invert_spd(invert_spd(input).mean(0))
 
 
 def mean_logeuc_spd(input):
+    """
+    Batch-wise log-Euclidean mean of tensors in the positive semidefinite cone.
+
+    The log-Euclidean mean is computed as the matrix exponential of the mean of
+    matrix logarithms.
+
+    :math:`\exp_M \left(\frac{1}{N}\sum_{i=1}^N \log_M X_{i}\right)`
+
+    Dimension
+    ---------
+    - Input: :math:`(N, *, D, D)`
+      N denotes batch size, `*` denotes any number of intervening dimensions,
+      D denotes matrix row and column dimension
+    - Output: :math:`(*, D, D)`
+
+    Parameters
+    ----------
+    input : Tensor
+        Batch of matrices over which the log-Euclidean mean is to be computed.
+
+    Returns
+    -------
+    output : Tensor
+        Log-Euclidean mean of the input batch.
+    """
     return symexp(symlog(input).mean(0))
 
 
 def mean_geom_spd(input, recondition=0, eps=1e-6, max_iter=10):
+    """
+    Batch-wise geometric mean of tensors in the positive semidefinite cone.
+
+    The geometric mean is computed via gradient descent along the geodesic on
+    the manifold. In brief:
+
+    Initialisation :
+     - The estimate of the mean is initialised to the Euclidean mean.
+    Iteration :
+     - Using the working estimate of the mean as the point of tangency, the
+       tensors are projected into a tangent space.
+     - The arithmetic mean of the tensors is computed in tangent space.
+     - This mean is projected back into the positive semidefinite cone using
+       the same point of tangency. It now becomes a new working estimate of the
+       mean and thus a new point of tangency.
+    Termination / convergence :
+     - The algorithm terminates either when the Frobenius norm of the
+       difference between the new estimate and the previous estimate is less
+       than a specified threshold, or when a maximum number of iterations has
+       been attained.
+
+    Dimension
+    ---------
+    - Input: :math:`(N, *, D, D)`
+      N denotes batch size, `*` denotes any number of intervening dimensions,
+      D denotes matrix row and column dimension
+    - Output: :math:`(*, D, D)`
+
+    Parameters
+    ----------
+    input : Tensor
+        Batch of matrices over which the geometric mean is to be computed.
+    recondition : float in [0, 1]
+        Conditioning factor to promote positive definiteness. If this is in
+        (0, 1], the original input will be replaced with a convex combination
+        of the input and an identity matrix.
+
+        :math:`\hat{X} = (1 - \psi) X + \psi I`
+
+        A suitable value can be used to ensure that all eigenvalues are
+        positive and therefore guarantee that the matrix is in the domain of
+        projection operations.
+    eps : float
+        The minimum value of the Frobenius norm required for convergence.
+    max_iter : nonnegative int
+        The maximum number of iterations of gradient descent to run before
+        termination.
+
+    Returns
+    -------
+    output : Tensor
+        Geometric mean of the input batch.
+    """
     ref = mean_euc_spd(input)
     for i in range(max_iter):
         tan = tangent_project_spd(input, ref, recondition)
