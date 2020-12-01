d029366bf32b1156836b706dce685f65ea72bdd2
rciric <rastko@stanford.edu>
2020-12-01 05:42:17 -0800

    (ENH) multichannel polynomial basis for convolution

---
+++ hypernova/functional/polynomial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Polynomial convolution
+~~~~~~~~~~~~~~~~~~~~~~
+Functions supporting polynomial convolution of time series.
+"""
+import torch
+
+
+def polychan(X, degree=2, include_const=False):
+    if X.dim() > 2:
+        pass
+    elif X.dim() == 2:
+        X = X.view(1, *X.size())
+    elif X.dim() == 1:
+        X = X.view(1, -1)
+    stack = [X]
+    for _ in range(degree - 1):
+        stack += [stack[-1] * X]
+    if include_const:
+        stack = [torch.ones_like(X)] + stack
+    return torch.stack(stack, 1)
