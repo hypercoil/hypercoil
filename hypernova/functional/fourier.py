f480ec39f5ef2b414857f719c02b2ca7729d2249
rciric <rastko@stanford.edu>
2020-12-03 02:00:40 -0800

    (DOC) frequency-domain filter documentation

--- hypernova/functional/fourier.py
+++ hypernova/functional/fourier.py
@@ -10,6 +10,36 @@ import torch
 
 
 def product_filter(X, weight, **params):
+    """
+    Convolve a multivariate signal via multiplication in the frequency domain.
+
+    Dimension
+    ---------
+    - Input: :math:`(N, *, obs)`
+      N denotes batch size, `*` denotes any number of intervening dimensions,
+      obs denotes number of observations
+    - Weight: :math:`(*, \left\lfloor \frac{obs}{2} \right\rfloor + 1)`
+    - Output: :math:`(N, *, 2 \left\lfloor \frac{obs}{2} \right\rfloor )`
+
+    Parameters
+    ----------
+    X : Tensor
+        The (potentially multivariate) signal to be filtered. The final axis
+        should correspond to the time domain of each signal or its analogue.
+    weight : Tensor
+        The filter gain at each frequency bin in the spectrum, ordered low to
+        high along the last axis. Dimensions before the last can be used to
+        apply different filters to different variables in the input signal
+        according to tensor broadcasting rules.
+    Any additional parameters provided will be passed to `torch.fft.rfft` and
+        `torch.fft.irfft`.
+
+    Returns
+    -------
+    out : Tensor
+        Original time series filtered via multiplication in the frequency
+        domain.
+    """
     n = X.size(-1)
     Xf = torch.fft.rfft(X, n=n, **params)
     Xf_filt = weight * Xf
