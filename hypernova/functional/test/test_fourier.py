a24b1b976137327b5be25f13baef10ee5e56af5f
rciric <rastko@stanford.edu>
2020-12-03 02:02:39 -0800

    (TEST) filter conformant with scipy behaviour

--- hypernova/functional/test/test_fourier.py
+++ hypernova/functional/test/test_fourier.py
@@ -2,19 +2,27 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
-Unit tests for polynomial convolution
+Unit tests for Fourier-domain filtering
 """
 import torch
+import numpy as np
+from scipy.fft import rfft, irfft
 from hypernova.fourier import (
-    productfilter
+    product_filter
 )
 
 
-testf = torch.allclose
+tol = 1e-7
+testf = lambda out, ref: np.allclose(out, ref, atol=tol)
 
 
 N = 100
-X = torch.rand(7, N)
+X = np.random.rand(7, N)
+Xt = torch.Tensor(X)
+
+
+def scipy_product_filter(X, weight):
+    return irfft(weight * rfft(X))
 
 
 def bandpass_filter():
@@ -22,3 +30,11 @@ def bandpass_filter():
     weight[:10] = 0
     weight[20:] = 0
     return weight
+
+
+def test_bandpass():
+    wt = bandpass_filter()
+    w = wt.numpy()
+    out = product_filter(Xt, wt).numpy()
+    ref = scipy_product_filter(X, w)
+    assert testf(out, ref)
