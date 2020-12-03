b6f30a7d05db0060cc4e0b1a738c25224528f3d5
rciric <rastko@stanford.edu>
2020-12-03 01:58:53 -0800

    (ENH) rudimentary Fourier-domain product for filtering data

---
+++ hypernova/functional/fourier.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Fourier-domain filter
+~~~~~~~~~~~~~~~~~~~~~
+Convolve the signal via multiplication in the Fourier domain.
+"""
+import torch
+
+
+def product_filter(X, weight, **params):
+    n = X.size(-1)
+    Xf = torch.fft.rfft(X, n=n, **params)
+    Xf_filt = weight * Xf
+    return torch.fft.irfft(Xf_filt, n=n, **params)
