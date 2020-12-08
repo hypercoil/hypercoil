681ab13d4d6fc57f36cf2f9b0539591adb9a51f5
rciric <rastko@stanford.edu>
2020-12-07 20:41:32 -0800

    (TEST) ensure that relaxed modularity matches standard modularity

--- hypernova/functional/test/test_graph.py
+++ hypernova/functional/test/test_graph.py
@@ -7,7 +7,8 @@ Unit tests for graph and network measures
 import numpy as np
 import torch
 from hypernova.functional import (
-    modularity_matrix
+    modularity_matrix,
+    relaxed_modularity
 )
 from communities.utilities import (
     modularity_matrix as modularity_matrix_ref,
@@ -18,12 +19,23 @@ from communities.utilities import (
 tol = 5e-7
 testf = lambda out, ref: np.allclose(out, ref, atol=tol)
 
-X = np.random.rand(3, 7, 7)
+X = np.random.rand(3, 20, 20)
 X += X.swapaxes(-1, -2)
+aff = np.random.randint(0, 4, 20)
+comms = [np.where(aff==c)[0] for c in np.unique(aff)]
+C = np.eye(4)[aff]
 Xt = torch.Tensor(X)
+Ct = torch.Tensor(C)
 
 
 def test_modularity_matrix():
     out = modularity_matrix(Xt, normalise=True)
     ref = np.stack([modularity_matrix_ref(x) for x in X])
+    assert testf(out, ref)
+
+
+def test_modularity():
+    out = relaxed_modularity(Xt, Ct, exclude_diag=True) / 2
+    ref = np.stack(
+        [modularity_ref(modularity_matrix_ref(x), comms) for x in X])
     assert testf(out, ref)
