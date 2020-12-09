88fb260b9651a6ca0371c7be0efc137fc4e853cd
rciric <rastko@stanford.edu>
2020-12-09 10:48:16 -0800

    (ENH) double exponential interpolable initialisation

---
+++ hypernova/init/laplace.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Laplace initialisation
+~~~~~~~~~~~~~~~~~~~~~~
+Initialise parameters to match a double exponential function.
+"""
+import torch
+
+
+def laplace_init_(tensor, loc=None, width=None, norm=None):
+    loc = loc or [(x - 1) / 2 for x in tensor.size()]
+    width = width or [1 for _ in range(X.dim())]
+    width = torch.Tensor(width)
+    axes = []
+    for ax, l, w in zip(tensor.size(), loc, width):
+        new_ax = torch.arange(-l, -l + ax)
+        new_ax = torch.exp(-torch.abs(new_ax) / w)
+        axes += [new_ax]
+    val = axes[0]
+    shape = [-1]
+    for ax in axes[1:]:
+        shape = [1] + shape
+        new = ax.view(shape)
+        val = val[..., None] * new
+    if norm == 'max':
+        val /= val.max()
+    elif norm == 'sum':
+        val /= val.sum()
+    val.type(tensor.dtype)
+    tensor[:] = val
