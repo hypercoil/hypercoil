76022301ccca94382165dc8be17f14a220f4536a
rciric <rastko@stanford.edu>
2020-12-09 10:51:08 -0800

    (DOC) document double exponential interpolable initialisation

--- hypernova/init/laplace.py
+++ hypernova/init/laplace.py
@@ -10,6 +10,35 @@ import torch
 
 
 def laplace_init_(tensor, loc=None, width=None, norm=None):
+    """
+    Laplace initialisation.
+
+    Initialise a tensor such that its values are interpolated by a
+    multidimensional double exponential function, :math:`e^{-|x|}`.
+
+    Parameters
+    ----------
+    tensor : Tensor
+        Tensor to initialise in-place.
+    loc : iterable or None (default None)
+        Origin point of the double exponential, in array coordinates. If None,
+        this will be set to the centre of the array.
+    width : iterable or None (default None)
+        Decay rate of the double exponential along each array axis. If None,
+        this will be set to 1 isotropically. If this is very large, the result
+        will approximate a delta function at the specified `loc`.
+    norm : 'max', 'sum', or None (default None)
+        Normalisation to apply to the output.
+        - 'max' divides the output by its maximum value such that the largest
+          value in the initialised tensor is exactly 1.
+        - 'sum' divides the output by its sum such that all entries in the
+          initialised tensor sum to 1.
+        - None indicates that the output should not be normalised.
+
+    Returns
+    -------
+    None. The input tensor is initialised in-place.
+    """
     loc = loc or [(x - 1) / 2 for x in tensor.size()]
     width = width or [1 for _ in range(X.dim())]
     width = torch.Tensor(width)
