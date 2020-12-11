df86849f1e325ca0b8761680f56f597d68e85ec5
rciric <rastko@stanford.edu>
2020-12-10 19:49:15 -0800

    (DOC) documentation for zero-phase frequency-domain filter

--- hypernova/functional/fourier.py
+++ hypernova/functional/fourier.py
@@ -47,5 +47,45 @@ def product_filter(X, weight, **params):
 
 
 def product_filtfilt(X, weight, **params):
+    """
+    Perform zero-phase digital filtering of a signal via multiplication in the
+    frequency domain.
+
+    This function operates by first filtering the data and then filtering a
+    time-reversed copy of the filtered data again. Note that this doubles the
+    effective filter order.
+
+    If the `weight` argument is strictly real, then the filter has no phase
+    delay component and it could make sense to simply use `product_filter`
+    depending on the context. (The gradient could still have an imaginary
+    component.)
+
+    Dimension
+    ---------
+    - Input: :math:`(N, *, obs)`
+      N denotes batch size, `*` denotes any number of intervening dimensions,
+      obs denotes number of observations
+    - Weight: :math:`(*, \left\lfloor \frac{obs}{2} \right\rfloor + 1)`
+    - Output: :math:`(N, *, 2 \left\lfloor \frac{obs}{2} \right\rfloor )`
+
+    Parameters
+    ----------
+    X : Tensor
+        The (potentially multivariate) signal to be filtered. The final axis
+        should correspond to the time domain of each signal or its analogue.
+    weight : Tensor
+        The filter gain at each frequency bin in the spectrum, ordered low to
+        high along the last axis. Dimensions before the last can be used to
+        apply different filters to different variables in the input signal
+        according to tensor broadcasting rules.
+    Any additional parameters provided will be passed to `torch.fft.rfft` and
+        `torch.fft.irfft`.
+
+    Returns
+    -------
+    out : Tensor
+        Original time series filtered forward and backward via multiplication
+        in the frequency domain.
+    """
     X_filt = product_filter(X, weight, **params)
     return product_filter(X_filt.flip(-1), weight, **params)
