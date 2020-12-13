51b171391b59f3e548b11a69ec2c0ec52c6a17e0
rciric <rastko@stanford.edu>
2020-12-13 12:06:10 -0800

    (FIX) cover additional spec cases in frequency filtering module

--- hypernova/nn/freqfilter.py
+++ hypernova/nn/freqfilter.py
@@ -9,6 +9,7 @@ Modules supporting filtering/convolution as a product in the frequency domain.
 import math
 import torch
 from torch.nn import Module, Parameter, init
+from itertools import chain
 from ..functional import product_filtfilt
 from ..functional.activation import amplitude_tanh
 from ..init.iirfilter import iirfilter_init_, clamp_init_
@@ -36,7 +37,10 @@ class FrequencyDomainFilter(Module):
 
     def reset_parameters(self):
         iirfilter_init_(self.weight, self.filter_specs, domain=self.domain)
-        clamp_init_(self.clamp_points, self.clamp_values, self.filter_specs)
+        if self.clamp_points is not None:
+            clamp_init_(self.clamp_points,
+                        self.clamp_values,
+                        self.filter_specs)
 
     def _set_dimension(self, dim, time_dim):
         if dim is None:
@@ -44,7 +48,7 @@ class FrequencyDomainFilter(Module):
                 raise ValueError('You must specify the dimension in either '
                                  'the frequency or time domain')
             else:
-                dim = (time_dim + 1) // 2
+                dim = time_dim // 2 + 1
         return dim
 
     def _set_activation(self):
@@ -54,8 +58,9 @@ class FrequencyDomainFilter(Module):
             return amplitude_tanh
 
     def _check_clamp(self):
-        clamps = [[len(f.keys()) for f in spec.clamps]
-                  for spec in self.filter_specs]
+        clamps = list(chain.from_iterable(
+            [[len(f.keys()) for f in spec.clamps]
+             for spec in self.filter_specs]))
         n_clamps = int(torch.Tensor(clamps).sum().item())
         if n_clamps <= 0:
             self.register_parameter('clamp_points', None)
@@ -79,14 +84,17 @@ class FrequencyDomainFilter(Module):
     def constrained_weight(self):
         return self._apply_clamps(self.activation(self.weight))
 
-    def forward(self, input):
-        return self.filter(input, self.constrained_weight)
-
     def __repr__(self):
-        s = f'{self.__class__.__name__}(domain={self.domain}, [\n'
-        for spec in self.filter_specs:
-            s += '    '
-            s += spec.__repr__()
-            s += '\n'
-        s += '])'
+        s = f'{self.__class__.__name__}(domain={self.domain}, filters=[\n'
+        s += ',\n'.join([f'    {spec.__repr__()}'
+                        for spec in self.filter_specs])
+        s += '\n])'
         return s
+
+    def forward(self, input):
+        if input.size(0) > 1 and input.dim() > 1:
+            input.unsqueeze_(-3)
+            weight = self.constrained_weight.unsqueeze(-2)
+        else:
+            weight = self.constrained_weight
+        return self.filter(input, weight)
