(TEST)(FIX) covariance module tests and bug fixes

hypernova/nn/cov.py
@@ -156,6 +156,10 @@ class _ToeplitzWeightedCov(_Cov):
             bias=bias, ddof=ddof, l2=l2, noise=noise, dropout=dropout,
             domain=domain, out_channels=out_channels
         )
+        if self.max_lag is not None:
+            self.mask = Parameter(torch.Tensor(
+                self.dim, self.dim
+            ).bool(), requires_grad=False)
         self.prepreweight_c = Parameter(torch.Tensor(
             self.max_lag + 1, self.out_channels
         ))
@@ -165,14 +169,27 @@ class _ToeplitzWeightedCov(_Cov):
         self.reset_parameters()
 
     def reset_parameters(self):
-        laplace_init_(self.prepreweight_c, loc=(0, 0), excl_axis=[1], var=0)
-        laplace_init_(self.prepreweight_r, loc=(0, 0), excl_axis=[1], var=0)
+        toeplitz_init_(
+            self.mask,
+            torch.Tensor([1 for _ in range(self.max_lag + 1)])
+        )
+        laplace_init_(self.prepreweight_c, loc=(0, 0), excl_axis=[1],
+                      var=0, domain=self.domain)
+        laplace_init_(self.prepreweight_r, loc=(0, 0), excl_axis=[1],
+                      var=0, domain=self.domain)
 
     @property
     def preweight(self):
         return toeplitz(c=self.prepreweight_c,
                         r=self.prepreweight_r,
-                        dim=(self.dim, self.dim))
+                        dim=(self.dim, self.dim),
+                        fill_value=self.domain.preimage(torch.zeros(1)).item())
+
+    @property
+    def postweight(self):
+        if self.mask is not None:
+            return self.inject_noise(self.weight) * self.mask
+        return self.inject_noise(self.weight)
 
 
 class _UnweightedCov(_Cov):

hypernova/nn/test/test_covnn.py
@@ -11,8 +11,9 @@ from hypernova.nn import (
     BinaryCovariance, BinaryCovarianceTW, BinaryCovarianceUW
 )
 from hypernova.functional.noise import (
-    DiagonalNoiseSource, DiagonalDropoutSource
+    DiagonalNoiseSource, DiagonalDropoutSource, BandDropoutSource
 )
+from hypernova.functional.domain import Logit
 
 
 testf = lambda x, y: np.allclose(x.detach(), y, atol=1e-5)
@@ -20,6 +21,9 @@ testf = lambda x, y: np.allclose(x.detach(), y, atol=1e-5)
 
 X = torch.rand(4, 13, 100)
 Y = torch.rand(4, 7, 100)
+dns = DiagonalNoiseSource()
+dds = DiagonalDropoutSource()
+bds = BandDropoutSource()
 
 
 def test_cov_uuw():
@@ -27,6 +31,10 @@ def test_cov_uuw():
     out = cov(X)
     ref = np.stack([np.corrcoef(x) for x in X])
     assert testf(out, ref)
+    cov = UnaryCovarianceUW(
+        100, estimator=hypernova.functional.pcorr,
+        out_channels=7, noise=dns) #, dropout=dds)
+    cov(X)
 
 
 def test_cov_utw():
@@ -34,6 +42,13 @@ def test_cov_utw():
     out = cov(X)
     ref = np.stack([np.corrcoef(x) for x in X])
     assert testf(out, ref)
+    cov = UnaryCovarianceTW(
+        100, estimator=hypernova.functional.pcorr, max_lag=3,
+        out_channels=7, noise=dns, dropout=bds, domain=Logit(4))
+    cov(X)
+    assert cov.prepreweight_c.size() == torch.Size([4, 7])
+    assert cov.weight[5, 15, 17] == cov.weight[5, 94, 96]
+    assert cov.postweight[3, 13, 17] == 0
 
 
 def test_cov_uw():