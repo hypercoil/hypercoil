be13f3a65deaf8e338b9512ac1324ec1c2c56c70
rciric <rastko@stanford.edu>
2020-12-17 10:09:25 -0800

    (TEST) unit tests for new noise sources

--- hypernova/functional/test/test_noise.py
+++ hypernova/functional/test/test_noise.py
@@ -7,7 +7,9 @@ Unit tests for noise sources
 import numpy as np
 import torch
 from hypernova.functional import (
-    SPSDNoiseSource
+    SPSDNoiseSource,
+    LowRankNoiseSource,
+    BandDropoutSource
 )
 
 
@@ -16,28 +18,28 @@ rtol = 1e-4
 testf = lambda out, ref: np.isclose(out, ref, atol=atol, rtol=rtol)
 
 
-def spsd_std_mean(dim=100, rank=None, std=0.05, iter=1000):
-    spsdns = SPSDNoiseSource(rank=rank, std=std)
+def lr_std_mean(dim=100, rank=None, std=0.05, iter=1000):
+    lrns = LowRankNoiseSource(rank=rank, std=std)
     return torch.Tensor(
-        [spsdns.sample([dim]).std() for _ in range(iter)
+        [lrns.sample([dim]).std() for _ in range(iter)
     ]).mean()
 
 
-def spsd_mean_mean(dim=100, rank=None, std=0.05, iter=1000):
-    spsdns = SPSDNoiseSource(rank=rank, std=std)
+def lr_mean_mean(dim=100, rank=None, std=0.05, iter=1000):
+    lrns = LowRankNoiseSource(rank=rank, std=std)
     return torch.Tensor(
-        [spsdns.sample([dim]).mean() for _ in range(iter)
+        [lrns.sample([dim]).mean() for _ in range(iter)
     ]).mean()
 
 
-def test_spsd_std():
-    out = spsd_std_mean()
+def test_lr_std():
+    out = lr_std_mean()
     ref = 0.05
     assert testf(out, ref)
-    out = spsd_std_mean(std=0.2)
+    out = lr_std_mean(std=0.2)
     ref = 0.2
     assert testf(out, ref)
-    out = spsd_std_mean(std=0.03, rank=7)
+    out = lr_std_mean(std=0.03, rank=7)
     ref = 0.03
     assert testf(out, ref)
 
@@ -48,13 +50,13 @@ def test_spsd_spsd():
     assert np.allclose(out, out.T, atol=1e-7)
     # ignore effectively-zero eigenvalues
     L = np.linalg.eigvals(out)
-    L[np.abs(L) < 1e-7] = 0
+    L[np.abs(L) < 1e-6] = 0
+    assert L.min() >= 0
     assert np.all(L >= 0)
 
-    spsdns = SPSDNoiseSource(rank=3)
-    out = spsdns.sample([100])
-    assert np.allclose(out, out.T, atol=1e-7)
-    # ignore effectively-zero eigenvalues
-    L = np.linalg.eigvals(out)
-    L[np.abs(L) < 1e-7] = 0
-    assert np.all(L >= 0)
+
+def test_band_correction():
+    bds = BandDropoutSource()
+    out = bds.sample([100]).sum()
+    ref = bds.bandmask.sum()
+    assert torch.abs((out - ref) / ref) < 0.15
