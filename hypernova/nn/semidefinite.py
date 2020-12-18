f6d074e77d1ea35f415f8f96e54f4515fd711f88
rciric <rastko@stanford.edu>
2020-12-18 00:47:24 -0800

    (PROTO) refactor projection prototype: batch and init, cone and tangent

--- hypernova/nn/semidefinite.py
+++ hypernova/nn/semidefinite.py
@@ -14,32 +14,46 @@ from ..functional import (
 )
 
 
-class TangentProject(Module):
-    def __init__(self, init_data, mean=None, recondition=0):
-        super(TangentProject, self).__init__()
+class _TangentProject(Module):
+    def __init__(self, mean=None, recondition=0):
+        super(_TangentProject, self).__init__()
+        self.dest = 'tangent'
         self.mean = mean or mean_euc_spd
         self.recondition = recondition
-        self.dest = 'tangent'
-        self.weight = Parameter(torch.Tensor(
-            *init_data.size()[1:]
-        ), requires_grad=False)
-
-        self.reset_parameters(init_data)
-
-    def reset_parameters(self, init_data):
-        rg = self.weight.requires_grad
-        self.weight.requires_grad = False
-        self.weight[:] = self.mean(init_data)
-        self.weight.requires_grad = rg
 
     def extra_repr(self):
-        s += f'init={self.mean.__name__}'
+        s = f'mean={self.mean.__name__}'
         if self.recondition != 0:
             s += f', psi={self.recondition}'
         return s
 
-    def forward(self, input, dest=self.dest):
+    def forward(self, input, dest=None):
+        dest = dest or self.dest
+        weight = self._weight(input)
         if dest == 'tangent':
-            return tangent_project_spd(input, self.weight, self.recondition)
+            return tangent_project_spd(input, weight, self.recondition)
         elif dest == 'cone':
-            return cone_project_spd(input, self.weight, self.recondition)
+            return cone_project_spd(input, weight, self.recondition)
+
+
+class TangentProject(_TangentProject):
+    def __init__(self, init_data, mean=None, recondition=0):
+        super(TangentProject, self).__init__(mean, recondition)
+        self.weight = Parameter(torch.Tensor(
+            *init_data.size()[1:]
+        ))
+        self.reset_parameters(init_data)
+
+    def _weight(self, input):
+        return self.weight
+
+    def reset_parameters(self, init_data):
+        rg = self.weight.requires_grad
+        self.weight.requires_grad = False
+        self.weight[:] = self.mean(init_data, recondition=self.recondition)
+        self.weight.requires_grad = rg
+
+
+class BatchTangentProject(_TangentProject):
+    def _weight(self, input):
+        return self.mean(input, recondition=self.recondition)
