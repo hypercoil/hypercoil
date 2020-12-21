23970f556c32b696d453c547973a01ef79cd4843
rciric <rastko@stanford.edu>
2020-12-21 12:34:27 -0800

    (REF) refactor and subclass transforms

--- hypernova/data/transforms.py
+++ hypernova/data/transforms.py
@@ -9,35 +9,37 @@ Dataset transformations for packaging neuroimaging data into Pytorch tensors.
 import torch
 import nibabel as nb
 import pandas as pd
+from abc import ABC, abstractmethod
 from bids.layout.models import BIDSFile
-
-
-class ReadNiftiTensor(object):
-    def __init__(self, dtype='torch.FloatTensor', nanfill=0):
-        self.dtype = dtype
-        self.nanfill = nanfill
-
-    def __call__(self, sample):
-        path = sample.path if isinstance(sample, BIDSFile) else sample
-        img = nb.load(path)
-        img = torch.Tensor(img.get_fdata()).type(self.dtype)
-        img[torch.isnan(img)] = self.nanfill
-        return img
-
-
-class ReadTableTensor(object):
-    def __init__(self, dtype='torch.FloatTensor', nanfill=0):
-        self.dtype = dtype
-        self.nanfill = nanfill
-
-    def __call__(self, sample):
-        path = sample.path if isinstance(sample, BIDSFile) else sample
-        data = pd.read_csv(path, sep='\t')
-        data = torch.Tensor(data.values).type(self.dtype)
-        data[torch.isnan(data)] = self.nanfill
-        return data
 
 
 class IdentityTransform(object):
     def __call__(self, sample):
         return sample
+
+
+class ReadNeuroTensor(ABC):
+    def __init__(self, dtype='torch.FloatTensor', nanfill=0):
+        self.dtype = dtype
+        self.nanfill = nanfill
+
+    def __call__(self, sample):
+        path = sample.path if isinstance(sample, BIDSFile) else sample
+        data = self.read(path)
+        data = torch.Tensor(data).type(self.dtype)
+        data[torch.isnan(data)] = self.nanfill
+        return data
+
+    @abstractmethod
+    def read(self, path):
+        raise NotImplementedError()
+
+
+class ReadNiftiTensor(ReadNeuroTensor):
+    def read(self, path):
+        return nb.load(path).get_fdata()
+
+
+class ReadTableTensor(ReadNeuroTensor):
+    def read(self, path):
+        return pd.read_csv(path, sep='\t').values
