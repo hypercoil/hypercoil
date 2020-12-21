d53c5ca30232a749d585d74a3cfd4b349db06986
rciric <rastko@stanford.edu>
2020-12-21 10:16:26 -0800

    (REF) BIDS interfaces as dataset methods

--- hypernova/data/dataref.py
+++ hypernova/data/dataref.py
@@ -6,22 +6,7 @@ Data references
 ~~~~~~~~~~~~~~~
 Interfaces between neuroimaging data and data loaders.
 """
-import bids
 from .transforms import ReadNiftiTensor, ReadTableTensor, IdentityTransform
-
-
-bids.config.set_option('extension_initial_dot', True)
-
-BIDS_SCOPE = 'derivatives'
-BIDS_DTYPE = 'func'
-
-BIDS_IMG_DESC = 'preproc'
-BIDS_IMG_SUFFIX = 'bold'
-BIDS_IMG_EXT = '.nii.gz'
-
-BIDS_CONF_DESC = 'confounds'
-BIDS_CONF_SUFFIX = 'timeseries'
-BIDS_CONF_EXT = '.tsv'
 
 
 class DataReference(object):
@@ -73,91 +58,3 @@ class FunctionalConnectivityDataReference(DataReference):
             s += f', task={self.task}'
         s += ')'
         return s
-
-
-def assemble_entities(layout, ignore=None):
-    ignore = ignore or {
-        'sub': [],
-        'ses': [],
-        'run': [],
-        'task': []
-    }
-    sub = layout.get_subjects()
-    ses = layout.get_sessions()
-    run = layout.get_runs()
-    task = layout.get_tasks()
-
-    sub = [i for i in sub if i not in ignore['sub']]
-    ses = [i for i in ses if i not in ignore['ses']]
-    run = [i for i in run if i not in ignore['run']]
-    task = [i for i in task if i not in ignore['task']]
-    return collate_product(sub, ses, run, task)
-
-
-def collate_product(sub, ses, run, task):
-    entities = []
-    prod_gen = []
-    if sub:
-        entities += ['subject']
-        prod_gen += [sub]
-    if ses:
-        entities += ['session']
-        prod_gen += [ses]
-    if run:
-        entities += ['run']
-        prod_gen += [run]
-    if task:
-        entities += ['task']
-        prod_gen += [task]
-    return entities, list(product(*prod_gen))
-
-
-def get_filters(entities, query):
-    filters = {}
-    for k, v in zip(entities, query):
-        filters[k] = v
-    return filters
-
-
-def query_and_reference_all(layout, entities, queries, space=None):
-    data_refs = []
-    for query in queries:
-        filters = get_filters(entities, query)
-        image = layout.get(
-            scope=BIDS_SCOPE,
-            datatype=BIDS_DTYPE,
-            desc=BIDS_IMG_DESC,
-            space=space,
-            suffix=BIDS_IMG_SUFFIX,
-            extension=BIDS_IMG_EXT,
-            **filters)
-        confounds = layout.get(
-            scope=BIDS_SCOPE,
-            datatype=BIDS_DTYPE,
-            desc=BIDS_CONF_DESC,
-            suffix=BIDS_CONF_SUFFIX,
-            extension=BIDS_CONF_EXT,
-            **filters)
-        if not image or not confounds:
-            continue
-        ref = FunctionalConnectivityDataReference(
-            data=image[0],
-            confounds=confounds[0],
-            label=int(filters['subject']),
-            **filters
-        )
-        data_refs += [ref]
-    return data_refs
-
-
-def fmriprep_references(
-    fmriprep_dir,
-    space=None,
-    additional_tables=None,
-    ignore=None):
-    layout = bids.BIDSLayout(
-        fmriprep_dir,
-        derivatives=[fmriprep_dir],
-        validate=False)
-    entities, queries = assemble_entities(layout, ignore)
-    return query_and_reference_all(layout, entities, queries, space)
