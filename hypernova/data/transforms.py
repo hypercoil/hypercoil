df5b784b44a64ffdbb5b40124a76ecc15cfbaba3
rciric <rastko@stanford.edu>
2020-12-21 00:45:44 -0800

    (ENH) transforms for reading NIfTI and tabular data into torch tensors

---
+++ hypernova/data/transforms.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Neuroimaging data transforms
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Dataset transformations for packaging neuroimaging data into Pytorch tensors.
+"""
+import torch
+import nibabel as nb
+import pandas as pd
+from bids.layout.models import BIDSFile
+
+
+class ReadNiftiTensor(object):
+    def __init__(self, dtype='torch.FloatTensor', nanfill=0):
+        self.dtype = dtype
+        self.nanfill = nanfill
+
+    def __call__(self, sample):
+        path = sample.path if isinstance(sample, BIDSFile) else sample
+        img = nb.load(path)
+        img = torch.Tensor(img.get_fdata()).type(self.dtype)
+        img[torch.isnan(img)] = self.nanfill
+        return data
+
+
+class ReadTableTensor(object):
+    def __init__(self, dtype='torch.FloatTensor', nanfill=0):
+        self.dtype = dtype
+        self.nanfill = nanfill
+
+    def __call__(self, sample):
+        path = sample.path if isinstance(sample, BIDSFile) else sample
+        data = pd.read_csv(path, sep='\t')
+        data = torch.Tensor(data.values).type(self.dtype)
+        data[torch.isnan(data)] = self.nanfill
+        return data
