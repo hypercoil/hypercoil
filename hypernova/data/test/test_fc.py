37a38fdc2e8253a7f6578f1678def61f7bea2f3c
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-01-01 14:43:46 -0800

    (REF) begin refactoring tests as classes: FC data tests

--- hypernova/data/test/test_fc.py
+++ hypernova/data/test/test_fc.py
@@ -2,112 +2,115 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
-Transforms
-~~~~~~~~~~
-Transforms applied to columns of a pandas DataFrame.
+Functional connectivity
+~~~~~~~~~~~~~~~~~~~~~~~
+Tests for functional connectivity-specific data loading.
 """
+import pytest
 import hypernova
 import pandas as pd
 from pathlib import Path
 from pkg_resources import resource_filename as pkgrf
 
 
-confpath = pkgrf('hypernova', 'testdata/desc-confounds_timeseries.tsv')
-metapath = pkgrf('hypernova', 'testdata/desc-confounds_timeseries.json')
-metadata = hypernova.data.load_metadata(metapath)
-shfc = hypernova.data.fc.FCShorthand()
-df = pd.read_csv(confpath, sep='\t')
+class TestModelSpec:
+    filenames = {
+        'confdata': 'desc-confounds_timeseries.tsv',
+        'confmeta': 'desc-confounds_timeseries.json'
+    }
 
+    def path_from_examples(self, key):
+        return pkgrf('hypernova', 'examples/{}'.format(self.filenames[key]))
 
-def test_36ev_expr():
-    model_formula = 'dd1((rps + wm + csf + gsr)^^2)'
-    spec_sh = shfc(model_formula, df.columns, metadata)
-    assert(spec_sh == (
-        'dd1((trans_x + trans_y + trans_z + rot_x + rot_y + rot_z + '
-        'white_matter + csf + global_signal)^^2)'))
-    expr = hypernova.data.Expression(
-        spec_sh,
-        transforms=[hypernova.data.fc.PowerTransform(),
-                    hypernova.data.fc.DerivativeTransform()]
-    )
-    assert(expr.children[0].children[0].n_children == 9)
+    @pytest.fixture(autouse=True)
+    def setup_class(self):
+        self.confpath = self.path_from_examples('confdata')
+        self.metapath = self.path_from_examples('confmeta')
+        self.metadata = hypernova.data.load_metadata(self.metapath)
+        self.shfc = hypernova.data.fc.FCShorthand()
+        self.df = pd.read_csv(self.confpath, sep='\t')
 
+    def expr_base(self, model_formula, expanded_spec, n_children):
+        spec_sh = self.shfc(model_formula, self.df.columns, self.metadata)
+        if expanded_spec:
+            assert(spec_sh == expanded_spec)
+        expr = hypernova.data.Expression(
+            spec_sh,
+            transforms=[hypernova.data.fc.PowerTransform(),
+                        hypernova.data.fc.DerivativeTransform()]
+        )
+        assert(expr.n_children == n_children)
 
-def test_36ev_spec():
-    model_formula = 'dd1((rps + wm + csf + gsr)^^2)'
-    fcms = hypernova.data.FCConfoundModelSpec(model_formula, '36ev')
-    out = fcms(df, metadata)
-    out[['trans_y', 'trans_y_power2', 'trans_y_derivative1']]
-    assert('trans_y_power2_derivative1' in out.columns)
-    assert(out.shape[1] == 36)
+    def spec_base(self, model_formula, name, shape):
+        fcms = hypernova.data.FCConfoundModelSpec(model_formula, name)
+        out = fcms(self.df, self.metadata)
+        assert(out.shape[1] == shape)
+        return out
 
+    def test_36ev_expr(self):
+        self.expr_base(
+            model_formula='dd1((rps + wm + csf + gsr)^^2)',
+            expanded_spec=(
+                'dd1((trans_x + trans_y + trans_z + rot_x + rot_y + rot_z + '
+                'white_matter + csf + global_signal)^^2)'),
+            n_children=1
+        )
 
-def test_acc_expr():
-    model_formula = 'd1(rps) + acc<v=29.9, mask=CSF+WM>'
-    spec_sh = shfc(model_formula, df.columns, metadata)
-    assert(spec_sh == (
-        'd1(trans_x + trans_y + trans_z + rot_x + rot_y + rot_z) + ' +
-        ' + '.join([f'a_comp_cor_{i:02}' for i in range(4)]) + ' + ' +
-        ' + '.join([f'a_comp_cor_{i:02}' for i in range(9, 22)])))
-    expr = hypernova.data.Expression(
-        spec_sh,
-        transforms=[hypernova.data.fc.PowerTransform(),
-                    hypernova.data.fc.DerivativeTransform()]
-    )
-    assert(expr.n_children == 18)
-    assert(expr.children[0].children[0].n_children == 6)
+    def test_36ev_spec(self):
+        out = self.spec_base(
+            model_formula='dd1((rps + wm + csf + gsr)^^2)',
+            name='36ev',
+            shape=36)
+        out[['trans_y', 'trans_y_power2', 'trans_y_derivative1']]
+        assert('trans_y_power2_derivative1' in out.columns)
 
+    def test_acc_expr(self):
+        self.expr_base(
+            model_formula='d1(rps) + acc<v=29.9, mask=CSF+WM>',
+            expanded_spec=(
+                'd1(trans_x + trans_y + trans_z + rot_x + rot_y + rot_z) + ' +
+                ' + '.join([f'a_comp_cor_{i:02}' for i in range(4)]) + ' + ' +
+                ' + '.join([f'a_comp_cor_{i:02}' for i in range(9, 22)])),
+            n_children=18
+        )
 
-def test_acc_spec():
-    model_formula = 'd1(rps) + acc<v=29.9, mask=CSF+WM>'
-    fcms = hypernova.data.FCConfoundModelSpec(model_formula, '36ev')
-    out = fcms(df, metadata)
-    assert('trans_y' not in out.columns)
-    assert('a_comp_cor_02' in out.columns)
-    assert(out.shape[1] == 23)
+    def test_acc_spec(self):
+        out = self.spec_base(
+            model_formula='d1(rps) + acc<v=29.9, mask=CSF+WM>',
+            name='acc',
+            shape=23)
+        assert('trans_y' not in out.columns)
+        assert('a_comp_cor_02' in out.columns)
 
+    def test_accn_expr(self):
+        self.expr_base(
+            model_formula='d0-1(rps) + acc<n=5, mask=CSF+WM>',
+            expanded_spec=(
+                'd0-1(trans_x + trans_y + trans_z + rot_x + rot_y + rot_z) + ' +
+                ' + '.join([f'a_comp_cor_{i:02}' for i in range(5)]) + ' + ' +
+                ' + '.join([f'a_comp_cor_{i:02}' for i in range(9, 14)])),
+            n_children=11
+        )
 
-def test_accn_expr():
-    model_formula = 'd0-1(rps) + acc<n=5, mask=CSF+WM>'
-    spec_sh = shfc(model_formula, df.columns, metadata)
-    assert(spec_sh == (
-        'd0-1(trans_x + trans_y + trans_z + rot_x + rot_y + rot_z) + ' +
-        ' + '.join([f'a_comp_cor_{i:02}' for i in range(5)]) + ' + ' +
-        ' + '.join([f'a_comp_cor_{i:02}' for i in range(9, 14)])))
-    expr = hypernova.data.Expression(
-        spec_sh,
-        transforms=[hypernova.data.fc.PowerTransform(),
-                    hypernova.data.fc.DerivativeTransform()]
-    )
-    assert(expr.n_children == 11)
-    assert(expr.children[0].children[0].n_children == 6)
+    def test_accn_spec(self):
+        out = self.spec_base(
+            model_formula='d0-1(rps) + acc<n=5, mask=CSF+WM>',
+            name='acc',
+            shape=22)
+        assert('a_comp_cor_30' not in out.columns)
+        assert('a_comp_cor_02' in out.columns)
 
+    def test_aroma_expr(self):
+        self.expr_base(
+            model_formula='wm + csf + aroma',
+            expanded_spec=None,
+            n_children=41
+        )
 
-def test_accn_spec():
-    model_formula = 'd0-1(rps) + acc<n=5, mask=CSF+WM>'
-    fcms = hypernova.data.FCConfoundModelSpec(model_formula, '36ev')
-    out = fcms(df, metadata)
-    assert('a_comp_cor_30' not in out.columns)
-    assert('a_comp_cor_02' in out.columns)
-    assert(out.shape[1] == 22)
-
-
-def test_aroma_expr():
-    model_formula = 'wm + csf + aroma'
-    spec_sh = shfc(model_formula, df.columns, metadata)
-    assert(len(spec_sh.split(' + ')) == 41)
-    expr = hypernova.data.Expression(
-        spec_sh,
-        transforms=[hypernova.data.fc.PowerTransform(),
-                    hypernova.data.fc.DerivativeTransform()]
-    )
-    assert(expr.n_children == 41)
-
-
-def test_aroma_spec():
-    model_formula = 'wm + csf + aroma'
-    fcms = hypernova.data.FCConfoundModelSpec(model_formula, '36ev')
-    out = fcms(df, metadata)
-    assert('aroma_motion_57' in out.columns)
-    assert('aroma_motion_38' not in out.columns)
-    assert(out.shape[1] == 41)
+    def test_aroma_spec(self):
+        out = self.spec_base(
+            model_formula='wm + csf + aroma',
+            name='aroma',
+            shape=41)
+        assert('aroma_motion_57' in out.columns)
+        assert('aroma_motion_38' not in out.columns)
