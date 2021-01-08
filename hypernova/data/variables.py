b541274367d9c92f0de34d4b3ad50f005a433872
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-01-08 01:14:25 -0800

    (ENH) enhance base functionality of DatasetVariables, new var classes

--- hypernova/data/variables.py
+++ hypernova/data/variables.py
@@ -7,6 +7,10 @@ Variables
 Dataset variable classes.
 """
 from abc import ABC, abstractmethod
+from .transforms import (
+    IdentityTransform, EncodeOneHot, ToTensor,
+    ReadNiftiTensorBlock, ReadTableTensorBlock
+)
 
 
 def get_col(df, label):
@@ -17,27 +21,63 @@ def get_col(df, label):
 
 
 class DatasetVariable(ABC):
+    def __init__(self, name='data'):
+        self.name = name
+        self.assignment = None
+        self.transform = IdentityTransform()
+
     @abstractmethod
+    def assign(self, arg):
+        pass
+
     def __call__(self):
-        return None
+        return self.transform(self.assignment)
+
+    def __repr__(self):
+        s = f'{type(self).__name__}('
+        s += f'name={self.name}, '
+        s += f'assigned={self.assignment is not None}, '
+        s += f'transform={type(self.transform).__name__}'
+        s += ')'
+        return s
 
 
 class CategoricalVariable(DatasetVariable):
-    def __init__(self, var, df):
-        values = get_col(df, var).unique()
-        self.name = var
+    def __init__(self, name, df):
+        super(CategoricalVariable, self).__init__(name)
+        values = get_col(df, name).unique()
         self.max_label = len(values)
         self.label_dict = dict(zip(values, range(self.max_label)))
         self.reverse_dict = {v: k for k, v in self.label_dict.items()}
+        self.transform = EncodeOneHot(n_levels=self.max_label)
 
-    def __call__(self, df):
+    def assign(self, df):
         values = get_col(df, self.name)
-        return [self.label_dict[v] for v in values]
+        self.assignment = [self.label_dict[v] for v in values]
 
 
 class ContinuousVariable(DatasetVariable):
-    def __init__(self, var, df):
-        self.name = var
+    def __init__(self, name, df):
+        super(ContinuousVariable, self).__init__(name)
+        self.transform = ToTensor()
 
-    def __call__(self, df):
-        return get_col(df, self.name)
+    def assign(self, df):
+        self.assignment = get_col(df, self.name)
+
+
+class NiftiBlockVariable(DatasetVariable):
+    def __init__(self, name, levels=None):
+        super(NiftiBlockVariable, self).__init__(name)
+        self.transform = ReadNiftiTensorBlock(names=levels)
+
+    def assign(self, df):
+        self.assignment = get_col(df, self.name).values.tolist()
+
+
+class TableBlockVariable(DatasetVariable):
+    def __init__(self, name, spec=None, levels=None):
+        super(TableBlockVariable, self).__init__(name)
+        self.transform = ReadTableTensorBlock(names=levels, spec=spec)
+
+    def assign(self, df):
+        self.assignment = get_col(df, self.name).values.tolist()
