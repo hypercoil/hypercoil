1f97faaea0c3b35d0afbb5371aab44dbc6e22a5a
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-01-09 17:22:12 -0800

    (REF) completely overhaul and atomise data transform functionality

--- hypernova/data/transforms.py
+++ hypernova/data/transforms.py
@@ -10,82 +10,116 @@ import torch
 import nibabel as nb
 import pandas as pd
 from abc import ABC, abstractmethod
+from ..formula import ModelSpec
 from .grabber import LightBIDSObject
 
 
-class IdentityTransform(object):
-    def __call__(self, sample):
-        return sample
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
 
-
-class ReadNeuroTensor(ABC):
-    def __init__(self, dtype='torch.FloatTensor', nanfill=None, names=None):
-        self.dtype = dtype
-        self.nanfill = nanfill
-        self.all_names = names
-
-    def __call__(self, sample):
-        path = sample.path if isinstance(sample, LightBIDSObject) else sample
-        data = self.read(path)
-        data = torch.Tensor(data).type(self.dtype)
-        if self.nanfill is not None:
-            data[torch.isnan(data)] = self.nanfill
-        data.names = self.names(data)
+    def __call__(self, data):
+        for t in self.transforms:
+            data = t(data)
         return data
 
-    @abstractmethod
-    def read(self, path):
-        raise NotImplementedError()
+
+class IdentityTransform(object):
+    def __call__(self, data):
+        return data
+
+
+class ToTensor(object):
+    def __init__(self, dtype='torch.FloatTensor'):
+        self.dtype = dtype
+
+    def __call__(self, data):
+        if isinstance(data, pd.DataFrame):
+            data = data.values
+        return torch.Tensor(data).type(self.dtype)
+
+
+class ToNamedTensor(ToTensor):
+    def __init__(self, dtype='torch.FloatTensor',
+                 names=None, truncate='last'):
+        self.all_names = names
+        self.truncate = truncate
+        super(ToNamedTensor, self).__init__(dtype)
+
+    def __call__(self, data):
+        out = super(ToNamedTensor, self).__call__(data)
+        out.names = self.names(out)
+        return out
 
     def names(self, data):
-        return self.all_names[:data.dim()]
+        if self.truncate == 'last':
+            return self.all_names[:data.dim()]
+        elif self.truncate == 'first':
+            return self.all_names[-data.dim():]
 
 
-class ReadNiftiTensor(ReadNeuroTensor):
-    def __init__(self, dtype='torch.FloatTensor', nanfill=None):
-        names = ('x', 'y', 'z', 't')
-        super(ReadNiftiTensor, self).__init__(dtype, nanfill, names)
-
-    def read(self, path):
-        return nb.load(path).get_fdata()
-
-
-class ReadTableTensor(ReadNeuroTensor):
-    def __init__(self, dtype='torch.FloatTensor', nanfill=None, spec=None):
-        names = ('var', 't')
-        super(ReadTableTensor, self).__init__(dtype, nanfill, names)
-        self.spec = spec
-
-    def read(self, path):
-        if self.spec is not None:
-            return self.spec(pd.read_csv(path, sep='\t')).values
-        return pd.read_csv(path, sep='\t').values
-
-
-class ReadNeuroTensorBlock(object):
-    def __init__(self, dtype='torch.FloatTensor',
-                 nanfill=None, names=None, **params):
-        self.dtype = dtype
+class NaNFill(object):
+    def __init__(self, nanfill=None):
         self.nanfill = nanfill
-        self.names = names
-        self.params = params
 
-    def __call__(self, sample):
-        if isinstance(sample, list):
-            if isinstance(self.names, list) and len(self.names) > 1:
-                nn = self.names[1:]
-            else:
-                nn = self.names
-            rec = [self.__class__(self.dtype, self.nanfill, nn, **self.params)
-                   for _ in sample]
-            tensors = extend_and_conform([r(s) for r, s in zip(rec, sample)])
-            names = tensors[0].names
-            tensors = torch.stack([t.rename(None) for t in tensors])
-            if self.names is not None:
-                tensors.names = [
-                    '_x_'.join(n) for n in [self.names]] + list(names)
-            return tensors
-        return self.transform(sample)
+    def __call__(self, data):
+        if self.nanfill is not None:
+            if isinstance(data. np.ndarray):
+                data[np.isnan(data)] = self.nanfill
+            elif isinstance(data, torch.Tensor):
+                data[torch.isnan(data)] = self.nanfill
+        return data
+
+
+class ApplyModelSpecs(object):
+    def __init__(self, models):
+        self.models = models
+
+    def __call__(self, data):
+        return {m.name: m(data) for m in self.models}
+
+
+class ApplyTransform(object):
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, iterable):
+        if isinstance(iterable, dict):
+            return {k: self.transform(v) for k, v in iterable.items()}
+        else:
+            return [self.transform(v) for v in iterable]
+
+
+class BlockTransform(object):
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, block):
+        if isinstance(block, list):
+            return [self(e) for e in block]
+        return self.transform(block)
+
+
+class UnzipTransformedBlock(object):
+    def __init__(self, depth=-1):
+        self.depth = depth
+        self.cur_depth = 0
+
+    def __call__(self, block):
+        if isinstance(block[0], list):
+            return self([self(e) for e in block])
+        return {k: [d[k] for d in block] for k in block[0].keys()}
+
+
+class ConsolidateBlock(object):
+    def __call__(self, block):
+        if isinstance(block, torch.Tensor):
+            return block
+        elif isinstance(block[0], torch.Tensor):
+            out = extend_and_conform(block)
+        else:
+            out = extend_and_conform([self(e) for e in block])
+        return torch.stack(out)
 
 
 def extend_and_conform(tensor_list):
@@ -103,35 +137,22 @@ def extend_to_size(tensor, size):
     return out
 
 
-class ReadNiftiTensorBlock(ReadNeuroTensorBlock):
-    def __init__(self, dtype='torch.FloatTensor', nanfill=None, names=None):
-        super(ReadNiftiTensorBlock, self).__init__(dtype, nanfill, names)
-        self.transform = ReadNiftiTensor(self.dtype, self.nanfill)
+class ReadDataFrame(object):
+    def __init__(self, sep='\t', **kwargs):
+        self.sep = sep
+        self.kwargs = kwargs
+
+    def __call__(self, path):
+        if isinstance(path, LightBIDSObject):
+            path = path.path
+        return pd.read_csv(path, sep=self.sep, **self.kwargs)
 
 
-class ReadTableTensorBlock(ReadNeuroTensorBlock):
-    def __init__(self, dtype='torch.FloatTensor',
-                 nanfill=None, names=None, **params):
-        super(ReadTableTensorBlock, self).__init__(
-            dtype, nanfill, names, **params)
-        self.transform = ReadTableTensor(
-            self.dtype, self.nanfill, **self.params)
+class ReadNeuroImage(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
 
-
-class EncodeOneHot(object):
-    def __init__(self, n_levels, dtype='torch.FloatTensor'):
-        self.n_levels = n_levels
-        self.dtype = dtype
-        self.patterns = torch.eye(self.n_levels)
-
-    def __call__(self, sample):
-        idx = torch.Tensor(sample).type('torch.LongTensor')
-        return self.patterns[idx].type(self.dtype)
-
-
-class ToTensor(object):
-    def __init__(self, dtype='torch.FloatTensor'):
-        self.dtype = dtype
-
-    def __call__(self, sample):
-        return torch.Tensor(sample).type(self.dtype)
+    def __call__(self, path):
+        if isinstance(path, LightBIDSObject):
+            path = path.path
+        return nb.load(path, **self.kwargs).get_fdata()
