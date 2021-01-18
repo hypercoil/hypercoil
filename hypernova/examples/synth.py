55fd1fd5e9e2f22576fda43499edafd2b7809964
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-01-17 23:28:40 -0800

    (ENH) inject a synthetic stim cycle pattern

--- hypernova/examples/synth.py
+++ hypernova/examples/synth.py
@@ -10,6 +10,7 @@ Creation process for a small synthetic test dataset.
 
 import numpy as np
 import nibabel as nb
+from scipy.signal import convolve
 
 
 def mixing(seed=666, p=50, d=5):
@@ -60,3 +61,29 @@ def package_image(seed=666, n=500, d=5, ax=4):
     affine = np.eye(4)
 
     return nb.Nifti1Image(dataobj=ts, affine=affine)
+
+
+def stim_pattern(on=10, off=40, n=500):
+    # The generation process is pretty meaningless and extremely crude.
+    # Please do not consider it for any actual scientific purpose.
+    cycle = np.zeros(on + off)
+    cycle[-on:] = 1
+    pattern = np.tile(cycle, n // (on + off) + 1)
+
+    xs = np.arange(1, (on + off) + 1)
+    z = [3.5, 2]
+    f1 = xs ** z[0] * np.exp(-xs)
+    f2 = (xs / 3) ** z[1] * np.exp(-(xs / 3))
+    pattern = convolve(pattern, f1 - f2, mode='same')
+
+    return pattern[:n]
+
+
+def stim_roi(ax=4):
+    roi = np.zeros(ax, ax, ax)
+    roi[:2, 1:3, 1:3] = 1
+    return roi.astype('bool')
+
+
+def synthesise_dataset(sub=10, ses=0, run=4, task=('')):
+    pass
