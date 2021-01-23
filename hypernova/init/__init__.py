8878583dcac2086a20cef771c5bafb5065b0b3ac
rciric <rastko@stanford.edu>
2021-01-22 23:14:39 -0800

    (ENH) atlas: add noise, normalise, init function

--- hypernova/init/__init__.py
+++ hypernova/init/__init__.py
@@ -4,6 +4,9 @@
 """
 Initialisation
 """
+from .atlas import (
+    DiscreteAtlas
+)
 from .iirfilter import (
     IIRFilterSpec
 )
--- hypernova/init/atlas.py
+++ hypernova/init/atlas.py
@@ -7,17 +7,21 @@ Atlas initialisation
 Tools for initialising parameters corresponding to brain atlases.
 """
 import torch
+import numpy as np
 import nibabel as nb
 from scipy.ndimage import gaussian_filter
+from ..functional import ScalarIIDNoiseSource
 
 
-class Atlas(object):
+class DiscreteAtlas(object):
     def __init__(self, path, null=0, label_dict=None, mask=None):
         self.path = path
         self.ref = nb.load(self.path)
         self.image = self.ref.get_fdata()
         self.labels = set(np.unique(self.image)) - set([null])
-        if mask is None:
+        if mask == 'auto':
+            mask = (self.image != null)
+        elif mask is None:
             mask = np.ones_like(self.image)
         self.mask = mask.astype(np.bool)
         self.n_labels = len(self.labels)
@@ -29,9 +33,28 @@ class Atlas(object):
             gaussian_filter(map, sigma=sigma, output=map)
         return map[self.mask]
 
-    @property
-    def map(self, sigma=None):
+    def map(self, sigma=None, noise=None, normalise=True):
         map = np.zeros((self.n_labels, self.n_voxels))
         for i, l in enumerate(self.labels):
             map[i] = self._extract_label(l, sigma)
+        map = torch.Tensor(map)
+        if noise is not None:
+            map = noise(map)
+        if normalise:
+            map /= map.sum(1, keepdim=True)
         return map
+
+
+def atlas_init_(tensor, atlas, kernel_sigma=None, noise_sigma=None, null=0):
+    rg = tensor.requires_grad
+    tensor.requires_grad = False
+    if noise_sigma is not None:
+        distr = torch.distributions.normal.Normal(
+            torch.Tensor([0]), torch.Tensor([noise_sigma])
+        )
+        noise = ScalarIIDNoiseSource(distr=distr)
+    else:
+        noise = None
+    map = atlas.map(sigma=kernel_sigma, noise=noise)
+    tensor[:] = torch.Tensor(map)
+    tensor.requires_grad = rg
