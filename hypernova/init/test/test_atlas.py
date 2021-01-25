1e3e4f6623cf0a613026369ed8863f9369478183
rciric <rastko@stanford.edu>
2021-01-25 00:58:14 -0800

    (TEST) smoke and regression tests for atlas linear layer

--- hypernova/init/test/test_atlas.py
+++ hypernova/init/test/test_atlas.py
@@ -6,7 +6,11 @@ Unit tests for atlas map initialisation
 """
 import pytest
 import torch
+import numpy as np
+import nibabel as nb
 import templateflow.api as tflow
+from nilearn.input_data import NiftiLabelsMasker
+from hypernova.nn.atlas import AtlasLinear
 from hypernova.init.atlas import (
     ContinuousAtlas,
     DiscreteAtlas,
@@ -28,6 +32,9 @@ class TestAtlasInit:
             self.atlas_discrete.n_labels,
             self.atlas_discrete.n_voxels
         ))
+        self.nil = NiftiLabelsMasker(labels_img=str(path),
+                                     resampling_target=None)
+        self.aff = nb.load(path).affine
         paths = tflow.get(
             'OASIS30ANTs',
             resolution=1,
@@ -37,6 +44,11 @@ class TestAtlasInit:
             self.atlas_continuous.n_labels,
             self.atlas_continuous.n_voxels
         ))
+        self.inp = np.arange(91 * 109 * 91 * 50).reshape(91, 109, 91, 50)
+        self.inp2 = torch.rand(
+            2, 1, 1, 2, *self.atlas_discrete.mask.shape, 10)
+        self.inpT = torch.Tensor(self.inp)
+        self.lin = AtlasLinear(self.atlas_discrete)
 
     def test_discrete_atlas(self):
         atlas_init_(
@@ -51,3 +63,13 @@ class TestAtlasInit:
             self.atlas_continuous)
         assert torch.allclose(self.tsr_continuous.sum(1), torch.Tensor([1]))
         assert self.tsr_continuous[:, 1].argmax() == 4
+
+    def test_atlas_nn_extradims(self):
+        out = self.lin(self.inp2)
+        assert out.size() == torch.Size([2, 1, 1, 2,
+                                         self.atlas_discrete.n_labels, 10])
+
+    def test_atlas_nn_regression(self):
+        out = self.lin(self.inpT)
+        ref = self.nil.fit_transform(nb.Nifti1Image(self.inp, affine=self.aff))
+        assert np.allclose(out.detach().numpy(), ref.T)
