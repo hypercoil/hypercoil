ec5d420f15c6e96c7c304cdc9319e13f94021447
rciric <rastko@stanford.edu>
2021-02-01 01:41:55 -0800

    (REF) separate generic column transform from ordered transform

--- hypernova/formula/coltransforms.py
+++ hypernova/formula/coltransforms.py
@@ -13,7 +13,98 @@ from collections import OrderedDict
 from functools import reduce
 
 
+class MatchRule(object):
+    def __init__(self, regex, rule, typedict=None):
+        self.regex = re.compile(regex)
+        self.rule = rule
+        self.typedict = typedict or {}
+
+    def _cast_all(self, dict):
+        for k, v in dict.items():
+            t = self.typedict.get(k)
+            if t:
+                dict[k] = t(v)
+        return dict
+
+    def __call__(self, dict):
+        return self.rule(self._cast_all(dict))
+
+
+class MatchOnly(MatchRule):
+    def __init__(self, regex):
+        rule = lambda x: x
+        super(MatchOnly, self).__init__(regex=regex, rule=rule)
+
+
+class AllOrders(MatchRule):
+    def __init__(self, regex, first=0):
+        self.first = first
+        rule = lambda x: x.update(order=set(range(self.first, x['order'] + 1)))
+        typedict = {
+            'order': int
+        }
+        super(AllOrders, self).__init__(
+            regex=regex,
+            rule=rule,
+            typedict=typedict
+        )
+
+
+class SelectOrder(MatchRule):
+    def __init__(self, regex):
+        rule = lambda x: x.update(order=set(self._order_as_range(x['order'])))
+        super(SelectOrder, self).__init__(regex=regex, rule=rule)
+
+    def _order_as_range(self, order):
+        """
+        Convert a hyphenated string representing order of the column transform
+        (if applicable) into a range object that can be passed as input to the
+        appropriate expansion function. For instance, the string `3-5` will be
+        converted to range(3, 6).
+        """
+        order = order.split('-')
+        order = [int(o) for o in order]
+        if len(order) > 1:
+            order = range(order[0], (order[-1] + 1))
+        return order
+
+
 class ColumnTransform(object):
+    def __init__(self, transform, matches, name='transform'):
+        self.transform = transform
+        self.matches = matches
+        self.name = name
+
+    def parse_expr(self, expr):
+        """
+        Search a formula string for the regular expressions indicating that
+        the transform is to be applied (stored in the `all` and `select`
+        attributes). If any matches are identified, extract any additional
+        arguments to the transform.
+        """
+        for match in self.matches:
+            m = re.match(match.regex, expr)
+            if m:
+                parsed = m.groupdict()
+                match(parsed)
+                return parsed
+
+    def argform(self, **args):
+        return None
+
+    def __call__(self, variables, data, **args):
+        selected = data[variables]
+        data_xfm = pd.DataFrame(
+            data=self.transform(selected, **args),
+            columns=[f'{v}_{self.name}{self.argform(**args)}'
+                     for v in variables])
+        return data_xfm
+
+    def __repr__(self):
+        return f'{type(self).__name__}()'
+
+
+class OrderedTransform(ColumnTransform):
     """
     Generic transformation applied column-wise to a DataFrame. Used to enhance
     hypernova.data.Expression and enable its parse tree to support additional
@@ -82,50 +173,21 @@ class ColumnTransform(object):
     """
     def __init__(self, transform, all, select,
                  name='transform', first=0, identity=None):
-        self.transform = transform
-        self.all = re.compile(all)
-        self.select = re.compile(select)
-        self.name = name
-        self.first = first
         self.identity = identity
-
-    def check_and_expand(self, expr, variables, data):
-        """
-        Search a formula string for the regular expressions indicating that
-        the transform is to be applied (stored in the `all` and `select`
-        attributes). If any matches are identified, apply each order of
-        transform thus specified.
-
-        This function is called by hypernova.data.Expression. In nearly all
-        cases, there is no need to call it outside of that context.
-        """
-        if re.search(self.all, expr):
-            order = self.all.findall(expr)
-            order = set(range(self.first, int(*order) + 1))
-        elif re.search(self.select, expr):
-            order = self.select.findall(expr)
-            order = set(self._order_as_range(*order))
-        data = self(
-            order=order,
-            variables=variables,
-            data=data
+        matches = [
+            AllOrders(regex=all, first=first),
+            SelectOrder(regex=select)
+        ]
+        super(OrderedTransform, self).__init__(
+            transform=transform,
+            name=name,
+            matches=matches
         )
-        return data
 
-    def _order_as_range(self, order):
-        """
-        Convert a hyphenated string representing order of the column transform
-        (if applicable) into a range object that can be passed as input to the
-        appropriate expansion function. For instance, the string `3-5` will be
-        converted to range(3, 6).
-        """
-        order = order.split('-')
-        order = [int(o) for o in order]
-        if len(order) > 1:
-            order = range(order[0], (order[-1] + 1))
+    def argform(self, order, **args):
         return order
 
-    def __call__(self, order, variables, data):
+    def __call__(self, variables, data, order, **args):
         """
         Apply the transform of a particular set of orders to specified columns
         of a DataFrame.
@@ -139,22 +201,22 @@ class ColumnTransform(object):
         data : DataFrame
             DataFrame containing the variables to be transformed.
         """
-        variables_xfm, data_xfm = OrderedDict(), OrderedDict()
+        order = order.copy()
+        data_xfm = OrderedDict()
         selected = data[variables]
         if self.identity in order:
             data_xfm[self.identity] = selected
             order -= {self.identity}
         for o in order:
-            data_xfm[o] = pd.DataFrame(
-                data=self.transform(selected, o),
-                columns=[f'{v}_{self.name}{o}' for v in variables])
+            data_xfm[o] = super(OrderedTransform, self).__call__(
+                variables=variables,
+                data=data,
+                order=o
+            )
         data_xfm = pd.concat(
             data_xfm.values(), axis=1
         )
         return data_xfm
-
-    def __repr__(self):
-        return f'{type(self).__name__}()'
 
 
 class NoOrderCallable():
