e1d92cec05cd64d10734baad5691121df21e68d0
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-04-15 04:43:38 -0700

    (REF) subclass reducing regularisations

--- hypernova/reg/smoothness.py
+++ hypernova/reg/smoothness.py
@@ -6,22 +6,14 @@ Smoothness
 ~~~~~~~~~~
 Penalise backwards differences to favour smoothness.
 """
-from torch import diff, norm as pnorm
-from torch.nn import Module
+from torch import diff
 from functools import partial
+from .norm import NormedRegularisation
 
 
-class SmoothnessPenalty(Module):
-    def __init__(self, nu=1, axis=-1, prepend=None, append=None, norm=None):
-        self.nu = nu
-        self.axis = axis
-        self.prepend = prepend
-        self.append = append
-        self.norm = norm or partial(pnorm, p=2)
-
-    def forward(self, weight):
-        return self.nu * self.norm(
-            diff(weight, dim=self.axis,
-                 prepend=self.prepend,
-                 append=self.append)
+class SmoothnessPenalty(NormedRegularisation):
+    def __init__(self, nu=1, axis=-1, prepend=None, append=None, norm=2):
+        self.reg = partial(
+            diff, dim=axis, prepend=prepend, append=append
         )
+        super(SmoothnessPenalty, self).__init__(nu=nu, p=norm, reg=reg)
