529ddb499edfdbf86f127f235757b0b7be08497a
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-04-15 04:45:23 -0700

    (MAINT) init for regularisations

---
+++ hypernova/reg/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Initialisation
+"""
+from .norm import (
+    ReducingRegularisation,
+    NormedRegularisation
+)
+from .smoothness import (
+	SmoothnessPenalty
+)
+from .symbimodal import (
+    SymmetricBimodal
+)
