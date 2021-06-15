3e0dcd2bfa666fc8c4a936b8bc8f75a1ebb976e5
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-06-15 14:12:17 -0700

    (ENH) init from any iid distr, init as const or identity stack

--- hypernova/init/base.py
+++ hypernova/init/base.py
@@ -7,6 +7,24 @@ Initialiser
 Base initialiser for a module.
 """
 import torch
+from functools import partial
+from ..functional.domain import Identity
+
+
+def from_distr_init_(tensor, distr):
+    """
+    Populate a tensor with values sampled from a specified distribution.
+
+    Parameters
+    ----------
+    tensor : Tensor
+        Tensor to populate or initialise from the specified distribution.
+    distr : Distribution
+        Pytorch distribution object from which to sample values used to
+        populate the tensor.
+    """
+    val = distr.sample(tensor.shape)
+    tensor[:] = val
 
 
 def uniform_init_(tensor, min=0, max=1):
@@ -14,8 +32,7 @@ def uniform_init_(tensor, min=0, max=1):
     Populate a tensor with values uniformly sampled i.i.d. from a specified
     interval. The tensor is updated in place.
 
-    TODO: This should be implemented as a specific case of an init from
-          distribution.
+    This is a convenience wrapper around `from_distr_init_`.
 
     Parameters
     ----------
@@ -28,8 +45,28 @@ def uniform_init_(tensor, min=0, max=1):
         Upper bound of the interval from which the tensor's elements are
         sampled.
     """
-    val = torch.rand_like(tensor) * (max - min) + min
-    tensor[:] = val
+    distr = torch.distributions.Uniform(min, max)
+    from_distr_init_(tensor, distr)
+
+
+def constant_init_(tensor, value=0):
+    """
+    Initialise a tensor to a constant value throughout. (The specified value
+    doesn't actually have to be scalar as long as it is broadcastable to the
+    tensor being initialised.)
+    """
+    tensor[:] = value
+
+
+def identity_init_(tensor, scale=1):
+    """
+    Initialise a tensor such that each of its slices is an identity matrix.
+    Currently this sets each slice defined by the last two axes to identity.
+    If there is a use case for other slices, it can be made more flexible in
+    the future.
+    """
+    dim = tensor.size(-1)
+    tensor[:] = scale * torch.eye(dim)
 
 
 class DomainInitialiser(object):
@@ -94,4 +131,30 @@ class BaseInitialiser(DomainInitialiser):
         interval (0, 1).
     """
     def __init__(self, init=None):
-        super(BaseInitialiser, self).__init__(init=init, domain=None)
+        self.init = init or uniform_init_
+        self.domain = Identity()
+
+    def __call__(self, tensor):
+        rg = tensor.requires_grad
+        tensor.requires_grad = False
+        self.init(tensor)
+        tensor.requires_grad = rg
+
+
+class DistributionInitialiser(DomainInitialiser):
+    def __init__(self, distr, domain=None):
+        self.distr = distr
+        init = partial(from_distr_init_, distr=self.distr)
+        super(DistributionInitialiser, self).__init__(
+            init=init,
+            domain=domain
+        )
+
+
+class ConstantInitialiser(DomainInitialiser):
+    def __init__(self, value=1, domain=None):
+        init = partial(constant_init_, value=value)
+        super(ConstantInitialiser, self).__init__(
+            init=init,
+            domain=domain
+        )
