2c5ddf38e0d73d4612d22c7ce77e09c2bdaf1241
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-06-21 22:22:28 -0700

    (DOC) document Dirichlet sample initialisation

--- hypercoil/init/dirichlet.py
+++ hypercoil/init/dirichlet.py
@@ -15,6 +15,29 @@ from ..functional.domain import MultiLogit
 
 
 def dirichlet_init_(tensor, distr, axis=-1):
+    """
+    Dirichlet sample initialisation.
+
+    Initialise a tensor such that any 1D slice through that tensor along a
+    given axis is a sample from a specified Dirichlet distribution. Each 1D
+    slice can therefore be understood as encoding a categorical probability
+    distribution.
+
+    Parameters
+    ----------
+    tensor : Tensor
+        Tensor to initialise in-place.
+    distr : instance of torch.distributions.Dirichlet
+        Parametrised Dirichlet distribution from which all 1D slices of the
+        input tensor along the specified axis are sampled.
+    axis : int (default -1)
+        Axis along which slices are sampled from the specified Dirichlet
+        distribution.
+
+    Returns
+    -------
+    None. The input tensor is initialised in-place.
+    """
     dim = list(tensor.size())
     del(dim[axis])
     val = distr.sample(dim)
@@ -23,6 +46,38 @@ def dirichlet_init_(tensor, distr, axis=-1):
 
 
 class DirichletInit(DomainInitialiser):
+    """
+    Dirichlet sample initialiser.
+
+    Initialise a tensor such that any 1D slice through that tensor along a
+    given axis is a sample from a specified Dirichlet distribution. Each 1D
+    slice can therefore be understood as encoding a categorical probability
+    distribution.
+
+    When this initialiser is coupled with a softmax domain (`MultiLogit`),
+    the parent module can ensure that 1D slices of the initialised weights
+    remain in the probability simplex as the module learns. This is currently
+    the default behaviour.
+
+    Parameters
+    ----------
+    n_classes : int
+        Number of classes in the distribution.
+    concentration : iterable
+        Concentration parameter for the Dirichlet distribution. This must have
+        length equal to `n_classes`.
+    axis : int (default -1)
+        Axis along which slices are sampled from the specified Dirichlet
+        distribution.
+    domain : Domain object (default MultiLogit)
+        Used in conjunction with an activation function to constrain or
+        transform the values of the initialised tensor. For instance, using
+        the MultiLogit domain constrains slices of the tensor (as seen by
+        data) to lie in the appropriate probability simplex. Domain objects
+        can be used with compatible modules and are documented further in
+        `hypercoil.functional.domain`. If no domain is specified, the
+        MultiLogit domain is used.
+    """
     def __init__(self, n_classes, concentration=None, axis=-1, domain=None):
         if isinstance(concentration, torch.Tensor):
             self.concentration = concentration
