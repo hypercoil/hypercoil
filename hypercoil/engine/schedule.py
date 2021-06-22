c2e124319878ca00686e7340005c9e7ce3b98a2d
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-06-21 21:48:46 -0700

    (PROTO) add convergence trigger, multi-trigger classes

--- hypercoil/engine/schedule.py
+++ hypercoil/engine/schedule.py
@@ -71,3 +71,57 @@ class LossTrigger(Trigger):
     def check(self):
         val = self.W[self.loss].value
         return check_thresh_condition(val, self.thr, self.condition)
+
+
+class ConvergenceTrigger(Trigger):
+    def __init__(self, thr, n_consecutive=1, loss='loss', W=None):
+        super().__init__(W)
+        self.thr = thr
+        self.n_consecutive = n_consecutive
+        self.loss = loss
+
+    def check(self):
+        val = self.W[self.loss].delta[-(self.n_consecutive + 1):]
+        if val.size(0) < (self.n_consecutive + 1):
+            return torch.tensor(False)
+        return ((val[0] - val).abs().max() < self.thr)
+
+
+class MultiTrigger(Trigger):
+    def __init__(self, triggers, W=None):
+        super().__init__()
+        self.triggers = list(triggers)
+        if W is not None:
+            self.register(W)
+
+    def register(self, W):
+        [t.register(W) for t in self.triggers]
+
+    def check(self):
+        return torch.Tensor([t.check() for t in self.triggers])
+
+    def __len__(self):
+        return len(self.triggers)
+
+    def __add__(self, other):
+        return MultiTrigger(self.triggers + other.triggers)
+
+    def __getitem__(self, index):
+        return self.triggers[index]
+
+
+class TriggerVector(MultiTrigger):
+    def __bool__(self):
+        raise RuntimeError('Boolean value of variable vector is ambiguous')
+
+
+class TriggerIntersection(MultiTrigger):
+    def check(self, W):
+        val = super().check(W)
+        return torch.all(val)
+
+
+class TriggerUnion(MultiTrigger):
+    def check(self, W):
+        val = super().check(W)
+        return torch.any(val)
