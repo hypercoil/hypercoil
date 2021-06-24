ed3a1c4fa36ce10571fa84fd5482db14af22b91f
Rastko Ciric <rc042010@mymail.pomona.edu>
2021-06-24 00:55:41 -0700

    (FIX) centre of mass: support for negative-valued axes

--- hypercoil/functional/cmass.py
+++ hypercoil/functional/cmass.py
@@ -12,17 +12,21 @@ import torch
 def cmass(X, axes=None, na_rm=False):
     dim = X.size()
     ndim = X.dim()
-    axes = axes or list(range(ndim))
-    out_dim = [s for ax, s in enumerate(dim) if ax not in axes]
+    all_axes = list(range(ndim))
+    if axes is not None:
+        axes = [all_axes[ax] for ax in axes]
+    else:
+        axes = all_axes
+    out_dim = [s for ax, s in enumerate(dim) if all_axes[ax] not in axes]
     out_dim += [len(axes)]
     out = torch.zeros(out_dim)
     for i, ax in enumerate(axes):
         coor = torch.arange(1, X.size(ax) + 1)
-        while coor.dim() < ndim - ax:
+        while coor.dim() < ndim - all_axes[ax]:
             coor.unsqueeze_(-1)
         num = (coor * X).sum(axes)
         denom = X.sum(axes)
         out[..., i] = num / denom - 1
         if na_rm is not False:
-            out[denom==0, i] = na_rm
+            out[denom == 0, i] = na_rm
     return out
