efd7901c9440eaab073c5eec393bac36eb9694a2
rciric <rastko@stanford.edu>
2021-11-26 15:02:49 -0800

    (REF) delegate frame-building functionality to grabbers and layouts

--- hypercoil/data/dataref.py
+++ hypercoil/data/dataref.py
@@ -248,16 +248,18 @@ class DataQuery(object):
         Currently this does nothing. Eventually it should override the
         variable's default transform. For now, just write a new variable
         class if the existing ones don't have suitable transforms.
+    template :
     filters : dict
         Additional keyword arguments to the query are used to filter the
         dataset to return only those data objects matching the query.
     """
     def __init__(self, name='data', pattern=None, variables=None,
-                 transform=None, **filters):
+                 transform=None, template=None, **filters):
         self.name = name
         self.pattern = pattern
         self.variables = variables
         self.transform = transform
+        self.template = template
         self.filters = filters
 
     def __call__(self, layout, **params):
@@ -279,6 +281,8 @@ class DataQuery(object):
         result : list(DatasetVariable)
             List of data objects in the layout matching the query.
         """
+        #TODO: Note that this doesn't at all consider the `pattern` field
+        # of the query
         filters = {}
         filters.update(self.filters)
         filters.update(params)
@@ -359,26 +363,17 @@ def data_references(data_dir, layout, reference, labels, outcomes,
         List of data reference objects created from files found in the input
         directory.
     """
+    #TODO
+    # Why is data_dir an input parameter again? No good reason, other than a
+    # bunch of calls to this already have it set as the first argument. Drop
+    # it and refactor before it becomes even more of a nuisance.
     labels = labels or []
     outcomes = outcomes or []
-    ident = list(observations) + list(levels)
-    entities = assemble_entities(layout, ident, ignore)
-    index, observations, levels, entities = collate_product(
-        entities, observations, levels)
-    data = query_all(layout, index, queries, **filters)
-    df = pd.DataFrame(
-        data=data,
-        index=index)
-    df_aux = read_additional_tables(additional_tables, entities)
-    #TODO
-    # Determine if we should concat first or delete nulls first. There's not
-    # a great sensible way to ensure null deletion occurs on the basis of data
-    # missing from the query frame vs. data missing from additional frames, if
-    # this is even desirable. This order would additionally impact the concat
-    # join type.
-    df = _concat_frames(df, df_aux)
-    df = delete_null_levels(df, observations, levels)
-    df = delete_null_obs(df, observations, levels)
+    df = layout.dataset(observations, levels,
+                        ignore=None, queries=None,
+                        additional_tables=None, **filters)
+    observations = [o for o in observations if o in df.index.names]
+    levels = [l for l in levels if l in df.index.names]
     obs = all_observations(df.index, observations, levels)
     labels, outcomes = process_labels_and_outcomes(df, labels, outcomes)
     variables = process_variables(queries)
@@ -387,225 +382,7 @@ def data_references(data_dir, layout, reference, labels, outcomes,
     return data_refs
 
 
-def assemble_entities(layout, ident, ignore=None):
-    """
-    Assemble all entities corresponding to the specified identifiers from a
-    layout.
-
-    Parameters
-    ----------
-    layout : object
-        Object representing a dataset layout with a `getall` method that
-        returns a list of all values of its argument entity present in the
-        dataset.
-    ident : list(str)
-        List of identifier variables present in the dataset and its layout.
-    ignore : dict(str: list) or None (default None)
-        Dictionary indicating identifiers to be ignored. Currently this
-        doesn't support any logical composition and takes logical OR over all
-        ignore specifications. In other words, data will be ignored if they
-        satisfy any of the ignore criteria.
-
-    Returns
-    -------
-    entities : dict
-        Dictionary enumerating the existing levels of each identifier variable
-        in the dataset.
-    """
-    entities = {}
-    ignore = ignore or {}
-    for i in ident:
-        if ignore.get(i) is None:
-            ignore[i] = []
-
-    for i in ident:
-        entities[i] = layout.getall(i)
-        entities[i] = [n for n in entities[i] if n not in ignore[i]]
-
-    return entities
-
-
-def collate_product(values, observations, levels):
-    """
-    Collate a product of all possible combinations of within-observation
-    variable assignments.
-
-    This returns all possible combinations, including potentially invalid ones
-    that are absent in the actual dataset. Additional functions like
-    `delete_null_levels` and `delete_null_obs` can help remove the invalid
-    combinations returned by `collate_product`.
-
-    Parameters
-    ----------
-    values : dict
-        Dictionary enumerating the existing levels of each identifier variable
-        in the dataset.
-    observations : list(str)
-        List of identifier variables that distinguish observations from one
-        another.
-    levels : list(str)
-        List of identifier variables that denote sub-levels of a single
-        observation.
-
-    Returns
-    -------
-    index : MultiIndex
-        Pandas MultiIndex object enumerating all possible combinations of
-        variable assignments in the input `values`.
-    observations : list
-        The input `observations`, filtered to exclude levels absent from the
-        data.
-    levels : list
-        The input `levels`, filtered to exclude levels absent from the data.
-    entities : list
-        List of all identifier levels present in the data.
-    """
-    entities = []
-    prod_gen = []
-    for name, l in values.items():
-        if l:
-            entities += [name]
-            prod_gen += [l]
-    observations = [o for o in observations if o in entities]
-    levels = [l for l in levels if l in entities]
-    index = pd.MultiIndex.from_product(prod_gen, names=entities)
-    return index, observations, levels, entities
-
-
-def query_all(layout, index, queries, **filters):
-    """
-    Query a layout for all values assigned to entities in an index.
-
-    Parameters
-    ----------
-    layout : object
-        Object representing a dataset layout with a `get` method that
-        returns a list of all data files that match a specified query.
-    index : MultiIndex
-        Pandas MultiIndex object enumerating all possible combinations of
-        variable assignments among dataset identifiers.
-    queries : list(DataQuery)
-        Data queries used to locate files in the dataset. Each entry locates a
-        different file for each unique identifier combination.
-    filters : dict
-        Additional filters to apply for each query.
-
-    Returns
-    -------
-    results : dict
-        Results for each query in `queries` submitted to the `layout` after
-        applying any `filters` additionally specified.
-    """
-    results = {q.name: [] for q in queries}
-    entities = index.names
-    for ident in index:
-        ident_filters = dict(zip(entities, ident))
-        for q in queries:
-            result = q(layout, **ident_filters, **filters)
-            if not result: result = [None]
-            results[q.name] += result
-    return results
-
-
-def read_additional_tables(paths, entities):
-    """
-    Read a set of TSV-formatted data files into DataFrames with a specified
-    set of index columns.
-
-    Parameters
-    ----------
-    paths : list(str)
-        List of paths to additional TSV-formatted data files.
-    entities : list
-        List of identifier entities corresponding to index columns in the
-        data files. These must be the first columns in the data files.
-
-    Returns
-    -------
-    list(DataFrame)
-        List of DataFrames read from the files in `paths`.
-    """
-    if paths is None:
-        return []
-    idx = list(range(len(entities)))
-    return [pd.read_csv(p, sep='\t', index_col=idx) for p in paths]
-
-
-def _concat_frames(df, df_aux):
-    """
-    Concatenate a data frame with a list of additional data frames.
-    """
-    dfs = [df] + df_aux
-    return pd.concat(dfs, axis=1)
-
-
-def delete_null_levels(df, observations, levels):
-    """
-    Remove from the data frame any identifier combinations that are absent
-    from all observations.
-
-    Parameters
-    ----------
-    df : DataFrame
-        DataFrame from which to remove null identifier rows.
-    observations : list(str)
-        Identifier variables whose assignments separate observations from one
-        another.
-    levels : list(str)
-        Identifier variables that separate multiple measures of the same
-        observation.
-
-    Returns
-    -------
-    DataFrame
-        Input DataFrame with null levels removed.
-    """
-    return _delete_nulls(df, observations, levels, all_levels)
-
-
-def delete_null_obs(df, observations, levels):
-    """
-    Remove from the data frame any missing observations.
-
-    Parameters
-    ----------
-    df : DataFrame
-        DataFrame from which to remove null observation rows.
-    observations : list(str)
-        Identifier variables whose assignments separate observations from one
-        another.
-    levels : list(str)
-        Identifier variables that separate multiple measures of the same
-        observation.
-
-    Returns
-    -------
-    DataFrame
-        Input DataFrame with null observations removed.
-    """
-    return _delete_nulls(df, observations, levels, all_observations)
-
-
-def _delete_nulls(df, observations, levels, all_combinations):
-    """
-    Remove from the data frame any identifier combinations with missing data
-    across all other levels. Abstract function called by `delete_null_levels`
-    and `delete_null_obs`.
-    """
-    for c in all_combinations(df.index, observations, levels):
-        if level_null(df, c):
-            df = df.drop(df.loc(axis=0)[c].index)
-    return df
-
-
-def all_levels(index, observations, levels):
-    """
-    Obtain all extant within-observation identifier combinations in a
-    DataFrame MultiIndex.
-    """
-    return _all_combinations(index, combine=levels, control=observations)
-
-
+# TODO: We need to refactor the below functions away.
 def all_observations(index, observations, levels):
     """
     Obtain all extant observation identifier combinations in a DataFrame
@@ -656,33 +433,6 @@ def _fill_idx_pattern(level, idx):
     """
     level = list(level)
     return [i if i is not None else level.pop(0) for i in idx]
-
-
-def level_null(df, level):
-    """
-    Return True if the entry corresponding to a specified index level is null
-    for at least one variable across all rows of a DataFrame.
-
-    Parameters
-    ----------
-    df : DataFrame
-        DataFrame to check for a null level.
-    level : tuple
-        Tuple slicing into the DataFrame's index. The tuple should specify
-        index values for the input DataFrame.
-
-    Returns
-    -------
-    bool
-        Indicates whether the input level is null, i.e., whether the entry is
-        null for at least one variable in every row of the DataFrame
-        corresponding to that level.
-    """
-    any_null = False
-    for v in df.columns:
-        v_null = df.loc(axis=0)[level][v].isnull()
-        any_null = (any_null | v_null)
-    return any_null.all()
 
 
 def process_labels_and_outcomes(df, labels, outcomes):
