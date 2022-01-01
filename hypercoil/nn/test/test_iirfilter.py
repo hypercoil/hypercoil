bd96ebe19b4a624caf825b228e240f8491ed969a
rciric <rastko@stanford.edu>
2021-12-31 16:26:03 -0800

    (MAINT) clean up IIR filter functionality

--- hypercoil/nn/test/test_iirfilter.py
+++ hypercoil/nn/test/test_iirfilter.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 import numpy as np
 from scipy.signal import butter, lfilter, filtfilt
@@ -5,29 +6,41 @@ from hypercoil.nn.iirfilter import DTDF
 from hypercoil.init.iirfilterbutforreal import IIRFilterSpec
 
 
-Z = np.random.rand(10, 5, 100)
-Z2 = np.random.rand(10, 5, 100, 10)
+class TestIIRFilter:
+    @pytest.fixture(autouse=True)
+    def setup_class(self):
+        self.Z = np.random.rand(10, 5, 100)
+        self.Z2 = np.random.rand(10, 5, 100, 10)
 
+        self.bw = butter(N=2, Wn=0.1, fs=1)
+        self.bw1 = butter(N=1, Wn=0.1, fs=1)
+        self.bw3 = butter(N=3, Wn=0.1, fs=1)
+        self.bwbp = butter(N=2, Wn=(0.1, 0.2), fs=1, btype='bandpass')
 
-bw = butter(N=2, Wn=0.1, fs=1)
-bw1 = butter(N=1, Wn=0.1, fs=1)
-bw3 = butter(N=3, Wn=0.1, fs=1)
-ref = lfilter(bw[0], bw[1], Z)
-ref1 = lfilter(bw1[0], bw1[1], Z)
-ref2 = lfilter(bw[0], bw[1], Z2, axis=-2)
-ref3 = lfilter(bw3[0], bw3[1], Z)
+    def compare_spec_and_ref(self, spec, ref):
+        ff = DTDF(spec)
+        out = ff(torch.tensor(self.Z)).detach()
+        assert np.allclose(ref, out, atol=1e-6)
 
+    def test_order2(self):
+        ref = lfilter(self.bw[0], self.bw[1], self.Z)
+        spec = IIRFilterSpec(N=2, Wn=0.1, fs=1, ftype='butter', btype='lowpass')
+        self.compare_spec_and_ref(spec, ref)
 
-bwX = IIRFilterSpec(N=2, Wn=0.1, fs=1, ftype='butter', btype='lowpass')
-ff = DTDF(bwX)
-out = ff(torch.tensor(Z))
+    def test_order1(self):
+        ref = lfilter(self.bw1[0], self.bw1[1], self.Z)
+        spec = IIRFilterSpec(N=1, Wn=0.1, fs=1, ftype='butter', btype='lowpass')
+        self.compare_spec_and_ref(spec, ref)
 
-bw1X = IIRFilterSpec(N=1, Wn=0.1, fs=1, ftype='butter', btype='lowpass')
-ff1 = DTDF(bw1X)
-out1 = ff1(torch.tensor(Z))
+    def test_order3(self):
+        ref = lfilter(self.bw3[0], self.bw3[1], self.Z)
+        spec = IIRFilterSpec(N=3, Wn=0.1, fs=1, ftype='butter', btype='lowpass')
+        self.compare_spec_and_ref(spec, ref)
 
-bw3X = IIRFilterSpec(N=3, Wn=0.1, fs=1, ftype='butter', btype='lowpass')
-ff3 = DTDF(bw3X)
-out3 = ff3(torch.tensor(Z))
-
-out2 = ff(torch.tensor(Z2), feature_ax=True)
+    def test_bandpass(self):
+        ref = lfilter(self.bwbp[0], self.bwbp[1], self.Z)
+        spec = IIRFilterSpec(N=2, Wn=(0.1, 0.2), fs=1, ftype='butter', btype='bandpass')
+        self.compare_spec_and_ref(spec, ref)
+        #TODO: add test for filtering across another axis when it's implemented
+        #ref2 = lfilter(bw[0], bw[1], Z2, axis=-2)
+        #out2 = ff(torch.tensor(Z2), feature_ax=True)
