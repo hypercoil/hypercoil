510cb881706dce454b7b99355c8e2d63fdc06938
rciric <rastko@stanford.edu>
2022-02-06 10:36:11 -0800

    (ENH) penalty on norm of only positive entries

--- hypercoil/reg/norm.py
+++ hypercoil/reg/norm.py
@@ -7,6 +7,7 @@ Normed penalty
 Generalised module for applying a normed penalty to the weight parameter
 of a module.
 """
+import torch
 from torch import norm as pnorm
 from torch.nn import Module
 from functools import partial
@@ -35,3 +36,14 @@ class NormedRegularisation(ReducingRegularisation):
 
     def extra_repr(self):
         return f'norm=L{self.p}'
+
+
+class UnilateralNormedRegularisation(NormedRegularisation):
+    def __init__(self, nu, p=2, reg=None):
+        if reg is None:
+            reg = lambda x: x
+        r = lambda x: torch.maximum(reg(x), torch.tensor(0))
+        super(UnilateralNormedRegularisation, self).__init__(
+            nu=nu, p=p, reg=r
+        )
+        self.p = p
