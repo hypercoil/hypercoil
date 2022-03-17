911ca25aaefe9c5c806cc13ddd87e03b3bebdca5
Rastko Ciric <rastko@stanford.edu>
2022-03-17 03:37:56 -0700

    (MAINT)(DOC) deprecate old SVM module, document new SVM module

--- hypercoil/nn/svm.py
+++ hypercoil/nn/svm.py
@@ -25,7 +25,7 @@ def hinge_loss(Y_hat, Y):
 
 def labels_binary(labels, dtype=torch.float):
     l = labels.unique()[0]
-    return [-2 * (labels == l).type(dtype) + 1]
+    return [2 * (labels == l).type(dtype) - 1]
 
 
 def labels_one_vs_rest(labels, dtype=torch.float):
@@ -121,216 +121,7 @@ class SigmoidKernel(Kernel):
         return super(SigmoidKernel, self).__call__(X, Z)
 
 
-class SVM(CvxpyLayer):
-    def __init__(self, n, K=GaussianKernel(), C=1.):
-        """
-        Initialise a kernelised support vector machine as a fully
-        differentiable convex optimisation layer.
-
-        Parameters
-        ----------
-        n : int
-            Number of observations in the training dataset. Because we use
-            the dual, this is a required parameter. We could automatically
-            grab it in `fit` and this would probably be better in the long
-            run (and more conformant with sklearn syntax), but we'll get to
-            that later...
-        K : callable (default Gaussian)
-            Kernel function. The callable signature should accept either
-            one or two :math:`n \times d` matrices and return a single
-            positive semidefinite :math:`d \times d` matrix (the kernel
-            matrix). If the callable receives a single argument, it
-            should return the kernel matrix computed 
-        C : float (default 1)
-            Hyperparameter that trades off between maximising the margin
-            and penalising soft-margin violations.
-
-        Notes
-        -----
-            This problem is framed as the dual of a soft-margin SVM. See
-            10:15 here: https://www.youtube.com/watch?v=zzn80wmclnw
-        
-            Note that we must introduce the new variable Z below to
-            satisfy the `cxvpylayers` disciplined parametric programming
-            requirement.
-        """
-        alpha = cvx.Variable((n, 1))
-        Z_ = cvx.Variable((n, 1))
-
-        self.C = C
-        self.K = K
-        self.n = n
-
-        X_ = cvx.Parameter((n, n), PSD=True)
-        Y_ = cvx.Parameter((n, 1))
-
-        # For DPP compliance, we cannot use the quadratic form.
-        # We formulate instead the following equivalent problem,
-        # taking the matrix square root in the forward pass below.
-        #
-        # This is equivalent to the more 'canonical' quadratic form
-        # objective = cvx.Maximize(
-        #     cvx.sum(alpha) - 0.5 * cvx.quad_form(
-        #         cvx.multiply(alpha, Y_),
-        #         cvx.atoms.affine.wraps.psd_wrap(X_)
-        #     )
-        # )
-        #
-        # See https://www.cvxpy.org/tutorial/advanced/index.html ... 
-        # ... #disciplined-parametrized-programming
-        objective = cvx.Maximize(
-            cvx.sum(alpha) - 0.5 * cvx.sum_squares(X_ @ Z_)
-        )
-        constraints = [
-            alpha >= 0,
-            alpha <= self.C,
-            Y_.T @ alpha == 0,
-            Z_ == cvx.multiply(alpha, Y_)
-        ]
-        problem = cvx.Problem(objective, constraints)
-        super().__init__(problem, parameters=[X_, Y_], variables=[alpha])
-
-    def fit(self, X, Y):
-        """
-        Fit the SVM to a training dataset.
-        """
-        #TODO: Figure out what the deal is with typing in cvxpy layers.
-        # It appears it might sometimes cast to double precision.
-        self.X, self.Y = X, Y
-        ker = self.K(self.X)
-        #TODO: the below could be a problem. Make this a parameter to the
-        # module.
-        eps = torch.finfo(ker.dtype).eps
-        fac = 0.01 * torch.linalg.matrix_norm(ker, 2)
-        ker = recondition_eigenspaces(ker, psi=fac, xi=fac)
-        symsqker = symsqrt(ker)
-        L, Q = torch.linalg.eigh(ker)
-        self.alpha = super().forward(
-            symsqker, Y
-        )[0]
-
-        # Recover the primal solution implicitly for prediction.
-        # As here (11:31): https://www.youtube.com/watch?v=zzn80wmclnw
-        # Also https://haipeng-luo.net/courses/CSCI567/2018_fall/lec6.pdf
-
-        # Bias term: first get the support vectors
-        sv_idx = self._support_vectors()
-        Y_sv = self.Y[sv_idx]
-        K_sv = ker[sv_idx]
-        
-        self.bias = self._estimate_bias(K_sv, Y_sv)
-
-    def _support_vectors(self):
-        """
-        Identify the indices of support vectors in the training set.
-        """
-        e1 = torch.logical_and(
-            self.alpha > 0,
-            self.alpha < self.C
-        )
-        e2 = torch.logical_and(
-            torch.logical_not(
-                torch.isclose(
-                    self.alpha,
-                    torch.tensor(self.C),
-                    atol=1e-3,
-                    rtol=1e-3)
-            ),
-            torch.logical_not(
-                torch.isclose(
-                    self.alpha,
-                    torch.tensor(0.),
-                    atol=1e-3,
-                    rtol=1e-3)
-            ),
-        )
-        sv_idx = torch.logical_and(e1, e2).squeeze()
-        return sv_idx
-
-    def _estimate_bias(self, K_sv, Y_sv):
-        """
-        Use the support vectors to reconstitute the SVM bias term from the
-        primal solution. Because the estimates of the bias might differ across
-        support vectors, we use a consistency weighting approach.
-        """
-        b_est = (Y_sv - K_sv @ (self.alpha * self.Y)).squeeze()
-        # Phantom code using consistency weighting.
-        #wei = self._consistency_weighting(b_est)
-        #b = ((b_est * wei).sum() / wei.sum()).item()
-        # b = torch.sign(
-        #     b_est[torch.abs(b_est).argmin()]
-        # ) * torch.abs(b_est).min()
-        b = self._modal_selection(b_est)
-        #print(b_est, wei, b)
-        return b
-    
-    def _consistency_weighting(self, estimates):
-        """
-        Come up with weights for each estimate based on its internal
-        consistency.
-        
-        In the SVM, we can use this to reconstitute the bias term using
-        support vectors. The estimates of the bias might not agree across the
-        support vectors, so we use the consistency of different estimates to
-        upweight or downweight them and produce a consensus.
-
-        NOTE: In practice I haven't found that we need to use this, so it's
-        never actually called right now. The functionality is retained here in
-        case it becomes relevant at some point.
-
-        I have no reference for this, and it is not guaranteed to work.
-        I found that it worked reasonably well in practice on several examples
-        that I tried.
-        """
-        wei = torch.abs((estimates.view(-1, 1) - estimates.view(1, -1)))
-        wei = (wei).mean(0)
-        wei = wei - wei.min()
-        wei = torch.tanh(wei.median() / wei)
-        return wei
-    
-    def _modal_selection(self, estimates, ratio=2):
-        """
-        Select a reasonable bias term by finding what passes as the mode.
-
-        TODO: This is probably not necessary. Test the code using all the
-        estimates and see if it suffers.
-        """
-        dist = torch.abs((estimates.view(-1, 1) - estimates.view(1, -1)))
-        dist = (torch.sqrt(dist)).mean(0)
-        _, modal_idx = torch.sort(dist)
-        n_sv = len(modal_idx)
-        modal = estimates[modal_idx[:(n_sv // ratio + 1)]]
-        return modal.mean()
-
-    def transform(self, X):
-        """
-        Differentiably classify new data using the fit SVM parameters.
-        """
-        return (self.alpha * self.Y).squeeze() @ (
-            self.K(self.X, X)) + self.bias
-
-    def fit_transform(self, X, Y):
-        """
-        Fit the SVM to a training dataset and return its predictions
-        on that dataset.
-        """
-        self.fit(X, Y)
-        return self.transform(X)
-
-    def forward(self, X, Y=None):
-        if self.training:
-            if Y is None:
-                raise ValueError(
-                    'Labelled examples are required when training. Either '
-                    'provide a label set as input `Y` or set the `training` '
-                    'attribute to False to perform inference.'
-                )
-            return self.fit_transform(X, Y)
-        else:
-            return self.transform(X)
-
-
-class MultiSVM(torch.nn.Module):
+class SVM(torch.nn.Module):
     def __init__(
         self,
         C=1.0,
@@ -349,6 +140,86 @@ class MultiSVM(torch.nn.Module):
         sample_weight=None
     ):
         """
+        Initialise a kernelised support vector machine as a fully
+        differentiable convex optimisation layer.
+
+        Do not use this module unless you have a good reason; to enforce DPP
+        compliance and ensure well-formed gradients, some performance
+        sacrifices have been made, and the forward pass quickly becomes
+        prohibitively slow for large problems. A long-term solution would
+        involve finding the analytic derivative through the SVM and
+        implementing a custom backward method while using the sklearn forward.
+        Or, alternatively finding a way to use SVMLIB as the back end solver.
+
+        Parameters
+        ----------
+        C : positive float (default 1)
+            Regularisation hyperparameter; a smaller value of C increases the
+            strength of the regularisation.
+        kernel : 'rbf', 'poly', 'linear', 'sigmoid', or callable
+            Kernel function. The callable signature should accept either
+            one or two :math:`n \times d` matrices and return a single
+            positive semidefinite :math:`n \times n` matrix (the kernel
+            matrix). By default, a Gaussian kernel (RBF) is used. It is also
+            possible to pass a pre-computed Gram matrix by setting this to
+            'precomputed'.
+        degree : int (default 3)
+            If a polynomial kernel is selected, specifies the maximum order
+            of the polynomial.
+        gamma : float, 'auto', or 'scale' (default 'scale')
+            Kernel coefficient for RBF, polynomial, and sigmoid kernels.
+            'scale' and 'auto' automatically configure the kernel coefficient
+            based on the input dataset, as in sklearn.
+        coef0 : float (default 0)
+            Bias coefficient for polynomial and sigmoid kernels.
+        class_weight : list or None (default None)
+            Sets the weight of C to C * `class_weight[i]` for class i. Can be
+            used to adjust the fit if classes are unbalanced.
+        verbose : bool (default False)
+            Toggles the verbosity of the solver.
+        decision_function_shape : 'ovr'
+            Currently, only one-vs-rest classifiers are available in the
+            multi-class setting.
+        recondition : float (default 0.01)
+            Reconditioning parameter to ensure positiveness and uniqueness of
+            all eigenvalues of the Gram matrix, which is necessary for
+            differentiation but likely sacrifices classification performance.
+            Reconditioning adds nonnegative values to the main diagonal of the
+            Gram matrix, approximately on the order of the largest singular
+            value of the matrix scaled by the provided reconditioning
+            factor.
+        formulate_on_forward_pass : bool (default False)
+            Indicates that the problem should be formulated on every forward
+            pass through the network. This will make the forward pass
+            extremely slow, but is necessary if training with a variable
+            number of observations. If this is false, the model cannot be used
+            unless either the `formulate` method is called or both the
+            `n_observations` and `n_classes` parameters are passed to the
+            constructor.
+        solver : None, 'ECOS', or 'SCS' (default None)
+            Convex solver to use for solving the SVM. By default the decision
+            is passed to the `diffcp` package. Note that this is generally
+            incredibly inefficient compared with using a dedicated SVM solver.
+        n_observations : int or None (default None)
+            Number of observations in the training dataset. If not specified,
+            this must be set when calling `formulate`; otherwise it will be
+            determined automatically during the forward pass.
+        n_classes : int or None (default None)
+            Number of classes in the dataset. If not specified, this must be
+            set when calling `formulate`; otherwise it will be determined
+            automatically during the forward pass.
+        sample_weight : tensor or None (default None)
+            Weight for each observation. If specified, the value of C for
+            observation i is scaled to C * sample_weight[i].
+
+        Notes
+        -----
+            This problem is framed as the dual of a soft-margin SVM. See
+            10:15 here: https://www.youtube.com/watch?v=zzn80wmclnw
+
+            Note that we must introduce the new variable Z below to
+            satisfy the `cxvpylayers` disciplined parametric programming
+            requirement.
         """
         self.update_params(
             C=C,
@@ -366,7 +237,7 @@ class MultiSVM(torch.nn.Module):
             n_classes=n_classes,
             sample_weight=sample_weight
         )
-        super(MultiSVM, self).__init__()
+        super(SVM, self).__init__()
 
     def update_params(
         self,
@@ -385,6 +256,11 @@ class MultiSVM(torch.nn.Module):
         n_classes=None,
         sample_weight=None
     ):
+        """
+        Function for safely updating the SVM parameters. Particularly when
+        changing the kernel, use this function instead of directly accessing
+        the attributes.
+        """
         if C is not None:
             self.C = C
         if kernel is not None:
@@ -427,6 +303,9 @@ class MultiSVM(torch.nn.Module):
             )
 
     def formulate_problem(self, n, C=None):
+        """
+        Formulate a single binary SVM classifier's dual problem in CVX.
+        """
         if C is None:
             C = self.C
         alpha = cvx.Variable((n, 1))
@@ -466,6 +345,23 @@ class MultiSVM(torch.nn.Module):
         )
 
     def formulate(self, n_observations, n_classes, sample_weight=None):
+        """
+        Formulate all SVM problems to be solved during each forward pass.
+
+        Parameters
+        ----------
+        n_observations : int
+            Number of observations in the training dataset. If not specified,
+            this must be set when calling `formulate`; otherwise it will be
+            determined automatically during the forward pass.
+        n_classes : int
+            Number of classes in the dataset. If not specified, this must be
+            set when calling `formulate`; otherwise it will be determined
+            automatically during the forward pass.
+        sample_weight : tensor or None (default None)
+            Weight for each observation. If specified, the value of C for
+            observation i is scaled to C * sample_weight[i].
+        """
         C = self.C
         if self.class_weight is not None:
             C = class_weight_vector(Y, self.class_weight) * C
@@ -558,6 +454,10 @@ class MultiSVM(torch.nn.Module):
             self.Y[i, :] = y.squeeze()
 
     def configure_gamma(self, X, n_features):
+        """
+        Helper method for automatically estimating the kernel coefficient
+        parameter gamma based on properties of the input dataset.
+        """
         if self.gamma == 'scale':
             gamma = 1 / (n_features * X.var())
             try:
@@ -656,6 +556,8 @@ class MultiSVM(torch.nn.Module):
         """
         Differentiably classify new data using the fit SVM parameters.
         """
+        if self.kernel == 'precomputed':
+            return (self.alpha * self.Y) @ X + self.bias
         return (self.alpha * self.Y) @ (self.K(self.X, X)) + self.bias
 
     def fit_transform(self, X, Y, sample_weight=None, formulate=False):
