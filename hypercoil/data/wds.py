1e240c796dcaa737aef94b763502a0a089943e58
Rastko Ciric <rastko@stanford.edu>
2022-03-18 16:10:38 -0700

    (DOC) document webdataset data sink / source functionality

--- hypercoil/data/wds.py
+++ hypercoil/data/wds.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+WebDataset
+~~~~~~~~~~
+DataReference conversion to WebDataset format.
+"""
 import random, pathlib, tarfile, shutil, warnings
 import torch
 import webdataset
 from collections import OrderedDict
 from functools import partial
 from webdataset.autodecode import torch_loads
-from hypercoil.data import functional as F
 from hypercoil.data.collate import gen_collate, extend_and_bind
 from hypercoil.data.dataset import ReferencedDataset
 
@@ -57,6 +64,39 @@ refs = ds.data_refs
 """
 
 def split_refs(refs, n_splits, split_level, shuffle_split, seed=None):
+    """
+    Split data references into approximately equal subsets.
+
+    Parameters
+    ----------
+    refs : iterable(DataReference)
+        Data references to be split into approximately equal subsets.
+    n_splits : int
+        Number of splits (equal subsets) into which the references are to be
+        divided.
+    split_level : iterable(ids) or None
+        By default, this operation splits references into subsets without
+        consideration for the identifiers of those references. This may lead
+        to incorrect results in some cases. For instance, if multiple data
+        points are collected per subject, they might be split across different
+        data splits. If one of these points ends up in the test set, this
+        could lead to overestimation of model performance. `split_level`
+        allows specification of an identifier combination that configures the
+        split. For instance, specifying ('subject',) as the split level will
+        split across subjects instead of references (assuming the existence of
+        a 'subject' field in each reference's internal ids). Specifying
+        multiple ids will create a level for each existing combination of
+        those ids' assignments in the dataset.
+    shuffle_split : bool
+        Results in random splits if true.
+    seed : int or None (default None)
+        Random seed for shuffling. Unused if `shuffle_split` is False.
+
+    Returns
+    -------
+    list(list(DataReference))
+        DataReferences split into `n_splits` approximately equal subsets.
+    """
     random.seed(seed)
     levels = OrderedDict()
     if split_level:
@@ -85,6 +125,9 @@ def split_refs(refs, n_splits, split_level, shuffle_split, seed=None):
 
 
 def process_obs(ref, dirname):
+    """
+    Process a single `DataReference` and save it to disk in torch format.
+    """
     try:
         data = ref()
     except AttributeError:
@@ -95,6 +138,11 @@ def process_obs(ref, dirname):
 
 
 def save_data(dirname, basename, data, baseext=None):
+    """
+    Given a key-value dictionary representing different elements of a data
+    sample (e.g., observations and labels), save the elements to disk in torch
+    format, using the file extension to denote the data element.
+    """
     baseext = baseext or ''
     for ext, val in data.items():
         if baseext:
@@ -107,12 +155,44 @@ def save_data(dirname, basename, data, baseext=None):
 
 
 def make_shard(dirname):
+    """
+    Archive a directory in a `.tar` shard.
+    """
     tar_path = f'{dirname}.tar'
     with tarfile.open(tar_path, 'w:') as tar:
         tar.add(dirname, arcname='.')
 
 
 def torch_wds(path, keys, map, shuffle=100, batch_size=1):
+    """
+    Load a `WebDataset` from `.tar` shards.
+
+    Parameter
+    ---------
+    path : str
+        General path to the `WebDataset` shards to include in the WebDataset.
+    keys : dict
+        Dictionary specifying the elements of each data point in the dataset
+        to include in each sample, together with the transformations to apply
+        to each element. For instance,
+        `{'x' : lambda x: x, 'y' : lambda y : EncodeOneHot()}`
+        first identifies two elements, x and y, in each data sample. It then
+        leaves x as is while encoding y as a one-hot vector.
+    map : callable
+        Transformation to apply to each observation as a whole. See `keys`
+        for applying transformations to specific elements.
+    shuffle : int (default 100)
+        Size of data buffer. When the data loader creates a batch, it randomly
+        chooses `batch_size` samples from the data buffer to constitute the
+        batch. A larger buffer generally slows performance but increases
+        randomness.
+    batch_size : int (default 1)
+        Batch size when sampling from the `WebDataset`.
+    """
+    #TODO: add arguments for n_splits_val and n_splits_test and automatically
+    # form the general path based on the omission of these. But that should
+    # probably be a parent function; leave this API exposed for those who want
+    # a greater degree of control.
     collation_fn = partial(gen_collate, concat=extend_and_bind)
     return (
         webdataset.WebDataset(path)
@@ -135,12 +215,62 @@ def make_wds(
     split_level=(),
     shard_level=(),
     shard_size=0,
-    seed=None):
+    seed=None
+):
+    """
+    Create a `WebDataset` from a collection of `DataReference`s.
+
+    Parameters
+    ----------
+    refs : iterable(DataReference)
+        `DataReference`s to be saved into a `WebDataset`.
+    path : str
+        Write path for the `.tar` shards that constitute the `WebDataset`.
+    n_splits : int
+        Number of splits (equal subsets) into which the references are to be
+        divided.
+    n_shards : int
+        Number of `.tar` shards to create per split as the `WebDataset`.
+    shuffle_shard : bool (default True)
+        Assigns data within each split to a random shard if true.
+    shuffle_split : bool (default True)
+        Results in random splits if true.
+    split_level : iterable(ids) (default ())
+        By default, this operation splits references into subsets without
+        consideration for the identifiers of those references. This may lead
+        to incorrect results in some cases. For instance, if multiple data
+        points are collected per subject, they might be split across different
+        data splits. If one of these points ends up in the test set, this
+        could lead to overestimation of model performance. `split_level`
+        allows specification of an identifier combination that configures the
+        split. For instance, specifying ('subject',) as the split level will
+        split across subjects instead of references (assuming the existence of
+        a 'subject' field in each reference's internal ids). Specifying
+        multiple ids will create a level for each existing combination of
+        those ids' assignments in the dataset.
+    shard_level : iterable(ids) (default ())
+        As `split_level`, but constrains assignment of within-split references
+        to shards.
+    shard_size : int (default 0)
+        Approximate number of `DataReference`s to include per shard. If this
+        is assigned a nonzero value, `n_shards` must also be set to 0.
+    seed : int or None (default None)
+        Random seed for shuffling. Unused if `shuffle_split` is False.
+
+    Returns
+    -------
+    Webdataset
+        Webdataset resulting from consecutive splitting and sharding of
+        `DataReference`s.
+    """
 
     wds_path = path or f'/tmp/wds-{random.getrandbits(128)}'
     pathlib.Path(wds_path).mkdir(parents=True, exist_ok=True)
     if shard_size and n_shards:
-        raise ValueError('Cannot specify both shard size and number')
+        raise ValueError(
+            'Cannot specify both shard size and number. '
+            'Ensure `n_shards` is set to 0 to use the shard size.'
+        )
 
 
     splits = split_refs(
@@ -166,7 +296,7 @@ def make_wds(
             dirname = f'{wds_path}/spl-{i:03d}_shd-{j:06d}'
             for ref in shard:
                 process_obs(ref=ref, dirname=dirname)
-            print(f'[SPLIT {i:03d}] [SHARD {j:06d}]')
+            print(f'[ SPLIT {i:03d} | SHARD {j:06d} ]')
             make_shard(dirname)
             shutil.rmtree(dirname)
 
