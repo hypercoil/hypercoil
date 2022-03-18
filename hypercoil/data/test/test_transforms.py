f4c83dc02ca5f1108abe54b94cc8789e8eb3c722
Rastko Ciric <rastko@stanford.edu>
2022-03-18 16:16:11 -0700

    (TEST) add new unit tests for tensor extension and nanfill

--- hypercoil/data/test/test_transforms.py
+++ hypercoil/data/test/test_transforms.py
@@ -8,6 +8,7 @@ Tests of data transforms atomically and in composition.
 """
 import pytest
 import torch
+import hypercoil.data.functional as F
 from hypercoil.data.transforms import (
     Compose, IdentityTransform, EncodeOneHot, ToTensor,
     ApplyModelSpecs, ApplyTransform, BlockTransform,
@@ -39,3 +40,25 @@ class TestTransforms:
             ApplyTransform(ConsolidateBlock())
         ])
         assert xfm(self.zipped)['x'].size() == torch.Size([3, 2, 3, 3])
+
+    def test_extend(self):
+        tensor_list = [
+            torch.rand(1, 3, 3, dtype=torch.double),
+            torch.rand(2, 2, 2),
+            torch.rand(1, 4, 1)
+        ]
+        extended_list = F.extend_to_max_size(tensor_list)
+        for t in extended_list:
+            assert t.shape == (2, 4, 3)
+
+    def test_nanfill(self):
+        Z = torch.tensor([
+            [0, float('nan')],
+            [1, float('nan')],
+            [2, 3]
+        ])
+        Znf, nm = F.nanfill(Z, fill='mean')
+        assert torch.logical_not(torch.any(torch.isnan(Znf)))
+        assert Znf.mean() == 1.5
+        Znf[nm] = -3
+        assert Znf.sum() == 0
