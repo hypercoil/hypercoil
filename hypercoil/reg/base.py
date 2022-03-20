2aee82cc1bdfe17eed40d144d111dd5f847bdc6a
Rastko Ciric <rastko@stanford.edu>
2022-03-19 17:37:33 -0700

    (DOC) base loss modules

--- hypercoil/reg/base.py
+++ hypercoil/reg/base.py
@@ -10,6 +10,9 @@ from torch.nn import Module
 
 
 class Loss(Module):
+    """
+    Base class for hypercoil loss functions.
+    """
     @property
     def extra_repr(self):
         return ()
@@ -22,6 +25,21 @@ class Loss(Module):
 
 
 class LossApply(Module):
+    """
+    Callable loss function wrapper that composes the loss with a selector or
+    other pretransformation.
+
+    Parameters
+    ----------
+    loss : callable
+        Loss function to apply.
+    apply : callable
+        Callable that pretransforms the input to the loss. This can be used
+        as a selector, so that different entries in a `LossScheme` are applied
+        to different combinations of weights, inputs, and outputs. One use
+        case is filtering the inputs to a `LossScheme` and forwarding only the
+        ones relevant to the loss function at hand.
+    """
     def __init__(self, loss, apply=None):
         super(LossApply, self).__init__()
         if apply is None:
@@ -37,6 +55,25 @@ class LossApply(Module):
 
 
 class ReducingLoss(Loss):
+    """
+    Callable loss function wrapper that composes an objective, which may have
+    a standard elementwise or slicewise definition, with a reduction operation
+    that maps its output to a scalar.
+
+    Example reductions include the mean and sum, as well as tensor norms.
+
+    Parameters
+    ----------
+    nu : float
+        Loss function weight multiplier.
+    reduction : callable
+        Map from a tensor of arbitrary dimension to a scalar. The output of
+        `loss` is passed into `reduction` to return a scalar.
+    loss : callable
+        Objective criterion, which might not always return a scalar output.
+    name : str or None (default None)
+        Identifying string for the instantiation of the loss object.
+    """
     def __init__(self, nu, reduction, loss, name=None):
         super(ReducingLoss, self).__init__()
         if name is None:
