47af1ad8992a6560545408595626f27c68a4ae4f
Rastko Ciric <rastko@stanford.edu>
2022-03-23 19:42:43 -0700

    (ENH) surface plots: operate on `AtlasLinear` modules

--- hypercoil/viz/surf.py
+++ hypercoil/viz/surf.py
@@ -5,17 +5,122 @@
 Brain surface plots with `surfplot`.
 These currently require a custom patch of `surfplot`.
 """
-import surfplot
 import torch
+import surfplot
+import numpy as np
 import nibabel as nb
+import matplotlib.pyplot as plt
 import templateflow.api as tflow
+from hypercoil.functional.cmass import cmass_coor
+from hypercoil.functional.sphere import spherical_geodesic
 from hypercoil.neuro.const import fsLR
 from hypercoil.viz.surfutils import _CMapFromSurfMixin
 
 
-class fsLRAtlasParcels(_CMapFromSurfMixin):
+POLE_DECODER = {
+    'cortex_L': np.array([
+        'medial', 'anterior', 'dorsal',
+        'lateral', 'posterior', 'ventral'
+    ]),
+    'cortex_R': np.array([
+        'lateral', 'anterior', 'dorsal',
+        'medial', 'posterior', 'ventral'
+    ]),
+}
+
+POLES = torch.tensor([
+    [100., 0., 0.],
+    [0., 100., 0.],
+    [0., 0., 100.],
+    [-100., 0., 0.],
+    [0., -100., 0.],
+    [0., 0., -100.],
+])
+
+
+class _SurfBrain:
+    def __init__(self, data, lh=None, rh=None, coor_mask=None, cmap='magma',
+                 layout='grid', dpi=200, size=(500, 400), zoom=None,
+                 views=None, mirror_views=False, flip=False, brightness=1,
+                 background=(1, 1, 1), label_text=None, parcels=False,
+                 color_range=None, as_outline=False, zero_transparent=True,
+                 cbar=None, cbar_label=None, struc_tag=None):
+        if parcels:
+            data = data.astype('long')
+            color_range = (1, len(cmap.colors))
+        self.data = self._mask_data(data)
+        self.cmap = self._select_cmap(cmap, struc_tag)
+
+    def _select_cmap(self, cmap, struc_tag=None):
+        return cmap
+
+    def __call__(self):
+        p = surfplot.Plot(
+            surf_lh=self.lh,
+            surf_rh=self.rh,
+            layout=self.layout,
+            views=self.views,
+            mirror_views=self.mirror_views,
+            size=self.size,
+            zoom=self.zoom,
+            background=self.background,
+            flip=self.flip,
+            brightness=self.brightness
+        )
+        p.add_layer(
+            self.data,
+            cmap=self.cmap,
+            color_range=self.color_range,
+            as_outline=self.as_outline,
+            zero_transparent=self.zero_transparent,
+            cbar=self.cbar,
+            cbar_label=self.cbar_label
+        )
+        fig = p.build()
+        fig.set_dpi(dpi)
+        fig.show()
+
+
+VIEWS = {
+    'dorsal' : {
+        'views' : 'dorsal',
+        'size' : (250, 300),
+        'zoom' : 3
+    },
+    'ventral' : {
+        'views' : 'ventral',
+        'size' : (250, 300),
+        'zoom' : 3,
+        'flip' : True
+    },
+    'posterior' : {
+        'views' : 'posterior',
+        'size' : (300, 300),
+        'zoom' : 3
+    },
+    'anterior' : {
+        'views' : 'anterior',
+        'size' : (300, 300),
+        'zoom' : 3,
+        'flip' : True
+    },
+    'medial' : {
+        'views' : 'medial',
+        'size' : (900, 300),
+        'zoom' : 1.8,
+    },
+    'lateral' : {
+        'views' : 'lateral',
+        'size' : (900, 300),
+        'zoom' : 1.8,
+    },
+}
+
+
+class fsLRSurfacePlot:
     def __init__(self, atlas):
-        self.atlas = atlas
+        self.module = atlas
+        self.atlas = atlas.atlas
         coor_query = fsLR().TFLOW_COOR_QUERY
         coor_query.update(suffix='veryinflated')
         self.lh, self.rh = (
@@ -25,49 +130,184 @@ class fsLRAtlasParcels(_CMapFromSurfMixin):
         self.dim_lh = nb.load(self.lh).darrays[0].dims[0]
         self.dim_rh = nb.load(self.rh).darrays[0].dims[0]
         self.dim = self.dim_lh + self.dim_rh
-        self.data_mask = torch.ones_like(self.atlas.mask)
-        self.cmap_mask = self.atlas.mask.clone()
-        self.data_mask[self.dim:] = False
-        self.cmap_mask[self.dim:] = False
+        
+        self.data_mask = {
+            'cortex_L' : torch.ones_like(self.atlas.mask),
+            'cortex_R' : torch.ones_like(self.atlas.mask),
+            'all' : torch.ones_like(self.atlas.mask)
+        }
+        self.cmap_mask = {
+            'cortex_L' : self.atlas.compartments['cortex_L'].clone(),
+            'cortex_R' : self.atlas.compartments['cortex_R'].clone(),
+            'all' : self.atlas.mask.clone()
+        }
+        self.data_mask['cortex_L'][self.dim_lh:] = False
+        self.data_mask['cortex_R'][:self.dim_lh] = False
+        self.data_mask['cortex_R'][self.dim:] = False
+        self.data_mask['all'][self.dim:] = False
+        self.cmap_mask['cortex_L'][self.dim_lh:] = False
+        self.cmap_mask['cortex_R'][:self.dim_lh] = False
+        self.cmap_mask['cortex_R'][self.dim:] = False
+        self.cmap_mask['all'][self.dim:] = False
         self.cortical_mask = (
             self.atlas.compartments['cortex_L'] |
             self.atlas.compartments['cortex_R']
         )
-        self.coor = self.atlas.coors[self.cortical_mask[self.atlas.mask]]
+        self.coor_all = self.atlas.coors[self.cortical_mask[self.atlas.mask]]
 
     def drop_null(self, null=0):
         subset = (self.data != null)
-        return self.data[subset], self.coor[subset]
+        return self.data[subset], self.coor_all[subset]
 
-    def __call__(self, cmap):
-        data = torch.zeros(len(self.atlas.mask), dtype=torch.long)
+
+class fsLRAtlasParcels(
+    _CMapFromSurfMixin,
+    fsLRSurfacePlot
+):
+    def __call__(self, cmap, views=('lateral', 'medial'), save=None):
+        data = torch.zeros_like(self.atlas.mask, dtype=torch.long)
         for compartment in ('cortex_L', 'cortex_R'):
             mask = self.atlas.compartments[compartment]
-            labels = self.atlas.maps[compartment].argmax(0)
+            labels = self.module.weight[compartment].argmax(0).detach()
             compartment_data = (
                 self.atlas.decoder[compartment][labels]
             )
-            compartment_data[self.atlas.maps[compartment].sum(0) == 0] = 0
+            compartment_data[self.module.weight[compartment].sum(0) == 0] = 0
             data[mask] = compartment_data
-        # We're assuming cortex-first ordering in the atlas mask.
-        self.data = data[self.cmap_mask]
+        self.data = data[self.cmap_mask['all']]
         cmap = self._select_cmap(cmap=cmap)
-        self.data = data[self.data_mask].numpy()
+        self.data = data[self.data_mask['all']].numpy()
 
-        p = surfplot.Plot(
-            surf_lh=self.lh,
-            surf_rh=self.rh,
-            size=(200, 300),
-            zoom=3,
-            views='dorsal',
-            brightness=1
-        )
-        p.add_layer(
-            self.data.astype('long')[:self.dim],
-            cmap=cmap,
-            color_range=(1, len(cmap.colors)),
-            cbar=None
-        )
-        fig = p.build()
-        fig.set_dpi(200)
-        fig.show()
+        for view in views:
+            view_args = VIEWS[view]
+            p = surfplot.Plot(
+                surf_lh=self.lh,
+                surf_rh=self.rh,
+                brightness=1,
+                **view_args
+            )
+            p.add_layer(
+                self.data.astype('long')[:self.dim],
+                cmap=cmap,
+                color_range=(1, len(cmap.colors)),
+                cbar=None
+            )
+            fig = p.build()
+            fig.set_dpi(200)
+            fig.show()
+            if save is not None:
+                plt.savefig(f'{save}_view-{view}.png',
+                            dpi=1000,
+                            bbox_inches='tight')
+
+
+
+class fsLRAtlasMaps(fsLRSurfacePlot):
+    def plot_nodemaps(self, figs, max_per_batch=21, scale=(2, 2),
+                      nodes_per_row=3, start_batch=0, stop_batch=None,
+                      save=None):
+        n_figs = len(figs)
+
+        n_batches = int(np.ceil(n_figs / max_per_batch))
+
+        figs_plotted = 0
+        figs_remaining = n_figs
+        batch_index = start_batch
+        if stop_batch is None:
+            stop_batch = n_batches
+        stop_batch = min(stop_batch, n_batches)
+
+        while batch_index < stop_batch:
+            print(batch_index)
+            start_fig = batch_index * max_per_batch
+            figs_remaining = n_figs - start_fig
+            figs_per_batch = min(max_per_batch, figs_remaining)
+            stop_fig = start_fig + figs_per_batch
+
+            n_rows = int(np.ceil(figs_per_batch / nodes_per_row))
+            figsize = (6 * nodes_per_row, 3 * n_rows)
+            
+
+            fig, ax = plt.subplots(
+                n_rows,
+                nodes_per_row,
+                figsize=figsize,
+                num=1,
+                clear=True
+            )
+            batch_figs = figs[start_fig:stop_fig]
+            for index, (name, f) in enumerate(batch_figs):
+                print(index)
+                i = index // nodes_per_row
+                j = index % nodes_per_row
+                f._check_offscreen()
+                x = f.to_numpy(transparent_bg=True, scale=(scale))
+                ax[i, j].imshow(x)
+                ax[i, j].set_xticks([])
+                ax[i, j].set_yticks([])
+                ax[i, j].set_title(f'Node {name}')
+
+            fig.show()
+            if save:
+                plt.savefig(f'{save}_batch-{batch_index}.png',
+                            dpi=300,
+                            bbox_inches='tight')
+                fig.clear()
+                plt.close('all')
+            batch_index += 1
+
+    def __call__(self, cmap='Blues', color_range=(0, 1),
+                 max_per_batch=21, save=None):
+        figs = [None for _ in range(self.atlas.decoder['_all'].max() + 1)]
+        for compartment in ('cortex_L', 'cortex_R'):
+            map = self.module.weight[compartment]
+            decoder = self.atlas.decoder[compartment]
+            compartment_mask = self.atlas.compartments[compartment]
+            coor = self.atlas.coors[compartment_mask[self.atlas.mask]].t()
+            cmasses = cmass_coor(map, coor, radius=100)
+            closest_poles = spherical_geodesic(
+                cmasses.t(),
+                POLES
+            ).argsort(-1)[:, :3]
+            closest_poles = POLE_DECODER[compartment][closest_poles.numpy()]
+            if compartment == 'cortex_L':
+                surf_lh = self.lh
+                surf_rh = None
+            elif compartment == 'cortex_R':
+                surf_lh = None
+                surf_rh = self.rh
+            for node, views, name in zip(map, closest_poles, decoder):
+                data = torch.zeros_like(
+                    self.atlas.mask,
+                    dtype=map.dtype
+                )
+                data[compartment_mask] = node.detach()
+                data = data[self.data_mask[compartment]].numpy()
+                p = surfplot.Plot(
+                    surf_lh=surf_lh,
+                    surf_rh=surf_rh,
+                    brightness=1,
+                    views=views.tolist(),
+                    zoom=1.25,
+                    size=(400, 200)
+                )
+                p.add_layer(
+                    data.astype('long')[:self.dim],
+                    cmap=cmap,
+                    cbar=None,
+                    color_range=color_range
+                )
+                figs[name] = p.render()
+        figs = [(i, f) for i, f in enumerate(figs) if f is not None]
+        
+        n_figs = len(figs)
+        batches_per_run = 5
+        total_batches = int(np.ceil(n_figs / max_per_batch))
+        batches_done = 0
+        while batches_done < total_batches:
+            start_batch = batches_done
+            stop_batch = start_batch + batches_per_run
+            self.plot_nodemaps(figs=figs, save=save,
+                               start_batch=start_batch,
+                               stop_batch=stop_batch)
+            batches_done += batches_per_run
