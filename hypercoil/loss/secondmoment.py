7262f4c27651e9e7eb218bf74019f72c5ee15140
Rastko Ciric <rastko@stanford.edu>
2022-03-28 11:31:08 -0700

    (ENH) mean specification and auto-z-scoring for second moment loss

--- hypercoil/loss/secondmoment.py
+++ hypercoil/loss/secondmoment.py
@@ -12,7 +12,16 @@ from .base import ReducingLoss
 from functools import partial
 
 
-def second_moment(weight, data):
+def _second_moment(weight, data, mu):
+    """
+    Core computation for second-moment loss.
+    """
+    diff = data.unsqueeze(-3) - mu.unsqueeze(-2)
+    sigma = (diff * weight.unsqueeze(-1)) ** 2 / weight.sum()
+    return sigma
+
+
+def second_moment(weight, data, standardise=False):
     r"""
     Compute the second moment of a dataset.
 
@@ -28,10 +37,23 @@ def second_moment(weight, data):
                     T denotes number of observations at each location (e.g.,
                     number of time points).
     """
+    if standardise:
+        data = (data - data.mean(-1)) / data.std(-1)
     mu = (weight @ data / weight.sum(-1, keepdim=True))
-    diff = data.unsqueeze(-3) - mu.unsqueeze(-2)
-    sigma = (diff * weight.unsqueeze(-1)) ** 2 / weight.sum()
-    return sigma
+    return _second_moment(weight, data, mu)
+
+
+def second_moment_centred(weight, data, mu,
+                          standardise_data=False,
+                          standardise_mu=False):
+    r"""
+    Compute the second moment of a dataset about a specified mean.
+    """
+    if standardise_data:
+        data = (data - data.mean(-1)) / data.std(-1)
+    if standardise_mu:
+        mu = (mu - mu.mean(-1)) / mu.std(-1)
+    return _second_moment(weight, data, mu)
 
 
 class SecondMoment(ReducingLoss):
@@ -67,11 +89,31 @@ class SecondMoment(ReducingLoss):
         the linear mapping, multiplied by the number of columns in the
         dataset.
     """
-    def __init__(self, nu=1, reduction=None, name=None):
+    def __init__(self, nu=1, standardise=False, reduction=None, name=None):
         reduction = reduction or mean
+        loss = partial(second_moment, standardise=standardise)
         super(SecondMoment, self).__init__(
             nu=nu,
             reduction=reduction,
-            loss=second_moment,
+            loss=loss,
+            name=name
+        )
+
+
+class SecondMomentCentred(ReducingLoss):
+    r"""
+    Compute the second moment of a dataset/linear mapping pair about a
+    specified mean.
+    """
+    def __init__(self, nu=1, standardise_data=False, standardise_mu=False,
+                 reduction=None, name=None):
+        reduction = reduction or mean
+        loss = partial(second_moment_centred,
+                       standardise_data=standardise_data,
+                       standardise_mu=standardise_mu)
+        super(SecondMomentCentred, self).__init__(
+            nu=nu,
+            reduction=reduction,
+            loss=loss,
             name=name
         )
