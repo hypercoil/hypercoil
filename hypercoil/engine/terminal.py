3248b59198f4a76f525c990ca602d355a2e488bf
Rastko Ciric <rastko@stanford.edu>
2022-03-30 01:43:34 -0700

    (FIX) reactive terminal requires leaf nodes

--- hypercoil/engine/terminal.py
+++ hypercoil/engine/terminal.py
@@ -31,14 +31,19 @@ class ReactiveTerminal(Terminal):
     calls in order to reduce the memory footprint.
     """
     def __init__(self, loss, slice_target, slice_axis, max_slice,
-                 normalise_by_len=True):
+                 normalise_by_len=True, pretransforms=None):
         super().__init__(loss=loss)
         self.slice_target = slice_target
         self.slice_axis = slice_axis
         self.max_slice = max_slice
         self.normalise_by_len = normalise_by_len
+        self.pretransforms = pretransforms or {}
 
     def forward(self, arg):
+        pretransform = {}
+        for k in self.pretransforms.keys():
+            if k != self.slice_target:
+                pretransform[k] = arg.__getitem__(k)
         slice_target = arg.__getitem__(self.slice_target)
         slc = [slice(None) for _ in range(slice_target.dim())]
         total = slice_target.shape[self.slice_axis]
@@ -49,6 +54,11 @@ class ReactiveTerminal(Terminal):
             slc[self.slice_axis] = slice(begin, end)
             sliced = slice_target[slc]
             arg.__setitem__(self.slice_target, sliced)
+            for k, v in self.pretransforms.items():
+                if k != self.slice_target:
+                    arg.__setitem__(k, v(pretransform[k]))
+                else:
+                    arg.__setitem__(k, v(sliced))
             Y = self.loss(**arg)
             if self.normalise_by_len:
                 l = sliced.shape[self.slice_axis]
@@ -67,16 +77,21 @@ class ReactiveMultiTerminal(Terminal):
     in `ReactiveTerminal` and whose values correspond to `slice_axis`.
     """
     def __init__(self, loss, slice_instructions, max_slice,
-                 normalise_by_len=True):
+                 normalise_by_len=True, pretransforms=None):
         super().__init__(loss=loss)
         self.slice_instructions = slice_instructions
         self.max_slice = max_slice
         self.normalise_by_len = normalise_by_len
+        self.pretransforms = pretransforms or {}
 
     def forward(self, arg):
         instructions = {}
         targets ={}
         totals = []
+        pretransform = {}
+        for k in self.pretransforms.keys():
+            if k not in self.slice_instructions.keys():
+                pretransform[k] = arg.__getitem__(k)
         for k, v in self.slice_instructions.items():
             targets[k] = arg.__getitem__(k)
             slc = [slice(None) for _ in range(targets[k].dim())]
@@ -94,6 +109,11 @@ class ReactiveMultiTerminal(Terminal):
                 sliced = slice_target[slc]
                 arg.__setitem__(k, sliced)
                 l = sliced.shape[v] # wasteful but cleaner this way
+            for k, v in self.pretransforms.items():
+                if k not in self.slice_instructions.keys():
+                    arg.__setitem__(k, v(pretransform[k]))
+                else:
+                    arg.__setitem__(k, v(arg[k]))
             Y = self.loss(**arg)
             if self.normalise_by_len:
                 Y = Y * l / total
