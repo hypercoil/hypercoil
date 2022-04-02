9390ad3db63b0babcf65e84a6c9b6ed31dc59611
Rastko Ciric <rastko@stanford.edu>
2022-04-01 19:35:40 -0700

    (DOC) general data reference documentation

--- hypercoil/data/dataref.py
+++ hypercoil/data/dataref.py
@@ -2,8 +2,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
-Data references
-~~~~~~~~~~~~~~~
 Interfaces between neuroimaging data and data loaders.
 """
 import pandas as pd
@@ -23,77 +21,82 @@ class DataReference(object):
 
     If this is a reference to a level of observations, those observations (or
     sub-levels of observations) can be further packaged recursively into
-    DataReference objects and assigned to the `subrefs` field. This enables a
-    parent DataLoader to traverse the dataset at any depth. See
-    `fMRIDataReference` for an example implementation of this and other
-    functionality.
+    ``DataReference`` objects and assigned to the ``subrefs`` field. This
+    enables a parent ``DataLoader`` to traverse the dataset at any depth. See
+    :doc:`fMRIDataReference <hypercoil.data.neuro.fMRIDataReference>`
+    for an example implementation of this and other functionality.
 
     Parameters
     ----------
-    data : DataFrame
-        DataFrame containing all assignments to the variables of the
-        `DataReference`. The name of each variable provided as an argument to
-        `DataReference` should be present as a column name in this DataFrame.
+    data : ``DataFrame``
+        ``pandas DataFrame`` containing all assignments to the variables of
+        the ``DataReference``. The name of each variable provided as an
+        argument to ``DataReference`` should be present as a column name in
+        this ``DataFrame``.
     idx : tuple or None
         Index names corresponding to the observation(s) to be included in this
-        DataReference from the provided DataFrame. If this is None, then it is
-        assumed that all observations in the provided DataFrame are to be
-        included.
+        ``DataReference`` from the provided ``DataFrame``. If this is None,
+        then it is assumed that all observations in the provided DataFrame are
+        to be included.
     level_names : iterable
         Names of any observation levels packaged together into this
-        `DataReference`. Each entry should also be an index column name in
-        `data`; furthermore, each index column whose corresponding `idx` entry
-        is a slice should be included here. In the future, this might be
+        ``DataReference``. Each entry should also be an index column name in
+        ``data``; furthermore, each index column whose corresponding ``idx``
+        entry is a slice should be included here. In the future, this might be
         computed automatically.
-    variables : list(VariableFactory)
-        List of VariableFactory objects that, when called, produce a variable
-        of each type in the `DataReference`. The use of factories rather than
-        the variables themselves enables them to be passed recursively to any
+    variables : list(:doc:`VariableFactory <hypercoil.data.variables.VariableFactory>`)
+        List of ``VariableFactory`` objects that, when called, produce a
+        variable of each type in the ``DataReference``. The use of factories
+        rather than the variables themselves enables them to be passed
+        recursively to any subreferences with ease and without overwriting the
+        parent variable at assignment.
+    labels : list(:doc:`VariableFactory <hypercoil.data.variables.VariableFactory>`)
+        List of ``VariableFactory`` objects that, when called, produce a
+        ``CategoricalVariable`` object for each categorical outcome variable
+        in the ``DataReference.`` The use of factories rather than the
+        variables themselves enables them to be passed recursively to any
         subreferences with ease and without overwriting the parent variable at
         assignment.
-    labels : list(VariableFactory)
-        List of VariableFactory objects that, when called, produce a
-        CategoricalVariable object for each categorical outcome variable in
-        the `DataReference.` The use of factories rather than the variables
-        themselves enables them to be passed recursively to any subreferences
-        with ease and without overwriting the parent variable at assignment.
-    outcomes : list(VariableFactory)
-        List of VariableFactory objects that, when called, produce a
-        ContinuousVariable object for each continuous outcome variable in the
-        `DataReference.` The use of factories rather than the variables
+    outcomes : list(:doc:`VariableFactory <hypercoil.data.variables.VariableFactory>`)
+        List of ``VariableFactory`` objects that, when called, produce a
+        ``ContinuousVariable`` object for each continuous outcome variable in
+        the ``DataReference``. The use of factories rather than the variables
         themselves enables them to be passed recursively to any subreferences
         with ease and without overwriting the parent variable at assignment.
 
     Attributes
     ----------
-    vfactory : list(VariableFactory)
-        The factories passed to the `variables` argument at initialisation.
-    lfactory : list(VariableFactory)
-        The factories passed to the `labels` argument at initialisation.
-    ofactory : list(VariableFactory)
-        The factories passed to the `outcomes` argument at initialisation.
-    variables : list(DatasetVariable)
+    vfactory : list(:doc:`VariableFactory <hypercoil.data.variables.VariableFactory>`)
+        The factories passed to the ``variables`` argument at initialisation.
+    lfactory : list(:doc:`VariableFactory <hypercoil.data.variables.VariableFactory>`)
+        The factories passed to the ``labels`` argument at initialisation.
+    ofactory : list(:doc:`VariableFactory <hypercoil.data.variables.VariableFactory>`)
+        The factories passed to the ``outcomes`` argument at initialisation.
+    variables : list(:doc:`DatasetVariable <hypercoil.data.variables.DatasetVariable>`)
         List of variable objects produced from calls to the factories
         provided at initialisation and assigned the corresponding fields of
-        the input DataFrame. If the references were produced from a call to
-        `data_references` or a parent method, then each variable thus produced
-        will result from a query to the dataset filesystem directory layout.
-    labels : list(DatasetVariable)
-        List of CategoricalVariable objects produced from calls to factories
-        provided at initialisation and assigned the corresponding fields of
-        the input DataFrame. Each corresponds to a specified categorical
-        outcome variable.
-    outcomes : list(DatasetVariable)
-        List of ContinuousVariable objects produced from calls to factories
-        provided at initialisation and assigned the corresponding fields of
-        the input DataFrame. Each corresponds to a specified continuous
-        outcome variable.
+        the input ``DataFrame``. If the references were produced from a call
+        to ``data_references`` or a parent method, then each variable thus
+        produced will result from a query to the dataset filesystem directory
+        layout.
+    labels : list(:doc:`DatasetVariable <hypercoil.data.variables.DatasetVariable>`)
+        List of ``CategoricalVariable`` objects produced from calls to
+        factories provided at initialisation and assigned the corresponding
+        fields of the input ``DataFrame``. Each corresponds to a specified
+        categorical outcome variable.
+    outcomes : list(:doc:`DatasetVariable <hypercoil.data.variables.DatasetVariable>`)
+        List of ``ContinuousVariable`` objects produced from calls to
+        factories provided at initialisation and assigned the corresponding
+        fields of the input ``DataFrame``. Each corresponds to a specified
+        continuous outcome variable.
     ids : dict
         Dictionary of identifiers for the current data reference.
 
-    Any variable name or identifier can additionally be accessed as an
-    attribute of DataReference. This will return the assigned value of the
-    fully transformed variable.
+    Notes
+    -----
+        Any variable name or identifier can additionally be accessed as an
+        attribute of ``DataReference``. This will return the assigned value of
+        the fully transformed variable.
     """
     def __init__(self, data, idx=None, level_names=None,
                  variables=None, labels=None, outcomes=None):
@@ -134,19 +137,20 @@ class DataReference(object):
 
     def parse_ids(self, values):
         """
-        Populate the values of any identifier variables of the DataReference.
+        Populate the values of any identifier variables of the
+        ``DataReference``.
 
         Parameters
         ----------
         values : list
             List of values of identifier variables, ordered identically to the
-            variables in the DataReference index.
+            variables in the ``DataReference`` index.
 
         Returns
         -------
         ids : dict
             Dictionary mapping identifier variable names to the values they
-            are assigned for this DataReference.
+            are assigned for this ``DataReference``.
         """
         ids = OrderedDict()
         for name, value in zip(self.df.index.names, values):
@@ -218,7 +222,7 @@ class DataReference(object):
     def __call__(self):
         """
         Obtain the fully transformed assignments to all variables of the
-        DataReference.
+        ``DataReference``.
 
         Assignments are returned as a dictionary of key-value pairs specifying
         the name of each variable and its fully transformed assignment.
@@ -232,29 +236,34 @@ class DataQuery(object):
     A query to a layout or grabber object representation of a filesystem
     directory containing a dataset.
 
-    Parameters/Attributes
-    ---------------------
+    Parameters
+    ----------
     name : str
         Name of the data object returned by the query.
     pattern : str or None
         Pattern within the search directory that all query results should
         conform to. If this is provided for all queries, then all subsequent
         searches will be substantially narrowed and accelerated.
-    variable : VariableFactoryFactory
-        A VariableFactoryFactory object whose child factory produces a
-        DatasetVariable of a type suitable for storing the paths returned
-        by the query.
+    variables : dict or :doc:`VariableFactoryFactory <hypercoil.data.variables.VariableFactoryFactory>`
+        A ``VariableFactoryFactory`` object whose child factory produces a
+        ``DatasetVariable`` of a type suitable for storing the paths returned
+        by the query, or a dictionary of such objects.
     transform : transform object or None
-        Currently this does nothing. Eventually it should override the
+        Currently *this does nothing*. Eventually it should override the
         variable's default transform. For now, just write a new variable
         class if the existing ones don't have suitable transforms.
-    template :
     filters : dict
         Additional keyword arguments to the query are used to filter the
         dataset to return only those data objects matching the query.
     """
+    ##TODO: figure out what, if anything, the template argument is doing.
+    # Remove arguments that do nothing.
     def __init__(self, name='data', pattern=None, variables=None,
                  transform=None, template=None, **filters):
+        if transform is not None:
+            raise NotImplementedError(
+                'Overriding default transforms is not yet implemented. '
+                'Create a new variable class instead.')
         self.name = name
         self.pattern = pattern
         self.variables = variables
@@ -264,21 +273,23 @@ class DataQuery(object):
 
     def __call__(self, layout, **params):
         """
+        Submit the query to a filesystem layout.
+
         Parameters
         ----------
         layout : object
             Object representing a filesystem directory containing a dataset.
-            It must implement a `get` method that returns all directory
+            It must implement a ``get`` method that returns all directory
             entries that match a set of filters.
         params : dict
             Additional keyword arguments to the query: these are combined with
             the filters provided at initialisation to form a query. If there is
-            a name collision, then filters provided at `__call__` take
+            a name collision, then filters provided at ``__call__`` take
             precedence.
 
         Returns
         -------
-        result : list(DatasetVariable)
+        result : list(:doc:`DatasetVariable <hypercoil.data.variables.DatasetVariable>`)
             List of data objects in the layout matching the query.
         """
         #TODO: Note that this doesn't at all consider the `pattern` field
@@ -319,12 +330,13 @@ def data_references(data_dir, layout, reference, labels, outcomes,
     layout : object
         Object representing a dataset layout. It must implement the following
         methods:
-        - `getall(i)`: returns a list of all values of `i` present in the
-                       dataset
-        - `get`: queries the dataset for matching entities
-    reference : DataReference class
-        Class or subclass of `DataReference` to produce as output.
-    labels : tuple or None (default ('subject',))
+
+        - ``getall(i)``: returns a list of all values of ``i`` present in the
+                         dataset
+        - ``get``: queries the dataset for matching entities
+    reference : :doc:`DataReference <hypercoil.data.dataref.DataReference>` class
+        Class or subclass of ``DataReference`` to produce as output.
+    labels : tuple or None (default ``('subject',)``)
         Names of categorical outcome variables to include in data references.
         These variables can be taken either from data identifiers or from
         additional tables. Labels become available as prediction targets for
@@ -334,15 +346,15 @@ def data_references(data_dir, layout, reference, labels, outcomes,
         These variables can be taken either from data identifiers or from
         additional tables. Outcomes become available as prediction targets for
         regression models.
-    observations : tuple (default ('subject',))
+    observations : tuple (default ``('subject',)``)
         List of data identifiers whose levels are packaged into separate data
         references. Each level should generally have the same values of any
         outcome variables.
-    levels : tuple or None (default ('session', 'run, task'))
+    levels : tuple or None (default ``('session', 'run, task')``)
         List of data identifiers whose levels are packaged as sublevels of the
         same data reference. This permits easier augmentation of data via
         pooling across sublevels.
-    queries : list(DataQuery objects)
+    queries : list(:doc:`DataQuery <hypercoil.data.dataref.DataQuery>`)
         Data queries used to locate files in the dataset. Each entry locates a
         different file for each unique identifier combination.
     filters : dict
@@ -355,13 +367,22 @@ def data_references(data_dir, layout, reference, labels, outcomes,
         Dictionary indicating identifiers to be ignored. Currently this
         doesn't support any logical composition and takes logical OR over all
         ignore specifications. In other words, data will be ignored so long as
-        they satisfy any of the ignore criteria.
+        they satisfy any of the ``ignore`` criteria.
 
     Returns
     -------
-    data_refs : list(DataReference)
+    data_refs : list(:doc:`DataReference <hypercoil.data.dataref.DataReference>`)
         List of data reference objects created from files found in the input
         directory.
+
+    See also
+    --------
+    fmriprep_references:
+        :doc:`Implementation on BIDS-formatted data
+        <hypercoil.data.bids.fmriprep_references>`.
+    hcp_references:
+        :doc:`Implementation on HCP-formatted data
+        <hypercoil.data.hcp.hcp_references>`.
     """
     #TODO
     # Why is data_dir an input parameter again? No good reason, other than a
