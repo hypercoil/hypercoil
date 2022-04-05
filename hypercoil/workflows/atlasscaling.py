f5d6261e7a2c6842f3288ea6ce64fc7b4efa2146
rciric <rastko@stanford.edu>
2022-04-05 02:18:49 -0700

    (PROTO) atlas scaling test: decent hyperparameters

--- hypercoil/workflows/atlasscaling.py
+++ hypercoil/workflows/atlasscaling.py
@@ -15,6 +15,11 @@ from hypercoil.functional import (
     cmass_coor,
     residualise
 )
+from hypercoil.engine import (
+    Epochs,
+    MultiplierSigmoidSchedule,
+    LossArchive
+)
 from hypercoil.loss import (
     LossScheme,
     LossApply,
@@ -45,14 +50,15 @@ from hypercoil.data.transforms import (
 from hypercoil.data.wds import torch_wds
 
 
-max_epoch = 2501
+max_epoch = 1001
 log_interval = 25
-data_interval = 10
-lr = 0.1
-batch_size = 10
-buffer_size = 25
+data_interval = 5
+lr = 0.05
+batch_size = 3
+buffer_size = 10
+device = 'cuda:0'
 atlas_path = '/mnt/pulsar/Downloads/atlases/glasser.nii'
-data_dir = '/mnt/andromeda/Data/HCP_wds/spl-{000..003}_shd-{000000..000003}.tar'
+data_dir = '/mnt/andromeda/Data/HCP_wds/spl-{000..007}_shd-{000000..000004}.tar'
 modal_cmap = pkgrf(
     'hypercoil',
     'viz/resources/cmap_modal.nii'
@@ -63,86 +69,7 @@ network_cmap = pkgrf(
 )
 
 
-dirichlet = DirichletInitSurfaceAtlas(
-    cifti_template=atlas_path,
-    mask_L=tflow.get(
-        template='fsLR',
-        hemi='L',
-        desc='nomedialwall',
-        density='32k'),
-    mask_R=tflow.get(
-        template='fsLR',
-        hemi='R',
-        desc='nomedialwall',
-        density='32k'),
-    compartment_labels={
-        'cortex_L': 200,
-        'cortex_R': 200,
-        'subcortex': 0
-    },
-    conc=100.,
-    dtype=torch.float,
-    device='cuda:0'
-)
-atlas = AtlasLinear(dirichlet, mask_input=False,
-                    domain=dirichlet.init['_all'].domain,
-                    dtype=torch.float, device='cuda:0')
-
-lh_coor = atlas.coors[dirichlet.compartments['cortex_L'][atlas.mask]].t()
-rh_coor = atlas.coors[dirichlet.compartments['cortex_R'][atlas.mask]].t()
-
-lh_mask = dirichlet.compartments['cortex_L'][atlas.mask].unsqueeze(0)
-rh_mask = dirichlet.compartments['cortex_R'][atlas.mask].unsqueeze(0)
-
-opt = torch.optim.Adam(params=atlas.parameters(), lr=lr)
-loss = LossScheme((
-    LossScheme((
-        Entropy(nu=0.0005, name='LeftHemisphereEntropy'),
-        Equilibrium(nu=500, name='LeftHemisphereEquilibrium'),
-        Compactness(nu=0.5, radius=100, coor=lh_coor,
-                    name='LeftHemisphereCompactness')
-    ), apply=lambda arg: arg.lh),
-    LossScheme((
-        Entropy(nu=0.0005, name='RightHemisphereEntropy'),
-        Equilibrium(nu=500, name='RightHemisphereEquilibrium'),
-        Compactness(nu=0.5, radius=100, coor=rh_coor,
-                    name='RightHemisphereCompactness')
-    ), apply=lambda arg: arg.rh),
-    LossApply(
-        HemisphericTether(nu=0.2, name='InterHemisphericTether'),
-        apply=lambda arg: UnpackingLossArgument(
-            lh=arg.lh,
-            rh=arg.rh,
-            lh_coor=arg.lh_coor,
-            rh_coor=arg.rh_coor)
-    ),
-    LossApply(
-        VectorDispersion(nu=5, metric=spherical_geodesic,
-                         name='LeftHemisphereDispersion'),
-        apply=lambda arg: cmass_coor(arg.lh, arg.lh_coor, 100).t()),
-    LossApply(
-        VectorDispersion(nu=5, metric=spherical_geodesic,
-                         name='RightHemisphereDispersion'),
-        apply=lambda arg: cmass_coor(arg.rh, arg.rh_coor, 100).t()),
-    LossApply(
-        LogDetCorr(nu=.01, psi=0.01, xi=0.01),
-        apply=lambda arg: arg.ts)
-))
-terminal_L = ReactiveTerminal(
-    loss=SecondMoment(nu=2500, standardise=False,
-                      name='LeftHemisphereSecondMoment'),
-    slice_target='data',
-    slice_axis=-1,
-    max_slice=10,
-    pretransforms={'weight': atlas.domain.image}
-)
-terminal_R = ReactiveTerminal(
-    loss=SecondMoment(nu=2500, name='RightHemisphereSecondMoment'),
-    slice_target='data',
-    slice_axis=-1,
-    max_slice=10,
-    pretransforms={'weight': atlas.domain.image}
-)
+epochs = Epochs(max_epoch)
 
 
 wndw = partial(
@@ -171,22 +98,130 @@ polybasis = torch.stack([
 ])
 
 
-for epoch in range(max_epoch):
+dirichlet = DirichletInitSurfaceAtlas(
+    cifti_template=atlas_path,
+    mask_L=tflow.get(
+        template='fsLR',
+        hemi='L',
+        desc='nomedialwall',
+        density='32k'),
+    mask_R=tflow.get(
+        template='fsLR',
+        hemi='R',
+        desc='nomedialwall',
+        density='32k'),
+    compartment_labels={
+        'cortex_L': 200,
+        'cortex_R': 200,
+        'subcortex': 0
+    },
+    conc=100.,
+    dtype=torch.float,
+    device=device
+)
+atlas = AtlasLinear(dirichlet, mask_input=False,
+                    domain=dirichlet.init['_all'].domain,
+                    dtype=torch.float, device=device)
+
+lh_coor = atlas.coors[dirichlet.compartments['cortex_L'][atlas.mask]].t()
+rh_coor = atlas.coors[dirichlet.compartments['cortex_R'][atlas.mask]].t()
+
+lh_mask = dirichlet.compartments['cortex_L'][atlas.mask].unsqueeze(0)
+rh_mask = dirichlet.compartments['cortex_R'][atlas.mask].unsqueeze(0)
+
+dispersion_nu = MultiplierSigmoidSchedule(
+    epochs=epochs, base=10,
+    transitions={(80, 120): 2, (200, 300): 0.5, (400, 500): 2, (600, 700): 0.5}
+)
+determinant_nu = MultiplierSigmoidSchedule(
+    epochs=epochs, base=.005,
+    transitions={(80, 120): .001, (200, 400): .0001}
+)
+equilibrium_nu = MultiplierSigmoidSchedule(
+    epochs=epochs, base=1e7,
+    transitions={(200, 300): 25000, (500, 700): 1e7}
+)
+entropy_nu = MultiplierSigmoidSchedule(
+    epochs=epochs, base=0.1,
+    transitions={(100, 200): 0.0001, (500, 800): 0.1}
+)
+compactness_nu = MultiplierSigmoidSchedule(
+    epochs=epochs, base=2,
+    transitions={(80, 120): 5, (400, 800): 20}
+)
+
+opt = torch.optim.Adam(params=atlas.parameters(), lr=lr)
+loss = LossScheme((
+    LossScheme((
+        Entropy(nu=entropy_nu, name='LeftHemisphereEntropy'),
+        Equilibrium(nu=equilibrium_nu, name='LeftHemisphereEquilibrium'),
+        Compactness(nu=compactness_nu, radius=100, coor=lh_coor,
+                    name='LeftHemisphereCompactness')
+    ), apply=lambda arg: arg.lh),
+    LossScheme((
+        Entropy(nu=entropy_nu, name='RightHemisphereEntropy'),
+        Equilibrium(nu=equilibrium_nu, name='RightHemisphereEquilibrium'),
+        Compactness(nu=compactness_nu, radius=100, coor=rh_coor,
+                    name='RightHemisphereCompactness')
+    ), apply=lambda arg: arg.rh),
+    LossApply(
+        HemisphericTether(nu=0.2, name='InterHemisphericTether'),
+        apply=lambda arg: UnpackingLossArgument(
+            lh=arg.lh,
+            rh=arg.rh,
+            lh_coor=arg.lh_coor,
+            rh_coor=arg.rh_coor)
+    ),
+    LossApply(
+        VectorDispersion(nu=dispersion_nu, metric=spherical_geodesic,
+                         name='LeftHemisphereDispersion'),
+        apply=lambda arg: cmass_coor(arg.lh, arg.lh_coor, 100).t()),
+    LossApply(
+        VectorDispersion(nu=dispersion_nu, metric=spherical_geodesic,
+                         name='RightHemisphereDispersion'),
+        apply=lambda arg: cmass_coor(arg.rh, arg.rh_coor, 100).t()),
+    LossApply(
+        LogDetCorr(nu=determinant_nu, psi=0.01, xi=0.01),
+        apply=lambda arg: arg.ts)
+))
+terminal_L = ReactiveTerminal(
+    loss=SecondMoment(nu=5, standardise=False,
+                      skip_normalise=True,
+                      name='LeftHemisphereSecondMoment'),
+    slice_target='data',
+    slice_axis=-1,
+    max_slice=10,
+    pretransforms={'weight': atlas.domain.image}
+)
+terminal_R = ReactiveTerminal(
+    loss=SecondMoment(nu=5, standardise=False,
+                      skip_normalise=True,
+                      name='RightHemisphereSecondMoment'),
+    slice_target='data',
+    slice_axis=-1,
+    max_slice=10,
+    pretransforms={'weight': atlas.domain.image}
+)
+loss_tape = LossArchive(epochs=epochs)
+loss.register_sentry(loss_tape)
+
+
+for epoch in epochs:
     if epoch % data_interval == 0:
         for sample in ds:
-            rest_mask = sample[-1][:, -3]
-            print(f'Searching for suitable sample: {rest_mask.sum()} found')
-            if rest_mask.sum() >= 1 and rest_mask.sum() <= 4:
-                X = sample[0][rest_mask.bool()][:, :atlas.mask.sum(), :]
-                gs = X.mean(-2)
-                regs = torch.cat((
-                    polybasis.tile(X.shape[0], 1, 1),
-                    gs.unsqueeze(-2)
-                ), -2).to('cuda')
-                X = X.to('cuda')
-                data = residualise(X, regs, driver='gels')
-                data = normalise(data)
-                break
+            X = sample[0][:, :atlas.mask.sum(), :].float()
+            gs = X.mean(-2)
+            regs = torch.cat((
+                polybasis.tile(X.shape[0], 1, 1),
+                gs.unsqueeze(-2)
+            ), -2).to('cuda')
+            X = X.to('cuda')
+            data = residualise(X, regs, driver='gels')
+            data = normalise(data)
+            break
+
+    if epoch == 500:
+        opt.param_groups[0]['lr'] /= 2.5
 
     # sometimes, torch is really stupid
     shape_L = (data.shape[0], lh_mask.sum(), -1)
@@ -205,10 +240,10 @@ for epoch in range(max_epoch):
     arg_L = {'data': data_L, 'weight': atlas.preweight['cortex_L']}
     arg_R = {'data': data_R, 'weight': atlas.preweight['cortex_R']}
     out = terminal_L(arg=arg_L)
-    print(f'{terminal_L.loss} : {out}')
+    print(f'- {terminal_L.loss} : {out}')
     #if (epoch % log_interval == 0): print(f'{terminal_L.loss} : {out}')
     out = terminal_R(arg=arg_R)
-    print(f'{terminal_R.loss} : {out}')
+    print(f'- {terminal_R.loss} : {out}')
     #if (epoch % log_interval == 0): print(f'{terminal_R.loss} : {out}')
     #out = loss(arg, verbose=(epoch % log_interval == 0))
     out = loss(arg, verbose=True)
@@ -227,7 +262,7 @@ for epoch in range(max_epoch):
             save=f'{results}/epoch-{epoch:08}'
         )
 
-
+#"""
 all_views = (
     'dorsal', 'ventral',
     'posterior', 'anterior',
@@ -245,5 +280,6 @@ plotter(
     views=all_views,
     save=f'{results}/parcellation_cmap-network'
 )
+#"""
 #plotter = fsLRAtlasMaps(atlas)
 #plotter(save=f'{results}/parcellation_maps')
