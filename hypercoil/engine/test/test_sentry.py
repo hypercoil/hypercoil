e9a44197920208ccbe9a98b647d506ec303ef19d
Rastko Ciric <rastko@stanford.edu>
2022-04-09 16:16:39 -0700

    (TEST) linear transition scheduler test

--- hypercoil/engine/test/test_sentry.py
+++ hypercoil/engine/test/test_sentry.py
@@ -9,8 +9,9 @@ import torch
 import numpy as np
 from hypercoil.engine import (
     Epochs,
-    MultiplierSchedule,
+    MultiplierTransformSchedule,
     MultiplierRecursiveSchedule,
+    MultiplierLinearSchedule,
     MultiplierSigmoidSchedule,
     LossArchive
 )
@@ -37,7 +38,7 @@ class TestSentry:
 
         max_epoch = 100
         epochs = Epochs(max_epoch)
-        schedule0 = MultiplierSchedule(
+        schedule0 = MultiplierTransformSchedule(
             epochs=epochs,
             transform=lambda e: 1.01 ** e
         )
@@ -51,15 +52,25 @@ class TestSentry:
             transitions={(21, 40): 5, (61, 80): 2},
             base=1
         )
+        schedule3 = MultiplierLinearSchedule(
+            epochs=epochs,
+            transitions={(10, 30): 5,
+                         (40, 50): 3,
+                         (55, 60): -1,
+                         (75, 90): 11},
+            base=1
+        )
         bad_transmitter = IncompleteTransmit()
         schedule0.register_action(bad_transmitter)
         archive = LossArchive(epochs)
         loss0 = SoftmaxEntropy(nu=schedule0, name='loss0')
         loss1 = SoftmaxEntropy(nu=schedule1, name='loss1')
         loss2 = SoftmaxEntropy(nu=schedule2, name='loss2')
+        loss3 = SoftmaxEntropy(nu=schedule3, name='loss3')
         loss0.register_sentry(archive)
         loss1.register_sentry(archive)
         loss2.register_sentry(archive)
+        loss3.register_sentry(archive)
         schedule0.register_sentry(archive) # test ignoring irrelevant messages
         Z = torch.rand(10)
         reset = False
@@ -68,6 +79,7 @@ class TestSentry:
             loss0(Z)
             loss1(Z)
             loss2(Z)
+            loss3(Z)
             if e == 95 and not reset:
                 epochs.reset()
                 schedule1.reset()
@@ -90,3 +102,13 @@ class TestSentry:
         assert np.isclose(archive.archive['loss2'][60], 5 * orig)
         assert np.isclose(archive.archive['loss2'][70], 3.5 * orig)
         assert np.isclose(archive.archive['loss2'][80], 2 * orig)
+
+        orig = archive.get('loss3')[0]
+        assert np.isclose(archive.archive['loss3'][10], 1 * orig)
+        assert np.isclose(archive.archive['loss3'][20], 3 * orig)
+        assert np.isclose(archive.archive['loss3'][40], 5 * orig)
+        assert np.isclose(archive.archive['loss3'][50], 3 * orig)
+        assert np.isclose(archive.archive['loss3'][60], -1 * orig)
+        assert np.isclose(archive.archive['loss3'][70], -1 * orig)
+        assert np.isclose(archive.archive['loss3'][80], 3 * orig)
+        assert np.isclose(archive.archive['loss3'][90], 11 * orig)
