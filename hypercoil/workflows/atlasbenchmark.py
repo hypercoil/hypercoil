9cbf6e3d698c6d93a8d002ecc746b2f65ce866c1
rciric <rastko@stanford.edu>
2022-04-08 19:52:29 -0700

    (REF) refactor atlas benchmarking workflow for CLI compatibility

--- hypercoil/workflows/atlasbenchmark.py
+++ hypercoil/workflows/atlasbenchmark.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
@@ -6,6 +7,7 @@ Atlas benchmarks
 ~~~~~~~~~~~~~~~~
 Benchmark parcellations.
 """
+import click
 import torch
 import pathlib
 import numpy as np
@@ -24,64 +26,106 @@ from hypercoil.data.transforms import (
 from hypercoil.data.wds import torch_wds
 
 
-atlas_root = '/mnt/pulsar/Data/atlases/spatialnulls'
-data_dir = '/mnt/andromeda/Data/HCP_wds/spl-008_shd-{000000..000004}.tar'
-batch_size = 1
-buffer_size = 1
-atlas_dim = 59412
-device = 'cuda:1'
-
-
-normalise = Normalise()
-maps = {
-    'images' : identity,
-    'confounds' : identity,
-    'tmask' : identity,
-    't_r' : identity,
-    'task' : identity,
-}
-ds = torch_wds(
+def run_benchmark(
+    atlas_list,
     data_dir,
-    keys=maps,
-    batch_size=batch_size,
-    shuffle=buffer_size,
-    map=identity
-)
-polybasis = torch.stack([
-    torch.arange(1200, device=device) ** i
-    for i in range(3)
-])
+    batch_size=1,
+    buffer_size=1,
+    atlas_dim=59412,
+    verbose=False,
+    device='cuda:0',
+    lstsq_driver='gels',
+    compute_full_matrix=False
+):
 
-mask = torch.zeros(91282, dtype=torch.bool, device=device)
-mask[:atlas_dim] = 1
-eval = AtlasEval(
-    mask=mask
-)
-
-print('[ Loading parcellations ]')
-parcellations = list(pathlib.Path(atlas_root).glob(
-    'parcellation*/parcellation*.nii'))
-parcellations.sort()
-for i, atlas in enumerate(parcellations):
-    eval.add_voxel_assignment(
-        name=f'labels-200_iter-{i:06}_null',
-        asgt=torch.tensor(
-            nb.load(atlas).get_fdata(),
-            dtype=torch.long,
-            device=device
-        )
+    normalise = Normalise()
+    maps = {
+        'images' : identity,
+        'confounds' : identity,
+        'tmask' : identity,
+        't_r' : identity,
+        'task' : identity,
+    }
+    ds = torch_wds(
+        data_dir,
+        keys=maps,
+        batch_size=batch_size,
+        shuffle=buffer_size,
+        map=identity
     )
-    break
+    polybasis = torch.stack([
+        torch.arange(1200, device=device) ** i
+        for i in range(3)
+    ])
 
-for s, sample in enumerate(ds):
-    print(f'[ Preparing next sample {s} ]')
-    X = sample[0].squeeze().to(device=device, dtype=torch.float)
-    if X.shape[-1] != 1200:
-        print(f'[ Incorrect time dim: {X.shape[-1]}. Skipping ]')
-        continue
-    gs = X.mean(-2, keepdim=True)
-    regs = torch.cat((polybasis, gs), -2)
-    data = residualise(X, regs, driver='gels')
-    data = normalise(data)
+    mask = torch.zeros(91282, dtype=torch.bool, device=device)
+    mask[:atlas_dim] = 1
+    eval = AtlasEval(
+        mask=mask,
+        lstsq_driver=lstsq_driver
+    )
 
-    break
+    print('[ Loading parcellations ]')
+    parcellations = list(atlas_list)
+    parcellations.sort()
+    for i, atlas in enumerate(parcellations):
+        eval.add_voxel_assignment(
+            name=atlas,
+            asgt=torch.tensor(
+                nb.load(atlas).get_fdata(),
+                dtype=torch.long,
+                device=device
+            )
+        )
+        break
+
+    for s, sample in enumerate(ds):
+        print(f'[ Preparing next sample {s} ]')
+        X = sample[0].squeeze().to(device=device, dtype=torch.float)
+        if X.shape[-1] != 1200:
+            print(f'[ Incorrect time dim: {X.shape[-1]}. Skipping ]')
+            continue
+        gs = X.mean(-2, keepdim=True)
+        regs = torch.cat((polybasis, gs), -2)
+        data = residualise(X, regs, driver=lstsq_driver)
+        data = normalise(data)
+
+        eval.evaluate(
+            data,
+            verbose=verbose,
+            compute_full_matrix=compute_full_matrix
+        )
+
+        break
+    return eval
+
+
+@click.command()
+@click.option('-a', '--atlas', 'atlas_list', multiple=True, required=True)
+@click.option('-d', '--data', 'data_dir', required=True, type=str)
+@click.option('-b', '--batch-size', default=1, type=int)
+@click.option('-s', '--buffer-size', default=1, type=int)
+@click.option('-l', '--atlas-dim', default=59412, type=int)
+@click.option('-v', '--verbose', default=False, type=bool, is_flag=True)
+@click.option('-c', '--device', default='cuda:0', type=str)
+@click.option('--compute-full-matrix', default=False, type=bool)
+@click.option('--lstsq-driver', default='gels',
+              type=click.Choice(['gels', 'gelsy', 'gelss', 'gelsd']))
+def main(atlas_list, data_dir, batch_size, buffer_size,
+         atlas_dim, verbose, device, compute_full_matrix, lstsq_driver):
+    eval = run_benchmark(
+        atlas_list=atlas_list,
+        data_dir=data_dir,
+        batch_size=batch_size,
+        buffer_size=buffer_size,
+        atlas_dim=atlas_dim,
+        verbose=verbose,
+        device=device,
+        lstsq_driver=lstsq_driver,
+        compute_full_matrix=compute_full_matrix
+    )
+    print(eval.results)
+
+
+if __name__ == '__main__':
+    main()
