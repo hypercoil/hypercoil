7b82f78597f4e7adbacf8940c11f1e78bdda96da
rciric <rastko@stanford.edu>
2022-04-20 19:51:37 -0700

    (ENH) second prototype conveyance chain

--- hypercoil/engine/conveyance.py
+++ hypercoil/engine/conveyance.py
@@ -9,10 +9,15 @@ Abstractions representing transport through a model.
 import torch
 from . import (
     SentryModule,
+    SentryMessage,
     ModelArgument,
     UnpackingModelArgument
 )
-from .actions import Convey
+from .action import (
+    Convey,
+    CountBatches,
+    BatchRelease
+)
 
 
 def transport(*pparams, **params):
@@ -20,17 +25,30 @@ def transport(*pparams, **params):
 
 
 class BaseConveyance(SentryModule):
-    def __init__(self, lines=None):
+    def __init__(self, lines=None, line_filters=None):
         super().__init__()
+        if isinstance(lines, str): lines = [lines]
         if lines is None: lines = [(None, None)]
+        if line_filters is None: line_filters = {}
         lines = [l if isinstance(l, tuple) else (l, l) for l in lines]
         self.lines = lines
+        self.receive = set()
+        self.transmit = set()
+        self.transmit_map = {}
+        self.data_transmission = SentryMessage()
+        self.line_filters = line_filters
         for (receive, transmit) in self.lines:
             convey = Convey(
                 receive_line=receive,
                 transmit_line=transmit
             )
             self.register_action(convey)
+            self.receive = self.receive.union({receive})
+            self.transmit = self.transmit.union({transmit})
+            if self.transmit_map.get(receive) is None:
+                self.transmit_map[receive] = [transmit]
+            else:
+                self.transmit_map[receive] += [transmit]
 
     def connect_downstream(self, conveyance):
         self.register_sentry(conveyance)
@@ -38,18 +56,33 @@ class BaseConveyance(SentryModule):
     def connect_upstream(self, conveyance):
         conveyance.register_sentry(self)
 
-    def _transmit(self, data, line):
+    def _filter_line(self, data, line):
+        if self.line_filters.get(line) is None:
+            return data
+        arg = type(data)
+        mapped = {k: v for k, v in data.items()
+                  if k in self.line_filters.get(line)}
+        return arg(**mapped)
+
+    def _update_transmission(self, data, line):
+        self.data_transmission.update(
+            (line, self._filter_line(data, line))
+        )
+
+    def _transmit(self):
         self.message.update(
-            ('DATA', {line : data})
+            ('DATA', self.data_transmission)
         )
         for s in self.listeners:
             s._listen(self.message)
+        self.data_transmission.clear()
         self.message.clear()
 
 
 class Conveyance(BaseConveyance):
-    def __init__(self, influx, efflux, model=None, lines=None):
-        super().__init__(lines)
+    def __init__(self, influx, efflux, model=None,
+                 lines=None, line_filters=None):
+        super().__init__(lines=lines, line_filters=line_filters)
         if model is None:
             model = transport
         self.model = model
@@ -62,19 +95,30 @@ class Conveyance(BaseConveyance):
             output = self.model(**input)
         else:
             output = self.model(input)
-        output = self.efflux(output)
-        self._transmit(output, line)
+        output = self.efflux(output, arg)
+        for transmit_line in self.transmit_map[line]:
+            self._update_transmission(output, transmit_line)
+        self._transmit()
         return output
 
 
 class Origin(BaseConveyance):
-    def __init__(self, pipeline, lines=None, **params):
-        super().__init__(lines)
+    def __init__(self, pipeline, lines=None, line_filters=None, **params):
+        if isinstance(lines, str): lines = [lines]
+        if lines is None: lines = [None]
+        lines = [('source', line) for line in lines]
+        super().__init__(lines=lines, line_filters=line_filters)
         self.pipeline = pipeline
         self.loader = torch.utils.data.DataLoader(
             self.pipeline, **params)
+        self.iter_loader = iter(self.loader)
 
     def forward(self, arg=None, line=None):
-        data = ModelArgument(**next(iter(self.loader)))
-        self._transmit(data, line)
+        data = ModelArgument(**next(self.iter_loader))
+        self.message.update(
+            ('BATCH', self.loader.batch_size)
+        )
+        for transmit_line in self.transmit_map[line]:
+            self._update_transmission(data, transmit_line)
+        self._transmit()
         return data
