7559d8f7797481b13b763b03ddd036535b4d1fe5
rciric <rastko@stanford.edu>
2022-05-05 14:36:45 -0700

    (PROTO) minimal model selection scaling workflow sketch

--- hypercoil/workflows/selectminimal.py
+++ hypercoil/workflows/selectminimal.py
@@ -6,12 +6,20 @@ from functools import partial
 from hypercoil.data.transforms import Normalise
 from hypercoil.data.functional import window_map
 from hypercoil.data.collate import gen_collate, extend_and_bind
-from hypercoil.engine import Epochs, LossArchive
+from hypercoil.engine import (
+    Epochs,
+    LossArchive,
+    ModelArgument,
+    UnpackingModelArgument
+)
 from hypercoil.functional import conditionalcorr, sym2vec
 from hypercoil.functional.domainbase import Identity
 from hypercoil.functional.utils import conform_mask
 from hypercoil.init.freqfilter import FreqFilterSpec
-from hypercoil.loss import ReducingLoss, QCFC
+from hypercoil.loss import (
+    ReducingLoss, QCFC, SoftmaxEntropy, LossScheme, LossApply
+)
+from hypercoil.loss.batchcorr import qcfc_loss
 from hypercoil.nn.interpolate import HybridInterpolate
 from hypercoil.nn.freqfilter import FrequencyDomainFilter
 from hypercoil.nn.select import (
@@ -25,6 +33,7 @@ batch_size = 100
 batch_size_val = 100
 max_epoch = 1000
 window_size = 200
+n_response_functions = 5
 n_ts = 400
 lr = 0.005
 wd = 1e-3
@@ -35,7 +44,7 @@ confounds_path = '/tmp/confounds/sub-{}_ses-{}_run-{}.pt'
 confounds_path_orig = '/mnt/andromeda/Data/HCP_S1200/data/{}/MNINonLinear/Results/rfMRI_REST{}_{}/Confound_Regressors.tsv'
 dtype = torch.float
 save_dev = 'cpu'
-device = 'cpu'
+device = 'cuda:0'
 cols = ['framewise_displacement', 'std_dvars']
 
 
@@ -124,7 +133,8 @@ fft_init = FreqFilterSpec(
 bpfft = FrequencyDomainFilter(
     filter_specs=[fft_init],
     time_dim=window_size,
-    domain=Identity()
+    domain=Identity(),
+    device=device
 )
 bpfft.preweight.requires_grad = False
 
@@ -132,22 +142,36 @@ selector = ResponseFunctionLinearSelector(
     model_dim=model_dim,
     n_columns=57,
     leak=0.01,
-    n_response_functions=5
+    n_response_functions=n_response_functions,
+    softmax=False,
+    device=device
 )
 
-qcfc = QCFC(nu=1, tol=0.05, name='QC-FC')
+
+qcfc = QCFC(nu=1, name='QC-FC')
+entropy = SoftmaxEntropy(nu=10, name='Entropy')
+
+loss_train = LossScheme([
+    #LossApply(entropy, apply=(lambda arg: arg.weight)),
+    LossApply(
+        qcfc,
+        apply=(lambda arg: UnpackingModelArgument(FC=arg.fc, QC=arg.qc)))
+])
 
 
-qcfc_val = QCFC(nu=1, name='QC-FC_Val')
+qcfc_val = LossScheme([LossApply(
+    QCFC(nu=1, name='QC-FC_Val'),
+    apply=(lambda arg: UnpackingModelArgument(FC=arg.fc, QC=arg.qc))
+)])
 
 
 epochs = Epochs(max_epoch)
 loss_tape = LossArchive(epochs=epochs)
-qcfc.register_sentry(loss_tape)
+loss_train.register_sentry(loss_tape)
 qcfc_val.register_sentry(loss_tape)
 
 
-opt = torch.optim.Adam(lr=lr, params=selector.parameters())
+#opt = torch.optim.Adam(lr=lr, params=selector.parameters())
 
 opt = torch.optim.SGD(
     lr=lr, momentum=momentum, weight_decay=wd,
@@ -169,41 +193,57 @@ def model(bold, confs, mask):
     confs = interpol(confs.unsqueeze(1), mask).squeeze()
     confs = bpfft(confs).squeeze()
     confmodel = selector(confs)
-    return bold, confmodel
+    return bold, confmodel, confs
 
 
 def ingest_data(dl, wndw):
     data = next(dl)
     qc = get_confs(data['__key__'])[:, 0]
-    bold = data['images']
-    confounds = data['confounds']
+    bold = n(data['images'])
+    confounds = n(data['confounds'])
     nanmask = (torch.isnan(bold).sum(-2) == 0)
     bold[torch.isnan(bold)] = 0.001 * torch.randn_like(bold[torch.isnan(bold)])
-    confounds[torch.isnan(confounds)] = 0.001 * torch.randn_like(confounds[torch.isnan(confounds)])
+    confounds[torch.isnan(confounds)] = 0.001 * torch.randn_like(
+        confounds[torch.isnan(confounds)])
     qc[torch.isnan(qc)] = 0.001 * torch.randn_like(qc[torch.isnan(qc)])
 
     [bold, confounds, qc, nanmask] = wndw((bold, confounds, qc, nanmask))
-    return bold, confounds, qc, nanmask
+    return (
+        bold.to(device=device),
+        confounds.to(device=device),
+        qc.to(device=device),
+        nanmask.to(device=device)
+    )
 
 
 def model_forward(model, bold, confounds, mask, qc, lossfn):
-    bold, confmodel = model(bold, confounds, mask)
+    bold, confmodel, confs = model(bold, confounds, mask)
     cor = conditionalcorr(bold, confmodel)
-    loss = lossfn(sym2vec(cor), qc.nanmean(1).unsqueeze(0))
-    return loss, confmodel
+    fc = sym2vec(cor)
+    arg = ModelArgument(
+        fc=fc,
+        qc=qc.nanmean(1).unsqueeze(0),
+        weight=selector.weight['lin']
+    )
+    loss = lossfn(arg, verbose=True)
+    return loss, confmodel, bold, confs
 
 
 val_iter = iter(dl_val)
 for epoch in epochs:
     print(f'[ Epoch {epoch} ]')
     if epoch > 2:
-        print(loss_tape.archive['QC-FC'][-3:], loss_tape.archive['QC-FC_Val'][-3:])
+        print(loss_tape.archive['QC-FC'][-3:],
+              loss_tape.archive['QC-FC_Val'][-3:])
     dl_iter = iter(dl)
     selector.train()
-    qcfc.tol = 0.1 * torch.rand(1).item()
+    #qcfc.tol = 0.1 * torch.rand(1).item()
     for _ in range(10):
         bold, confounds, qc, nanmask = ingest_data(dl_iter, wndw)
-        loss, _ = model_forward(model, bold, confounds, nanmask.unsqueeze(1), qc, qcfc)
+        loss, _, _, _ = model_forward(
+            model, bold, confounds,
+            nanmask.unsqueeze(1),
+            qc, loss_train)
 
         if torch.any(torch.isnan(loss)):
             assert 0
@@ -212,12 +252,16 @@ for epoch in epochs:
         opt.step()
         opt.zero_grad()
 
-        print(f'[ Train loss : {loss.item()} ]')
 
-
+    refqcfc = 0
     with torch.no_grad():
         selector.eval()
         for _ in range(10):
             bold, confounds, qc, nanmask = ingest_data(val_iter, wndw)
-            loss, _ = model_forward(model, bold, confounds, nanmask.unsqueeze(1), qc, qcfc_val)
-            print(f'[ Validation loss : {loss.item()} ]')
+            loss, confmodel, bold, confs = model_forward(
+                model, bold, confounds,
+                nanmask.unsqueeze(1),
+                qc, qcfc_val)
+            reffc = sym2vec(conditionalcorr(bold, confs[:, 8].unsqueeze(1)))
+            refqcfc += qcfc_loss(QC=qc.nanmean(1).unsqueeze(0), FC=reffc).mean().item()
+        print(f'  (Reference : {refqcfc / 10})')
