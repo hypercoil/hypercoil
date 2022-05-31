51a689d31dc6babde8e16308e01cf1095d90e5a5
rciric <rastko@stanford.edu>
2022-05-31 10:51:54 -0700

    (FIX) sync working version of data sampler workflow

--- hypercoil/workflows/sampling.py
+++ hypercoil/workflows/sampling.py
@@ -33,18 +33,23 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+import os
 import torch
 import pathlib
 import pandas as pd
+import webdataset as wds
+import matplotlib.pyplot as plt
 from torch.nn import Module, Parameter
 from torch.autograd import grad
 from functools import partial
 from itertools import chain
 
+from hypercoil.data.collate import gen_collate, extend_and_bind
 from hypercoil.engine import Epochs
 from hypercoil.functional import corr, sym2vec
+from hypercoil.loss import UnilateralNormedLoss, ModularityLoss
 from hypercoil.nn.activation import CorrelationNorm
-from hypercoil.nn.sylo import SyloResNet
+from hypercoil.nn.sylo import SyloResNet, Sylo
 from hypercoil.nn.recombinator import QueryEncoder, Recombinator
 from hypercoil.nn.window import WindowAmplifier
 
@@ -57,50 +62,30 @@ device = 'cpu'
 n_subjects = 10
 n_tasks = 6
 
+data_dir = '/mnt/andromeda/Data/MSC_schaefer/{000000..000781}.tar'
+results = '/tmp/generative/'
+
 # architectural hyperparameters
-channel_sequence = (1, 12, 24, 36, 48, 60)
-generator_channel_sequence = (32, 32, 32, 24, 12, 32)
-block_sequence = (1, 1, 1, 1, 1)
-dim_sequence = (400, 200, 100, 50, 25, 10)
-lattice_order_sequence = (4, 3, 2, 2, 1)
+channel_sequence = (1, 12, 36, 60)
+generator_channel_sequence = (24, 12, 12, 32)
+block_sequence = (1, 1, 1)
+dim_sequence = (400, 100, 25, 10)
+lattice_order_sequence = (4, 2, 1)
 block_length = 2
-channel_multiplier = 4
+channel_multiplier = 2
 embedding_dim = 5 # dim per embedding
 noise_dim = 10
 
 # training hyperparameters
-lr = 0.005
+lr = 0.002
 window_size = 200
-max_epoch = 1000
+max_epoch = 5000
 batch_size = 10
-critic_steps_per_generator_step = 5
-
-
-tasks = {
-    'rest' : 0,
-    'motor' : 1,
-    'memoryscenes' : 2,
-    'memorywords' : 3,
-    'memoryfaces' : 4,
-    'glasslexical' : 5
-}
-data = []
-sub = []
-task = []
-
-paths = pathlib.Path(
-    '/Users/rastkociric/Downloads/xwave/data/MSC/ts/'
-).glob('*ses-func01*ts.1D')
-for path in paths:
-    sub += [int(str(path).split('/')[-1].split('_')[0].split('-')[-1][-2:]) - 1]
-    task += [tasks[str(path).split('/')[-1].split('_')[2].split('-')[-1]]]
-    data_cur = pd.read_csv(path, header=None, sep=' ').values.T
-    data_cur = corr(torch.tensor(data_cur, dtype=dtype, device=device))
-    data += [data_cur]
-    print(path, data_cur.shape)
-data = torch.stack(data)
-sub = torch.tensor(sub, device=device)
-task = torch.tensor(task, device=device)
+critic_steps_per_generator_step = 3
+positive_only_nu = 0.1
+community_nu = 0.01
+log_interval = 1
+log_interval = log_interval * (1 + critic_steps_per_generator_step)
 
 
 class Critic(Module):
@@ -117,13 +102,13 @@ class Critic(Module):
             n_lattices=1,
             channel_multiplier=channel_multiplier,
             norm_layer=torch.nn.InstanceNorm2d,
-            nlin=CorrelationNorm
+            nlin=partial(torch.nn.LeakyReLU, negative_slope=0.1) #None #CorrelationNorm
         )
         final_dim = dim_sequence[-1]
         d0, d1 = channel_sequence[-1], final_dim * (final_dim - 1) // 2
         #TODO: use real init . . .
-        W_L = torch.randn(d0, 1, dtype=data.dtype, device=data.device) / d0
-        W_R = torch.randn(d1, 1, dtype=data.dtype, device=data.device) / d1
+        W_L = torch.randn(d0, 1, dtype=dtype, device=device) / d0
+        W_R = torch.randn(d1, 1, dtype=dtype, device=device) / d1
         self.W_L = Parameter(W_L)
         self.W_R = Parameter(W_R)
 
@@ -149,15 +134,15 @@ class Encoder(Module):
             n_lattices=1,
             channel_multiplier=channel_multiplier,
             norm_layer=torch.nn.InstanceNorm2d,
-            nlin=CorrelationNorm
+            nlin=partial(torch.nn.LeakyReLU, negative_slope=0.1) #None #CorrelationNorm
         )
         final_dim = dim_sequence[-1]
         d0, d1 = channel_sequence[-1], final_dim * (final_dim - 1) // 2
-        W_L = torch.randn(d0, 5, dtype=data.dtype, device=data.device) / d0
-        W_R = torch.randn(d1, 4, dtype=data.dtype, device=data.device) / d1
+        W_L = torch.randn(d0, 5, dtype=dtype, device=device) / d0
+        W_R = torch.randn(d1, 4, dtype=dtype, device=device) / d1
         self.W_L = Parameter(W_L)
         self.W_R = Parameter(W_R)
-        
+
     def set_potentials(self, critic):
         for i, layer in enumerate(self.model.model.layers):
             c_E = layer[0].compression
@@ -167,14 +152,14 @@ class Encoder(Module):
                 c_E.mask[:] = c_C.mask.clone()
                 c_E.sign[:] = c_C.sign.clone()
                 c_E.sparsity = c_C.sparsity
-                c_E.initialised = True    
+                c_E.initialised = True
 
     def forward(self, x, query=None):
         batch_size = x.size(0)
         z_hat = self.model(x.unsqueeze(1), query=query)
         z_hat = self.W_L.t() @ sym2vec(z_hat) @ self.W_R
         return z_hat.view(batch_size, -1)
-    
+
 
 class Generator(Module):
     def __init__(self):
@@ -190,13 +175,13 @@ class Generator(Module):
             n_lattices=1,
             channel_multiplier=channel_multiplier,
             norm_layer=torch.nn.InstanceNorm2d,
-            nlin=CorrelationNorm
+            nlin=partial(torch.nn.LeakyReLU, negative_slope=0.1) #None #CorrelationNorm
         )
         self.final = Recombinator(
             in_channels=generator_channel_sequence[-1],
             out_channels=1
         )
-        
+
     def set_potentials(self, critic):
         for i, layer in enumerate(critic.model.model.layers):
             c_G = self.model.model.layers[-(i + 1)][0].compression
@@ -207,7 +192,7 @@ class Generator(Module):
                 c_G.sign[:] = c_C.sign.clone()
                 c_G.sparsity = c_C.sparsity
                 c_G.initialised = True
-        
+
     def forward(self, z, query=None):
         batch_size = z.size(0)
         in_dim = dim_sequence[-1]
@@ -337,8 +322,8 @@ class QueryBiGAN(Module):
         interpolate_x, interpolate_z = self._grad_penalty_interpolate(
             n=batch_size, x=x, z_hat=z_hat, x_tilde=x_tilde, z=z
         )
-        print(x.shape, z.shape, x_tilde.shape, z_hat.shape)
-        print(interpolate_x.shape, interpolate_z.shape)
+        #print(x.shape, z.shape, x_tilde.shape, z_hat.shape)
+        #print(interpolate_x.shape, interpolate_z.shape)
         if self.qnet is not None:
             interpolate_z_enc, _ = self.qnet.encode(interpolate_z)
             critic_scalar = self.critic(interpolate_x, interpolate_z_enc).sum()
@@ -352,9 +337,9 @@ class QueryBiGAN(Module):
             grad_x.view(batch_size, -1),
             grad_z.view(batch_size, -1)
         ), dim=-1)
-        print(grads)
-        return ((grads.norm(2, dim=-1) - 1) ** 2).mean()
-    
+        #print(grads)
+        return ((grads.norm(2, dim=-1) - 1) ** 2).mean().abs()
+
     def forward(self, x, z, query_E=None, query_G=None):
         z_hat = self.encode(x, query=query_E)
         x_tilde = self.generate(z, query=query_G)
@@ -368,7 +353,7 @@ class QueryBiGAN(Module):
             x_tilde=x_tilde.data,
             z=z.data
         )
-        print(GP_loss)
+        #print(GP_loss)
         C_loss = -EG_loss + GP_loss
         return C_loss, EG_loss, (x, z_hat), (x_tilde, z)
 
@@ -397,18 +382,74 @@ class ConnectomeQueryBiGAN(QueryBiGAN):
         )
 
 
-optimizer_EG = torch.optim.Adam(
-    params=(
-        list(model.encoder.parameters()) +
-        list(model.generator.parameters()) +
-        list(model.qnet.parameters())
-    ), 
-    lr=lr)
-optimizer_C = torch.optim.Adam(
-    params=model.critic.parameters(),
-    lr=lr)
+def correlate(data):
+    ts = data.to(device=device)
+    nanmask = torch.isnan(ts)
+    ts[nanmask] = 0
+    nanmask = nanmask.sum(-2).to(dtype=torch.bool)
+    return corr(ts, weight=~nanmask)
+
+
+def regularisation(network, x=None):
+    nonnegative_loss = 0
+    for m in network.modules():
+        if isinstance(m, Sylo):
+            nonnegative_loss += positive_only(-m.weight_L)
+    if x is None:
+        modularity_loss = 0
+    else:
+        C = torch.softmax(
+            network.sylo1['community'].weight_L.squeeze().t(), -1
+        )
+        modularity_loss = community_loss(A=x, C=C)
+    return nonnegative_loss, modularity_loss
+
+
+def wndw_map(dict, keys, window):
+    try:
+        return {
+            k : (window(v)[0] if k in keys else v)
+            for k, v in dict.items()
+        }
+    except ValueError:
+        return dict
+
+
+def unsqueeze(dict, keys):
+    return {k : v.unsqueeze(0) if k in keys else v for k, v in dict.items()}
+
+def remove(dict):
+    del dict['__key__']
+    return dict
+
+os.makedirs(results, exist_ok=True)
+collate = partial(gen_collate, concat=extend_and_bind)
 window = WindowAmplifier(window_size=window_size)
-epochs = Epochs(max_epoch)
+wndw_map = partial(wndw_map, keys='bold', window=window)
+map2 = partial(unsqueeze, keys=('task', 'sub'))
+dpl = wds.DataPipeline(
+    wds.SimpleShardList(data_dir),
+    wds.shuffle(100),
+    wds.tarfile_to_samples(1),
+    wds.decode(lambda x, y: wds.torch_loads(y)),
+    wds.shuffle(batch_size),
+    wds.map(wndw_map),
+    wds.map(map2),
+)
+dl = torch.utils.data.DataLoader(
+    dpl, num_workers=1,
+    batch_size=batch_size,
+    collate_fn=collate)
+dl_init = torch.utils.data.DataLoader(
+    dpl, num_workers=1,
+    batch_size=100,
+    collate_fn=collate
+)
+with torch.no_grad():
+    data_init = next(iter(dl_init))
+    potentials = correlate(data_init['bold']).mean(0)
+    del data_init
+    del dl_init
 
 
 model = ConnectomeQueryBiGAN(
@@ -417,8 +458,28 @@ model = ConnectomeQueryBiGAN(
     critic=Critic(),
     qnet=QueryNet()
 )
-model.set_potentials(data.mean(0))
+model.set_potentials(potentials.cpu())
 model.to(device)
+
+positive_only = UnilateralNormedLoss(nu=positive_only_nu)
+community_loss = ModularityLoss(nu=community_nu)
+
+
+optimizer_EG = torch.optim.Adam(
+    params=(
+        list(model.encoder.parameters()) +
+        list(model.generator.parameters()) +
+        list(model.qnet.parameters())
+    ),
+    lr=lr)
+optimizer_C = torch.optim.Adam(
+    params=model.critic.parameters(),
+    lr=lr)
+epochs = Epochs(max_epoch)
+
+
+noise = torch.randn(8, 2 * embedding_dim + noise_dim)
+vidx = 8 *(dim_sequence[0] ** 2) // 10
 
 
 C_update, EG_update = True, False
@@ -426,39 +487,78 @@ C_steps = 0
 
 C_losses, EG_losses = [], []
 
+cur_step = 0
 for epoch in epochs:
-    for data in dl:
-        ts = data['bold'].to(device=device)
-        subject_query = data['subject'].to(device=device)
-        task_query = data['task'].to(device=device)
-        # TODO: read data and apply window
-        # TODO: get nanmask and set nans to 0
-        data = corr(ts, weight=nanmask)
+    dl_iter = iter(dl)
+    for data in dl_iter:
+        print(f'[ Epoch {epoch} | Step {cur_step} ]')
+        x = correlate(data['bold'])
+        subject_query = data['sub'].to(device=device, dtype=torch.long)
+        task_query = data['task'].to(device=device, dtype=torch.long)
+        if cur_step == 0:
+            exemplars = {
+                'x' : x[:8],
+                'subject_query' : subject_query[:8],
+                'task_query' : task_query[:8]
+            }
+        #print(data['__key__'])
 
-        x, subject_query, task_query = data[:4], sub[:4], task[:4]
         C_loss, EG_loss, (x, z_hat), (x_tilde, z) = model(
             x, subject_query, task_query
         )
-        print(f'- [ Wasserstein loss | {EG_loss} ] ')
-        print(f'- [ Critic loss with gradient penalty | {C_loss} ] ')
         if C_update:
+            print('- [ Training critic ]')
             optimizer_C.zero_grad()
             C_loss.backward()
+            nonneg, mod = regularisation(model.critic.model.model, x=x)
+            nonneg.backward()
+            mod.backward()
             C_losses += [C_loss.detach().item()]
+            optimizer_C.step()
             C_steps += 1
-            if C_steps > critic_steps_per_generator_step:
+            if C_steps >= critic_steps_per_generator_step:
                 C_update, EG_update = False, True
                 C_steps = 0
-                continue
         elif EG_update:
+            print('- [ Training encoder / generator ]')
             optimizer_EG.zero_grad()
             EG_loss.backward()
             EG_losses += [EG_loss.detach().item()]
+            nonneg_e, mod = regularisation(model.encoder.model.model, x=x)
+            nonneg_g, _ = regularisation(model.generator.model.model)
+            nonneg = nonneg_g + nonneg_e
+            nonneg.backward()
+            mod.backward()
+            optimizer_EG.step()
             C_update, EG_update = True, False
-            
-    if epoch % log_interval == 0:
-        fig, ax = subplots(3, 8)
-        for i, conn in exemplars:
-            ax[0][i].imshow(conn.detach_().cpu())
-            
-        fig.savefig(f'{results}/desc-samples_epoch-{epoch:08}.png')
+        print(f'- [ Wasserstein loss | {EG_loss} ] ')
+        print(f'- [ Critic loss with gradient penalty | {C_loss} ] ')
+        print(f'- [ Nonnegativity loss | {nonneg} ] ')
+        print(f'- [ Modularity loss | {mod} ] ')
+
+        if cur_step % log_interval == 0:
+            with torch.no_grad():
+                (q_E, q_G), z = model.qnet(
+                    subject_query=exemplars['subject_query'],
+                    task_query=exemplars['task_query']
+                )
+                recon = model.reconstruct(
+                    exemplars['x'], query_E=q_E, query_G=q_G).cpu()
+                gen = model.generate(noise, query=q_G).cpu()
+
+                fig, ax = plt.subplots(3, 8, figsize=(24, 9))
+                for i, conn in enumerate(exemplars['x']):
+                    ax[0][i].imshow(-conn.cpu(), vmin=-0.4, vmax=0.4, cmap='RdBu')
+                    ax[0][i].set_xticks([])
+                    ax[0][i].set_yticks([])
+                    vlim = recon[i].abs().ravel().sort().values[vidx]
+                    ax[1][i].imshow(-recon[i], vmin=-vlim, vmax=vlim, cmap='RdBu')
+                    ax[1][i].set_xticks([])
+                    ax[1][i].set_yticks([])
+                    vlim = gen[i].abs().ravel().sort().values[vidx]
+                    ax[2][i].imshow(-gen[i], vmin=-vlim, vmax=vlim, cmap='RdBu')
+                    ax[2][i].set_xticks([])
+                    ax[2][i].set_yticks([])
+
+                fig.savefig(f'{results}/desc-samples_step-{cur_step:09}.png')
+        cur_step += 1
