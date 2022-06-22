39f24ac01451a56eb45e4c21f1a04d26337ced22
Rastko Ciric <rastko@stanford.edu>
2022-06-21 23:22:36 -0700

    (TEST) test diffusion mapping against `brainspace` reference

--- hypercoil/functional/test/test_connectopy.py
+++ hypercoil/functional/test/test_connectopy.py
@@ -10,11 +10,13 @@ import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 from brainspace import datasets
 from brainspace.gradient.embedding import(
-    laplacian_eigenmaps as le_ref
+    laplacian_eigenmaps as le_ref,
+    diffusion_mapping as dm_ref
 )
 from pkg_resources import resource_filename as pkgrf
 from hypercoil.functional.connectopy import (
-    laplacian_eigenmaps
+    laplacian_eigenmaps,
+    diffusion_mapping
 )
 
 
@@ -30,7 +32,7 @@ class TestConnectopy:
             'hypercoil',
             'results/'
         )
-        fig.savefig(f'{results}/eigenmaps_{name}.png')
+        fig.savefig(f'{results}/{name}.png')
 
     def test_circle_eigenmaps(self):
         dim = 101
@@ -42,7 +44,7 @@ class TestConnectopy:
         self.plot_gradients(
             ref=Q_ref[..., :2].T,
             test=Q[..., :2].t(),
-            name='circle'
+            name='eigenmaps_circle'
         )
 
     def test_schaefer_eigenmaps(self):
@@ -52,7 +54,7 @@ class TestConnectopy:
         self.plot_gradients(
             ref=Q_ref[..., :2].T,
             test=Q[..., :2].t(),
-            name='schaefer'
+            name='eigenmaps_schaefer'
         )
 
     def test_circle_eigenmaps_sparse(self):
@@ -67,5 +69,54 @@ class TestConnectopy:
         self.plot_gradients(
             ref=Q_ref[..., :2].T,
             test=Q[..., :2].t(),
-            name='sparse'
+            name='eigenmaps_sparse'
+        )
+
+    def test_circle_diffusion(self):
+        dim = 101
+        A = torch.diag_embed(torch.ones(dim - 1), 1)
+        A[0, -1] = 1
+        A = A + A.t()
+        Q, L = diffusion_mapping(A)
+        Q_ref, L_ref = dm_ref(A.numpy())
+        self.plot_gradients(
+            ref=Q_ref[..., :2].T,
+            test=Q[..., :2].t(),
+            name='diffusion_circle'
+        )
+
+    def test_schaefer_diffusion(self):
+        A = datasets.load_group_fc(parcellation='schaefer')
+        Q, L = diffusion_mapping(torch.tensor(A))
+        Q_ref, L_ref = dm_ref(A)
+        self.plot_gradients(
+            ref=Q_ref[..., :2].T,
+            test=Q[..., :2].t(),
+            name='diffusion_schaefer'
+        )
+        Q, L = diffusion_mapping(
+            torch.tensor(A),
+            alpha=1,
+            diffusion_time=10
+        )
+        Q_ref, L_ref = dm_ref(A, alpha=1, diffusion_time=10)
+        self.plot_gradients(
+            ref=Q_ref[..., :2].T,
+            test=Q[..., :2].t(),
+            name='diffusion_schaefer_param'
+        )
+
+    def test_circle_diffusion_sparse(self):
+        dim = 101
+        W = torch.ones(dim, dtype=torch.float)
+        E = list(zip(range(dim - 1), range(1, dim)))
+        E += [(100, 0)]
+        E = torch.tensor(E)
+        Q, L = diffusion_mapping(W, E.t())
+        A = csr_matrix((W, E.t()), (dim, dim))
+        Q_ref, L_ref = dm_ref(A)
+        self.plot_gradients(
+            ref=Q_ref[..., :2].T,
+            test=Q[..., :2].t(),
+            name='diffusion_sparse'
         )
