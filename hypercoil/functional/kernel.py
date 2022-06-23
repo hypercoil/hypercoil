e7f7adf2073bf1f57f0e4155ba236bbdc6c7e4eb
Rastko Ciric <rastko@stanford.edu>
2022-06-22 22:55:17 -0700

    (ENH) kernels and distance: handle dense or sparse feature tensors

--- hypercoil/functional/kernel.py
+++ hypercoil/functional/kernel.py
@@ -5,7 +5,7 @@
 Parameterised similarity kernels and distance metrics.
 """
 import torch
-from .utils import _conform_vector_weight
+from .utils import sparse_mm, _conform_vector_weight, _promote_nnz_dim
 
 
 def _default_gamma(X, gamma):
@@ -14,9 +14,28 @@ def _default_gamma(X, gamma):
     return gamma
 
 
-def linear_kernel(X0, X1=None, theta=None):
-    if X1 is None:
-        X1 = X0
+def _embed_params_in_diagonal(theta):
+    dim = theta.size(-1)
+    indices = torch.arange(dim, dtype=theta.dtype, device=theta.device)
+    indices = torch.stack((indices, indices))
+    theta = _promote_nnz_dim(theta)
+    return torch.sparse_coo_tensor(
+        indices, theta, size=(dim, dim, *theta.shape[1:])
+    )
+
+
+def _embed_params_in_sparse(theta):
+    dim = theta.size(-1)
+    with torch.no_grad():
+        nzi = theta.abs().sum(list(range(theta.dim() - 2)))
+        indices = torch.stack(torch.where(nzi))
+    values = _promote_nnz_dim(theta[..., indices[0], indices[1]])
+    return torch.sparse_coo_tensor(
+        indices, values, size=(dim, dim, *values.shape[1:])
+    )
+
+
+def _linear_kernel_dense(X0, X1, theta):
     if theta is None:
         return X0 @ X1.transpose(-1, -2)
     elif theta.dim() == 1 or theta.shape[-1] != theta.shape[-2]:
@@ -24,6 +43,33 @@ def linear_kernel(X0, X1=None, theta=None):
         return (X0 * theta) @ X1.transpose(-1, -2)
     else:
         return X0 @ theta @ X1.transpose(-1, -2)
+
+
+def _linear_kernel_sparse(X0, X1, theta):
+    if theta is None:
+        return sparse_mm(X0, X1.transpose(0, 1))
+    elif theta.dim() == 1 or theta.shape[-1] != theta.shape[-2]:
+        theta = _embed_params_in_diagonal(theta)
+        #shape = [1 for _ in range(1, values.dim())]
+        #theta = theta.view(-1, *shape)
+        #print(values.shape, theta.shape)
+        #values = values * theta
+    else:
+        theta = _embed_params_in_sparse(theta)
+    #indices = X0._indices()
+    #X0 = torch.sparse_coo_tensor(
+    #    indices=indices, values=values, size=(X0.size()))
+    X0 = sparse_mm(X0, theta)
+    return sparse_mm(X0, X1.transpose(0, 1))
+
+
+def linear_kernel(X0, X1=None, theta=None):
+    if X1 is None:
+        X1 = X0
+    if X0.is_sparse:
+        return _linear_kernel_sparse(X0, X1, theta)
+    else:
+        return _linear_kernel_dense(X0, X1, theta)
 
 
 def linear_distance(X0, X1=None, theta=None):
