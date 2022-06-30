(TEST)(FIX) volume-normalising activation function for PSD matrices

hypercoil/functional/activation.py
@@ -299,7 +299,10 @@ def isochor(input, volume=1, max_condition=None, softmax_temp=None):
         #print(psi, L)
         L = (1 - psi) * L + psi * torch.ones_like(L)
         #print(L)
-    # L = L / (L.prod(-1, keepdim=True) ** (1 / L.size(-1)))
-    Lnorm = (L.log().sum() * (1 / L.size(-1))).exp()
+    #L = L / (L.prod(-1, keepdim=True) ** (1 / L.size(-1)))
+    Lnorm = ((
+        L.log().sum(-1, keepdim=True) -
+        torch.tensor(volume, dtype=L.dtype).log()
+    ) * (1 / L.size(-1))).exp()
     L = L / Lnorm
     return Q @ (L.unsqueeze(-1) * Q.transpose(-1, -2))

hypercoil/functional/test/test_activation.py
@@ -7,7 +7,7 @@ Unit tests for activation functions
 import pytest
 import torch
 from hypercoil.functional import cov, corr
-from hypercoil.functional.activation import corrnorm
+from hypercoil.functional.activation import corrnorm, isochor
 
 
 class TestActivationFunctions:
@@ -21,3 +21,21 @@ class TestActivationFunctions:
             torch.sign(torch.diagonal(B)) ==
             torch.sign(torch.diagonal(corrnorm(B)))
         )
+
+    def test_isochor(self):
+        A = torch.randn(5, 20, 20)
+        A = A @ A.transpose(-1, -2)
+        out = isochor(A)
+        assert torch.allclose(
+            torch.linalg.det(out), torch.tensor(1.), rtol=1e-2)
+
+        out = isochor(A, volume=4)
+        assert torch.allclose(
+            torch.linalg.det(out), torch.tensor(4.), rtol=1e-2)
+
+        out = isochor(A, volume=4, max_condition=5)
+        assert torch.allclose(
+            torch.linalg.det(out), torch.tensor(4.), rtol=1e-2)
+        L, Q = torch.linalg.eigh(out)
+        print(L.amax(dim=-1), L.amin(dim=-1))
+        assert torch.all((L.amax(dim=-1) / L.amin(dim=-1)) < 5.01)