b9b3195088e067a5692ffffc302a1a0d2179a6ca
Rastko Ciric <rastko@stanford.edu>
2022-06-30 18:43:58 -0700

    (ENH) ephemeral optim: allow different params for ephemeral groups, handle existing index

--- hypercoil/engine/ephemeral.py
+++ hypercoil/engine/ephemeral.py
@@ -25,16 +25,21 @@ class EphemeralMixin:
     def load_ephemeral(self, params, buffers=None):
         if isinstance(params, torch.Tensor):
             params = [params]
-        params_ephemeral = {'params' : params}
-        params_ephemeral.update(self.params_ephemeral)
-        self.param_groups += [params_ephemeral]
-        self.ephemeral_index = len(self.param_groups) - 1
+        if self.ephemeral_index is None:
+            params_ephemeral = {'params' : params}
+            params_ephemeral.update(self.params_ephemeral)
+            self.param_groups += [params_ephemeral]
+            self.ephemeral_index = len(self.param_groups) - 1
+        else:
+            self.param_groups[self.ephemeral_index]['params'] += params
         if all([not buffer for buffer in buffers.values()]):
             return
         for i, p in enumerate(params):
-            self.state[p] = {}
+            if self.state.get(p) is None:
+                self.state[p] = {}
             for buffer, value in buffers.items():
-                print(buffer, value)
+                #TODO: this doesn't allow for the case that we want to
+                # override a buffer to `None`
                 if value is not None:
                     self.state[p][buffer] = value[i]
 
@@ -56,7 +61,9 @@ class EphemeralMixin:
 
 class SGDEphemeral(EphemeralMixin, SGD):
     def __init__(self, params, lr, momentum=0, dampening=0,
-                 weight_decay=0, nesterov=False, *, maximize=False): #,
+                 weight_decay=0, nesterov=False, *, maximize=False,
+                 lr_ephemeral=None, momentum_ephemeral=None,
+                 dampening_ephemeral=None, weight_decay_ephemeral=None): #,
                  #foreach: Optional[bool] = None):
         super().__init__(
             params=params,
@@ -68,12 +75,20 @@ class SGDEphemeral(EphemeralMixin, SGD):
             maximize=maximize,
             #foreach=foreach
         )
+        if lr_ephemeral is None:
+            lr_ephemeral = lr
+        if momentum_ephemeral is None:
+            momentum_ephemeral = momentum
+        if dampening_ephemeral is None:
+            dampening_ephemeral = dampening
+        if weight_decay_ephemeral is None:
+            weight_decay_ephemeral = weight_decay
         self.ephemeral_index = None
         self.params_ephemeral = {
-            'lr' : lr,
-            'momentum' : momentum,
-            'dampening' : dampening,
-            'weight_decay' : weight_decay,
+            'lr' : lr_ephemeral,
+            'momentum' : momentum_ephemeral,
+            'dampening' : dampening_ephemeral,
+            'weight_decay' : weight_decay_ephemeral,
             'nesterov' : nesterov,
             'maximize' : maximize
         }
@@ -90,7 +105,9 @@ class SGDEphemeral(EphemeralMixin, SGD):
 
 class AdamEphemeral(EphemeralMixin, Adam):
     def __init__(self, params, lr=0.001, betas=(0.9, 0.999), eps=1e-08,
-                 weight_decay=0, amsgrad=False, *, maximize=False): #,
+                 weight_decay=0, amsgrad=False, *, maximize=False,
+                 lr_ephemeral=None, betas_ephemeral=None,
+                 weight_decay_ephemeral=None): #,
                  #foreach: Optional[bool] = None):
         super().__init__(
             params=params,
@@ -102,12 +119,18 @@ class AdamEphemeral(EphemeralMixin, Adam):
             maximize=maximize,
             #foreach=foreach
         )
+        if lr_ephemeral is None:
+            lr_ephemeral = lr
+        if betas_ephemeral is None:
+            betas_ephemeral = betas
+        if weight_decay_ephemeral is None:
+            weight_decay_ephemeral = weight_decay
         self.ephemeral_index = None
         self.params_ephemeral = {
-            'lr' : lr,
-            'betas' : betas,
+            'lr' : lr_ephemeral,
+            'betas' : betas_ephemeral,
             'eps' : eps,
-            'weight_decay' : weight_decay,
+            'weight_decay' : weight_decay_ephemeral,
             'amsgrad' : amsgrad,
             'maximize' : maximize
         }
