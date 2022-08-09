7fce97b3cf24da523aa1093fa562879cf849db17
Rastko Ciric <rastko@stanford.edu>
2022-08-09 01:05:10 -0700

    (REF)(TEST) JAX recore: test PSD cone operations

--- hypercoil/functional/test/test_semidefinite.py
+++ hypercoil/functional/test/test_semidefinite.py
@@ -6,12 +6,13 @@ Unit tests for operations in the positive semidefinite cone
 """
 import pytest
 import numpy as np
-import torch
+from scipy.linalg import expm, logm
 from nilearn.connectome.connectivity_matrices import (
     _form_symmetric, _map_eigenvalues, _geometric_mean
 )
 from hypercoil.functional import (
-    tangent_project_spd, cone_project_spd, mean_geom_spd
+    tangent_project_spd, cone_project_spd,
+    mean_euc_spd, mean_harm_spd, mean_logeuc_spd, mean_geom_spd
 )
 
 
@@ -42,26 +43,18 @@ class TestSemidefinite:
         self.approx = lambda out, ref: np.allclose(
             out, ref, atol=self.tol, rtol=self.tol)
         np.random.seed(10)
-        torch.manual_seed(10)
 
         A = np.random.rand(10, 10)
         AM = np.random.rand(200, 10, 10)
         self.A = A @ A.T
         self.AM = AM @ np.swapaxes(AM, -1, -2)
         self.R = self.AM.mean(0)
-        self.At = torch.Tensor(self.A)
-        self.AMt = torch.Tensor(self.AM)
-        self.Rt = torch.Tensor(self.R)
-        if torch.cuda.is_available():
-            self.AtC = self.At.clone().cuda()
-            self.AMtC = self.AMt.clone().cuda()
-            self.RtC = self.Rt.clone().cuda()
 
     def test_tangent_project(self):
-        out = tangent_project_spd(self.At, self.Rt).numpy()
+        out = tangent_project_spd(self.A, self.R)
         ref = nilearn_tangent_project([self.A], self.R)
         assert np.allclose(out, ref, atol=1e-1, rtol=1e-1)
-        out = tangent_project_spd(self.AMt, self.Rt).numpy()
+        out = tangent_project_spd(self.AM, self.R)
         ref = nilearn_tangent_project(self.AM, self.R)
         # Note that this is a very weak condition! This would likely
         # experience major improvement if pytorch develops a proper
@@ -71,27 +64,27 @@ class TestSemidefinite:
 
     def test_cone_project(self):
         V = nilearn_tangent_project([self.A], self.R).squeeze()
-        Vt = torch.Tensor(V)
-        out = cone_project_spd(Vt, self.Rt).numpy()
+        out = cone_project_spd(V, self.R)
         ref = nilearn_cone_project(V, self.R)
-        assert self.approx(out, ref)
-        out = cone_project_spd(self.AMt, self.Rt).numpy()
+        assert np.allclose(out, ref, atol=1e-2, rtol=1e-2)
+        out = cone_project_spd(self.AM, self.R)
         ref = np.stack([nilearn_cone_project(AMi, self.R) for AMi in self.AM])
-        assert self.approx(out, ref)
+        assert np.allclose(out, ref, atol=1e-2, rtol=1e-2)
 
-    def test_proper_inverse(self):
-        Vt = tangent_project_spd(self.AMt, self.Rt, recondition=5e-4)
-        AM_rec = cone_project_spd(Vt, self.Rt, recondition=5e-4).numpy()
-        assert np.allclose(self.AM, AM_rec, atol=1e-2, rtol=1e-2)
+    def test_means(self):
+        out = mean_euc_spd(self.AM, axis=0)
+        ref = self.AM.mean(0)
+        assert np.allclose(out, ref)
+        out = mean_harm_spd(self.AM, axis=0)
+        ref = np.linalg.inv(np.linalg.inv(self.AM).mean(0))
+        assert np.allclose(out, ref, atol=1e-2, rtol=1e-2)
+        out = mean_logeuc_spd(self.AM, axis=0)
+        ref = expm(np.stack([logm(m) for m in self.AM]).mean(0))
+        assert np.allclose(out, ref, atol=1e-2, rtol=1e-2)
+
 
     def test_geometric_mean(self):
-        out = mean_geom_spd(self.AMt, recondition=1e-3).numpy()
+        out = mean_geom_spd(self.AM, recondition=1e-3)
         ref = _geometric_mean([i for i in self.AM])
         # Another fairly weak condition.
         assert np.abs(out - ref).max() < .1
-
-    @pytest.mark.cuda
-    def test_cone_tangent_cuda(self):
-        VtC = tangent_project_spd(self.AMtC, self.RtC, recondition=5e-4)
-        AM_rec = cone_project_spd(VtC, self.RtC, recondition=5e-4)
-        out = mean_geom_spd(self.AMtC, recondition=1e-6)
