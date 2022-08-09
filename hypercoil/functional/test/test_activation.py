4a29a6a123995459535a437abdf386b1ecff09d7
Rastko Ciric <rastko@stanford.edu>
2022-08-08 17:18:07 -0700

    (REF)(TEST) JAX recore: activation tests

--- hypercoil/functional/test/test_activation.py
+++ hypercoil/functional/test/test_activation.py
@@ -5,45 +5,80 @@
 Unit tests for activation functions
 """
 import pytest
-import torch
+import jax
+import jax.numpy as jnp
+import numpy as np
 from hypercoil.functional import cov, corr
-from hypercoil.functional.activation import corrnorm, isochor
+from hypercoil.functional.activation import (
+    laplace, expbarrier,
+    amplitude_tanh, amplitude_atanh,
+    amplitude_laplace, amplitude_expbarrier,
+    corrnorm, isochor
+)
 
 
 class TestActivationFunctions:
 
+    def test_activations_forward(self):
+        A = np.random.rand(5, 5, 5)
+        laplace(A)
+        expbarrier(A)
+        amplitude_tanh(A)
+        amplitude_atanh(A)
+        amplitude_laplace(A)
+        amplitude_expbarrier(A)
+        jax.jit(laplace)(A)
+        jax.jit(expbarrier)(A)
+        jax.jit(amplitude_tanh)(A)
+        jax.jit(amplitude_atanh)(A)
+        jax.jit(amplitude_laplace)(A)
+        jax.jit(amplitude_expbarrier)(A)
+
+        assert np.allclose(amplitude_tanh(amplitude_atanh(A)), A)
+
     def test_corrnorm(self):
-        A = torch.rand(12, 30)
-        assert torch.all(corrnorm(cov(A)) == corr(A))
-        B = torch.randn(12, 12)
-        B = B + B.t()
-        assert torch.all(
-            torch.sign(torch.diagonal(B)) ==
-            torch.sign(torch.diagonal(corrnorm(B)))
+        A = np.random.rand(12, 30)
+        assert np.all(corrnorm(cov(A)) == corr(A))
+        B = np.random.randn(12, 12)
+        B = B + B.T
+        assert np.all(
+            jnp.sign(jnp.diagonal(B)) ==
+            jnp.sign(jnp.diagonal(corrnorm(B)))
         )
+
+        # test jit
+        jit_corrnorm = jax.jit(corrnorm)
+        assert np.all(jit_corrnorm(cov(A)) == corr(A))
 
     def test_isochor(self):
         # on rare occasions it's outside tolerance
-        torch.random.manual_seed(238)
-        A = torch.randn(5, 20, 20)
-        A = A @ A.transpose(-1, -2)
+        key = jax.random.PRNGKey(238)
+        subkey, key = jax.random.split(key)
+        A = jax.random.normal(subkey, (5, 20, 20))
+        A = A @ A.swapaxes(-1, -2)
         out = isochor(A)
-        assert torch.allclose(
-            torch.linalg.det(out), torch.tensor(1.), rtol=1e-2)
+        assert np.allclose(jnp.linalg.det(out), 1, atol=1e-2)
 
         out = isochor(A, volume=4)
-        assert torch.allclose(
-            torch.linalg.det(out), torch.tensor(4.), rtol=1e-2)
+        assert np.allclose(jnp.linalg.det(out), 4, atol=1e-2)
 
         out = isochor(A, volume=4, max_condition=5)
-        assert torch.allclose(
-            torch.linalg.det(out), torch.tensor(4.), rtol=1e-2)
-        L, Q = torch.linalg.eigh(out)
-        print(L.amax(dim=-1), L.amin(dim=-1))
-        assert torch.all((L.amax(dim=-1) / L.amin(dim=-1)) < 5.01)
+        assert np.allclose(jnp.linalg.det(out), 4, atol=1e-2)
+        L, Q = jnp.linalg.eigh(out)
+        assert np.all((L.max(-1) / L.min(-1)) < 5.01)
 
         out = isochor(A, softmax_temp=1e10)
-        assert torch.allclose(out, torch.eye(20), atol=1e-6)
+        assert np.allclose(out, jnp.eye(20), atol=1e-4)
 
         out = isochor(A, volume=(2 ** 20), max_condition=1)
-        assert torch.allclose(out, 2 * torch.eye(20), atol=1e-6)
+        assert np.allclose(out, 2 * jnp.eye(20), atol=1e-4)
+
+        # test jit
+        jit_isochor = jax.jit(isochor)
+        out = jit_isochor(A, volume=4, max_condition=5)
+        assert np.allclose(jnp.linalg.det(out), 4, atol=1e-2)
+        L, Q = jnp.linalg.eigh(out)
+        assert np.all((L.max(-1) / L.min(-1)) < 5.01)
+
+        out = jit_isochor(A, softmax_temp=1e10)
+        assert np.allclose(out, jnp.eye(20), atol=1e-4)
