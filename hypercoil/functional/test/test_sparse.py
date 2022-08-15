2c7fc1237b3df7b025003dc70cba369993e34ee4
Rastko Ciric <rastko@stanford.edu>
2022-08-14 17:50:49 -0700

    (TEST) test sparse-sparse matmul

--- hypercoil/functional/test/test_sparse.py
+++ hypercoil/functional/test/test_sparse.py
@@ -7,7 +7,7 @@ Unit tests for sparse matrix utilities.
 import jax
 import numpy as np
 from hypercoil.functional.sparse import(
-    random_sparse, to_batch
+    random_sparse, to_batch, spspmm
 )
 
 
@@ -29,3 +29,37 @@ class TestSparse:
         for M in batch:
             assert M.nse == nse
             assert M.shape == shape
+
+    def test_spspmm(self):
+        shape_A = (10, 8)
+        shape_B = (10, 18)
+        batch_size = 5
+        density = 0.1
+        batch_A = [
+            random_sparse(
+                jax.random.PRNGKey(np.random.randint(2 ** 32)),
+                shape_A,
+                density=density)
+            for _ in range(batch_size)]
+        batch_B = [
+            random_sparse(
+                jax.random.PRNGKey(np.random.randint(2 ** 32)),
+                shape_B,
+                density=density)
+            for _ in range(batch_size)]
+        A = to_batch(batch_A)
+        B = to_batch(batch_B)
+        out = spspmm(A, B).todense()
+        ref = np.stack([(a.T @ b).todense() for a, b in zip(batch_A, batch_B)], axis=-1)
+        assert np.allclose(out, ref)
+        assert out.shape == (shape_A[1], shape_B[1], batch_size)
+
+        spspmm_jit = jax.jit(spspmm)
+        out = spspmm_jit(A, B)
+        assert out.shape == (shape_A[1], shape_B[1], batch_size)
+        out = spspmm_jit(A, A)
+        assert out.shape == (shape_A[1], shape_A[1], batch_size)
+        out = spspmm_jit(B, B)
+        assert out.shape == (shape_B[1], shape_B[1], batch_size)
+        out = spspmm_jit(B, A)
+        assert out.shape == (shape_B[1], shape_A[1], batch_size)
