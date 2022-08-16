509fd2bf565b2e0142a27854c59e83ba2cc629fa
Rastko Ciric <rastko@stanford.edu>
2022-08-16 10:57:47 -0700

    (TEST) basic format tests for top-k sparse BCOO

--- hypercoil/functional/test/test_sparse.py
+++ hypercoil/functional/test/test_sparse.py
@@ -7,22 +7,33 @@ Unit tests for sparse matrix utilities.
 import jax
 import numpy as np
 from hypercoil.functional.sparse import(
-    random_sparse, to_batch, spspmm
+    random_sparse,
+    random_sparse_batchfinal, to_batch_batchfinal, spspmm_batchfinal
 )
 
 
 class TestSparse:
-    def test_sparse_batch(self):
+    def test_sparse_topk(self):
+        out = random_sparse(
+            (4, 3, 1000, 1000),
+            k=5,
+            key=jax.random.PRNGKey(4839)
+        )
+        assert out.shape == (4, 3, 1000, 1000)
+        assert out.indices.shape == (1, 1, 1000, 5, 1)
+        assert out.data.shape == (4, 3, 1000, 5)
+
+    def test_sparse_batch_batchfinal(self):
         shape = (10, 10)
         batch_size = 5
         density = 0.1
         batch = [
-            random_sparse(
+            random_sparse_batchfinal(
                 jax.random.PRNGKey(np.random.randint(2 ** 32)),
                 shape,
                 density=density)
             for _ in range(batch_size)]
-        B = to_batch(batch)
+        B = to_batch_batchfinal(batch)
         nse = int(density * np.prod(shape))
         assert B.shape == shape + (batch_size,)
         assert B.nse <= batch_size * nse
@@ -30,31 +41,31 @@ class TestSparse:
             assert M.nse == nse
             assert M.shape == shape
 
-    def test_spspmm(self):
+    def test_spspmm_batchfinal(self):
         shape_A = (10, 8)
         shape_B = (10, 18)
         batch_size = 5
         density = 0.1
         batch_A = [
-            random_sparse(
+            random_sparse_batchfinal(
                 jax.random.PRNGKey(np.random.randint(2 ** 32)),
                 shape_A,
                 density=density)
             for _ in range(batch_size)]
         batch_B = [
-            random_sparse(
+            random_sparse_batchfinal(
                 jax.random.PRNGKey(np.random.randint(2 ** 32)),
                 shape_B,
                 density=density)
             for _ in range(batch_size)]
-        A = to_batch(batch_A)
-        B = to_batch(batch_B)
-        out = spspmm(A, B).todense()
+        A = to_batch_batchfinal(batch_A)
+        B = to_batch_batchfinal(batch_B)
+        out = spspmm_batchfinal(A, B).todense()
         ref = np.stack([(a.T @ b).todense() for a, b in zip(batch_A, batch_B)], axis=-1)
         assert np.allclose(out, ref)
         assert out.shape == (shape_A[1], shape_B[1], batch_size)
 
-        spspmm_jit = jax.jit(spspmm)
+        spspmm_jit = jax.jit(spspmm_batchfinal)
         out = spspmm_jit(A, B)
         assert out.shape == (shape_A[1], shape_B[1], batch_size)
         out = spspmm_jit(A, A)
