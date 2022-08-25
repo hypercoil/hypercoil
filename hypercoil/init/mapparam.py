242a75445c97c951fd4e67cf17786664840dfbd7
Rastko Ciric <rastko@stanford.edu>
2022-08-24 18:34:39 -0700

    (REF) JAX/equinox recore: domain mapper base and trivial classes

--- hypercoil/init/mapparam.py
+++ hypercoil/init/mapparam.py
@@ -7,15 +7,15 @@ Generic image / preimage mapper.
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-from typing import Optional, Tuple
+from typing import Any, Callable, Optional, Tuple
 from ..functional.utils import (
-    Tensor, PyTree
+    Tensor, PyTree, complex_decompose, complex_recompose
 )
 
 
 class MappedParameter(eqx.Module):
-    param_name: str = "weight"
     original: Tensor
+    param_name: str = "weight"
 
     def __init__(self, model: PyTree, *, param_name: str = "weight"):
         self.param_name = param_name
@@ -76,3 +76,116 @@ class Renormalise(OutOfDomainHandler):
         out = x - x.mean(axis)
         out = out / ((upper - lower) / (unew - lnew))
         return out + lnew - out.min(axis)
+
+
+class DomainMappedParameter(MappedParameter):
+    original: Tensor
+    param_name: str = "weight"
+    image_bound: Any = None
+    preimage_bound: Any = None
+    handler: Any = None
+
+    def __init__(
+        self,
+        model: PyTree,
+        *,
+        param_name: str = "weight",
+        image_bound: Any = None,
+        preimage_bound: Any = None,
+        handler: Callable = None
+    ):
+        self.handler = handler or Clip()
+        self.image_bound = image_bound or (-float('inf'), float('inf'))
+        self.preimage_bound = preimage_bound or (-float('inf'), float('inf'))
+        super(DomainMappedParameter, self).__init__(
+            model=model, param_name=param_name)
+
+    def preimage_map(self, param: Tensor) -> Tensor:
+        x = self.handler.apply(param, self.image_bound)
+        i = self.preimage_map_impl(x)
+        i = self.handler.apply(i, self.preimage_bound)
+        return i
+
+    def image_map(self, param: Tensor) -> Tensor:
+        return self.image_map_impl(param)
+
+    def preimage_map_impl(self, param: Tensor) -> Tensor:
+        raise NotImplementedError()
+
+    def image_map_impl(self, param: Tensor) -> Tensor:
+        raise NotImplementedError()
+
+    def test(self, param: Tensor):
+        return self.handler.test(param, self.image_bound)
+
+    def handle_ood(self, param: Tensor) -> Tensor:
+        return self.handler.apply(param, self.image_bound)
+
+
+class AffineDomainMappedParameter(DomainMappedParameter):
+    original: Tensor
+    param_name: str = "weight"
+    loc: Tensor = 0.
+    scale: Tensor = 1.
+    image_bound: Any = None
+    preimage_bound: Any = None
+    handler: Any = None
+
+    def __init__(
+        self,
+        model: PyTree,
+        *,
+        loc: Tensor = 0.,
+        scale: Tensor = 1.,
+        param_name: str = "weight",
+        image_bound: Any = None,
+        preimage_bound: Any = None,
+        handler: Callable = None
+    ):
+        self.loc = loc
+        self.scale = scale
+        super(AffineDomainMappedParameter, self).__init__(
+            model=model,
+            param_name=param_name,
+            image_bound=image_bound,
+            preimage_bound=preimage_bound,
+            handler=handler
+        )
+
+    def preimage_map(self, param: Tensor) -> Tensor:
+        x = self.handler.apply(param, self.image_bound)
+        i = self.preimage_map_impl((x - self.loc) / self.scale)
+        i = self.handler.apply(i, self.preimage_bound)
+        return i
+
+    def image_map(self, param: Tensor) -> Tensor:
+        return self.scale * self.image_map_impl(param) + self.loc
+
+
+class PhaseAmplitudeMixin:
+    def preimage_map(self, param: Tensor) -> Tensor:
+        ampl, phase = complex_decompose(param)
+        ampl = super().preimage_map(ampl)
+        return complex_recompose(ampl, phase)
+
+    def image_map(self, param: Tensor) -> Tensor:
+        ampl, phase = complex_decompose(param)
+        ampl = super().image_map(ampl)
+        return complex_recompose(ampl, phase)
+
+
+class IdentityMappedParameter(MappedParameter):
+    def preimage_map(self, param: Tensor) -> Tensor:
+        return param
+
+    def image_map(self, param: Tensor) -> Tensor:
+        return param
+
+
+class AffineMappedParameter(AffineDomainMappedParameter):
+
+    def preimage_map_impl(self, param: Tensor) -> Tensor:
+        return param
+
+    def image_map_impl(self, param: Tensor) -> Tensor:
+        return param
