b1a4922f3195dfea4ee4ca2d8ed4cd43c7379f80
Rastko Ciric <rastko@stanford.edu>
2022-09-01 18:56:37 -0700

    (TEST) JAX recore: frequency domain filter initialiser

--- hypercoil/init/test/test_freqfilter.py
+++ hypercoil/init/test/test_freqfilter.py
@@ -4,76 +4,125 @@
 """
 Unit tests for IIR filter initialisation
 """
-import pytest
+import jax
+import jax.numpy as jnp
+import equinox as eqx
 import numpy as np
-import torch
+import matplotlib.pyplot as plt
+from pkg_resources import resource_filename as pkgrf
+from hypercoil.functional.utils import Tensor
+from hypercoil.init.mapparam import AmplitudeTanhMappedParameter
 from hypercoil.init.freqfilter import (
     FreqFilterSpec,
-    freqfilter_init_,
-    clamp_init_
+    freqfilter_init,
+    clamp_init,
+    FreqFilterInitialiser,
 )
 
 
 class TestIIRFilter:
 
-    @pytest.fixture(autouse=True)
-    def setup_class(self):
-        N = torch.Tensor([[1], [4]])
-        Wn = torch.Tensor([[0.1, 0.3], [0.4, 0.6]])
-        self.filter_specs = [
-            FreqFilterSpec(Wn=[0.1, 0.3]),
-            FreqFilterSpec(Wn=Wn, N=N),
+    def plot_ampl_response(self, spectra, names, path):
+        fig = plt.figure(figsize=(10, 10))
+        for spectrum in spectra:
+            freqs = np.linspace(0, 1, spectrum.shape[-1])
+            spectrum = np.abs(spectrum)
+            plt.plot(freqs, spectrum)
+        plt.legend(names)
+        results = pkgrf(
+            'hypercoil',
+            'results/'
+        )
+        plt.suptitle('Emulated amplitude response')
+        plt.title('Do not use this as a guide: it is just a test', fontsize=8)
+        fig.savefig(f'{results}/freqfilter_{path}.png', bbox_inches='tight')
+
+    def test_spectra(self):
+        N = (1, 4)
+        Wn = ((0.1, 0.3), (0.4, 0.6))
+        filter_specs = (
+            FreqFilterSpec(Wn=[0.1, 0.3], ftype='butter'),
+            FreqFilterSpec(Wn=Wn, N=N, ftype='butter'),
             FreqFilterSpec(Wn=Wn, ftype='ideal'),
             FreqFilterSpec(Wn=[0.1, 0.2], N=[2, 2], btype='lowpass'),
-            FreqFilterSpec(Wn=Wn, N=N, ftype='cheby1', rp=0.1),
+            FreqFilterSpec(Wn=Wn, N=N, ftype='cheby1', rp=0.01),
             FreqFilterSpec(Wn=Wn, N=N, ftype='cheby2', rs=20),
             FreqFilterSpec(Wn=Wn, N=N, ftype='ellip', rs=20, rp=0.1),
-            FreqFilterSpec(Wn=Wn, ftype='randn')
+            FreqFilterSpec(Wn=((0.2, 0.3), (0.4, 0.6)), N=N,
+                           ftype='bessel', norm='amplitude'),
+            FreqFilterSpec(Wn=Wn, ftype='randn'),
+        )
+        filter_names = (
+            'butter', 'ideal', 'cheby1', 'cheby2', 'ellip', 'bessel', 'randn'
+        )
+        filter_idx = (1, 2, 4, 5, 6, 7, 8)
+
+        spectra = [
+            s.initialise_spectrum(key=jax.random.PRNGKey(0), worN=200)
+            for s in filter_specs
         ]
-        self.Z = torch.complex(torch.Tensor(21, 15, 50),
-                               torch.Tensor(21, 15, 50))
-        self.clamped_specs = [
+        spectra = [s[0] for i, s in enumerate(spectra) if i in filter_idx]
+        self.plot_ampl_response(spectra, filter_names, 'spectra')
+        out = freqfilter_init(
+            shape=(3, 1, 200),
+            filter_specs=filter_specs,
+            key=jax.random.PRNGKey(0))
+        n_filters = sum([len(s.Wn) for s in filter_specs])
+        assert out.shape == (3, n_filters, 200)
+
+        model = eqx.nn.Linear(
+            in_features=200, out_features=1,
+            key=jax.random.PRNGKey(0))
+        model = FreqFilterInitialiser.init(
+            model,
+            filter_specs=filter_specs,
+            key=jax.random.PRNGKey(0)
+        )
+        weight_out = model.weight
+        assert weight_out.shape == (n_filters, 200)
+        assert np.allclose(weight_out, out[0])
+
+    def test_clamped_spectra(self):
+        N = (1, 4)
+        Wn = ((0.1, 0.3), (0.4, 0.6))
+        clamped_specs = [
             FreqFilterSpec(Wn=[0.1, 0.3]),
-            FreqFilterSpec(Wn=Wn, clamps=[{}, {0.1: 1}]),
+            FreqFilterSpec(Wn=Wn, clamps=[{0.1: 1}]), # broadcast clamp
             FreqFilterSpec(Wn=[0.1, 0.3], clamps=[{0.1: 0, 0.5:1}]),
             FreqFilterSpec(Wn=Wn, N=N, clamps=[{0.05: 1, 0.1: 0},
-                                              {0.2: 0, 0.5: 1}])
+                                               {0.2: 0, 0.5: 1}])
         ]
-        self.P = torch.Tensor(6, 30)
-        self.V = torch.Tensor(7)
-        self.P2 = torch.Tensor(1, 30)
-        self.V2 = torch.Tensor(0)
-        self.Z2 = torch.complex(torch.Tensor(21, 6, 50),
-                                torch.Tensor(21, 6, 50))
+        points, values = clamp_init(
+            shape=(3, 1, 200),
+            filter_specs=clamped_specs,
+            key=jax.random.PRNGKey(0))
+        n_filters = sum([len(s.Wn) for s in clamped_specs])
+        assert points.shape == values.shape
+        assert points.shape == (n_filters, 200)
+        assert points.sum() == 8
+        assert values.sum() == 5
+        assert np.logical_or(values == 0, values == 1).all()
 
-        if torch.cuda.is_available():
-            self.ZC = self.Z.clone().cuda()
-            self.PC = self.P.clone().cuda()
-            self.VC = self.V.clone().cuda()
-            self.Z2C = self.Z2.clone().cuda()
-            self.P2C = self.P2.clone().cuda()
-            self.V2C = self.V2.clone().cuda()
-
-    def test_freqfilter(self):
-        freqfilter_init_(self.Z, self.filter_specs)
-        assert sum(
-            [spec.n_filters for spec in self.filter_specs]
-            ) == self.Z.size(-2)
-
-    def test_clamps(self):
-        clamp_init_(self.P, self.V, self.clamped_specs)
-        clamp_init_(self.P2, self.V2, [self.clamped_specs[0]])
-        freqfilter_init_(self.Z2, self.clamped_specs)
-
-    @pytest.mark.cuda
-    def test_freqfilter_cuda(self):
-        freqfilter_init_(self.ZC, self.filter_specs)
-        assert sum(
-            [spec.n_filters for spec in self.filter_specs]
-            ) == self.ZC.size(-2)
-
-    @pytest.mark.cuda
-    def test_clamps_cuda(self):
-        clamp_init_(self.PC, self.VC, self.clamped_specs)
-        clamp_init_(self.P2C, self.V2C, [self.clamped_specs[0]])
-        freqfilter_init_(self.Z2C, self.clamped_specs)
+        #TODO: replace this with an actual freqfilter module after it's
+        #      translated to jax
+        class FreqFilter(eqx.Module):
+            weight: Tensor
+            clamp: tuple[Tensor, Tensor]
+            def __init__(self, worN, key):
+                self.weight = jnp.zeros((1, worN))
+                self.clamp = (
+                    jnp.zeros((1, worN)),
+                    jnp.zeros((1, worN))
+                )
+        model = FreqFilter(worN=200, key=jax.random.PRNGKey(0))
+        model = FreqFilterInitialiser.init(
+            model,
+            filter_specs=clamped_specs,
+            clamp_name='clamp',
+            key=jax.random.PRNGKey(0)
+        )
+        points_out, values_out = model.clamp
+        assert points_out.shape == values_out.shape
+        assert points_out.shape == (n_filters, 200)
+        assert (points_out == points).all()
+        assert np.allclose(values_out, values)
