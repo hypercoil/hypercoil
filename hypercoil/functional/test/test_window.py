badf6c6000270a015f6ce613e8944cc20c4de42b
Rastko Ciric <rastko@stanford.edu>
2022-09-08 13:29:09 -0700

    (ENH)(TEST) overlapping windowing functions

--- hypercoil/functional/test/test_window.py
+++ hypercoil/functional/test/test_window.py
@@ -8,7 +8,7 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 from hypercoil.functional.window import (
-    sample_nonoverlapping_windows,
+    sample_windows,
 )
 
 
@@ -18,7 +18,7 @@ class TestWindow:
 
         input = jnp.arange(1000).reshape(10, 100)
         window_fn = jax.jit(
-            sample_nonoverlapping_windows(True),
+            sample_windows(False, True),
             static_argnames=('window_size', 'num_windows')
         )
         out = window_fn(
@@ -34,7 +34,7 @@ class TestWindow:
         for size in (50, 70, 90):
             input = jnp.arange(1000)[None, :]
             window_fn = jax.jit(
-                sample_nonoverlapping_windows(False),
+                sample_windows(False, False),
                 static_argnames=('window_size', 'num_windows')
             )
             out = window_fn(
@@ -49,3 +49,40 @@ class TestWindow:
                 for j in range(i + 1, len(out)):
                     diff = np.abs(window - out[[j]].T)
                     assert (diff > 0).all()
+
+    def test_window_overlapping(self):
+        key = jax.random.PRNGKey(0)
+
+        input = jnp.arange(1000).reshape(10, 100)
+        window_fn = jax.jit(
+            sample_windows(True, True),
+            static_argnames=('window_size', 'num_windows')
+        )
+        out = window_fn(
+            input,
+            window_size=10,
+            num_windows=10,
+            key=key
+        )
+        assert out.shape == (10, 10, 10)
+
+        input = jnp.arange(1000)[None, :]
+        window_fn = jax.jit(
+            sample_windows(True, False),
+            static_argnames=('window_size', 'num_windows')
+        )
+        out = window_fn(
+            input,
+            window_size=50,
+            num_windows=10,
+            key=key
+        )
+        out = np.array(out)
+        assert out.shape == (10, 50)
+        a = 0
+        for i, window in enumerate(out):
+            for j in range(i + 1, len(out)):
+                diff = np.abs(window - out[[j]].T)
+                a += (diff == 0).sum()
+        # some windows overlap. Not guaranteed, but works for this seed.
+        assert a > 0
--- hypercoil/functional/window.py
+++ hypercoil/functional/window.py
@@ -26,7 +26,7 @@ def document_window_fn(fn: Callable) -> Callable:
         The decorated function.
     """
     fn.__doc__ = f"""
-    Sample non-overlapping windows from a tensor.
+    Sample windows from a tensor.
 
     Parameters
     ----------
@@ -49,9 +49,70 @@ def document_window_fn(fn: Callable) -> Callable:
     return fn
 
 
-def _sample_nonoverlapping_impl(
+def _select_fn_allow_overlap(
+    input_size: int,
+    window_size: int,
+    num_windows: int,
+    key: 'jax.random.PRNGKey',
+) -> Tensor:
+    return jax.random.choice(
+        key,
+        a=(input_size - window_size + 1),
+        shape=(num_windows,),
+        replace=False,
+    )
+
+
+def _select_fn_no_overlap(
+    input_size: int,
+    window_size: int,
+    num_windows: int,
+    key: 'jax.random.PRNGKey',
+) -> Tensor:
+    unused_size = input_size - window_size * num_windows
+    intervals = distrax.Multinomial(
+        total_count=unused_size,
+        probs=jnp.ones(num_windows + 1) / (num_windows + 1)
+    ).sample(seed=key)
+    start = jnp.arange(num_windows + 1) * window_size + jnp.cumsum(intervals)
+    return start[:-1]
+
+
+def _slice_fn_existing_ax(
+    tensor: Tensor,
+    start: Sequence,
+    slices: Sequence,
+    sizes: Sequence,
+    windowing_axis: int = -1,
+    multiplying_axis: int = 0,
+) -> Generator:
+    for w in start:
+        slc = slices.copy()
+        slc[windowing_axis] = w
+        window = jax.lax.dynamic_slice(tensor, tuple(slc), sizes)
+        yield window
+
+
+def _slice_fn_new_ax(
+    tensor: Tensor,
+    start: Sequence,
+    slices: Sequence,
+    sizes: Sequence,
+    windowing_axis: int = -1,
+    multiplying_axis: int = 0,
+) -> Generator:
+    for w in start:
+        slc = slices.copy()
+        slc[windowing_axis] = w
+        window = jax.lax.dynamic_slice(tensor, tuple(slc), sizes)
+        window = jnp.expand_dims(window, axis=multiplying_axis)
+        yield window
+
+
+def _sample_window_impl(
     tensor: Tensor,
     window_size: int,
+    slice_fn: Callable,
     select_fn: Callable,
     num_windows: int = 1,
     windowing_axis: int = -1,
@@ -65,18 +126,24 @@ def _sample_nonoverlapping_impl(
     if multiplying_axis < 0:
         multiplying_axis = tensor.ndim + multiplying_axis
     input_size = tensor.shape[windowing_axis]
-    unused_size = input_size - window_size * num_windows
 
-    intervals = distrax.Multinomial(
-        total_count=unused_size,
-        probs=jnp.ones(num_windows + 1) / (num_windows + 1)
-    ).sample(seed=key)
-    start = jnp.arange(num_windows + 1) * window_size + jnp.cumsum(intervals)
-    start = start[:-1]
+    start = select_fn(
+        input_size=input_size,
+        window_size=window_size,
+        num_windows=num_windows,
+        key=key,
+    )
     slices = [0] * tensor.ndim
     sizes = tuple(s if i != windowing_axis else window_size
                   for i, s in enumerate(tensor.shape))
-    windows = tuple(select_fn(start, slices, sizes))
+    windows = tuple(slice_fn(
+        tensor=tensor,
+        start=start,
+        slices=slices,
+        sizes=sizes,
+        windowing_axis=windowing_axis,
+        multiplying_axis=multiplying_axis,
+    ))
     return jnp.concatenate(windows, axis=multiplying_axis)
 
 
@@ -90,21 +157,11 @@ def sample_nonoverlapping_windows_existing_ax(
     *,
     key: 'jax.random.PRNGKey',
 ) -> Tensor:
-    def _select_windows(
-        start: Sequence,
-        slices: Sequence,
-        sizes: Sequence,
-    ) -> Generator:
-        for w in start:
-            slc = slices.copy()
-            slc[windowing_axis] = w
-            window = jax.lax.dynamic_slice(tensor, tuple(slc), sizes)
-            yield window
-
-    return _sample_nonoverlapping_impl(
+    return _sample_window_impl(
         tensor,
         window_size=window_size,
-        select_fn=_select_windows,
+        slice_fn=_slice_fn_existing_ax,
+        select_fn=_select_fn_no_overlap,
         num_windows=num_windows,
         windowing_axis=windowing_axis,
         multiplying_axis=multiplying_axis,
@@ -122,18 +179,11 @@ def sample_nonoverlapping_windows_new_ax(
     *,
     key: 'jax.random.PRNGKey',
 ) -> Tensor:
-    def _select_windows(start, slices, sizes):
-        for w in start:
-            slc = slices.copy()
-            slc[windowing_axis] = w
-            window = jax.lax.dynamic_slice(tensor, tuple(slc), sizes)
-            window = jnp.expand_dims(window, axis=multiplying_axis)
-            yield window
-
-    return _sample_nonoverlapping_impl(
+    return _sample_window_impl(
         tensor,
         window_size=window_size,
-        select_fn=_select_windows,
+        slice_fn=_slice_fn_new_ax,
+        select_fn=_select_fn_no_overlap,
         num_windows=num_windows,
         windowing_axis=windowing_axis,
         multiplying_axis=multiplying_axis,
@@ -141,15 +191,62 @@ def sample_nonoverlapping_windows_new_ax(
     )
 
 
-def sample_nonoverlapping_windows(
+@document_window_fn
+def sample_overlapping_windows_existing_ax(
+    tensor: Tensor,
+    window_size: int,
+    num_windows: int = 1,
+    windowing_axis: int = -1,
+    multiplying_axis: int = 0,
+    *,
+    key: 'jax.random.PRNGKey',
+) -> Tensor:
+    return _sample_window_impl(
+        tensor,
+        window_size=window_size,
+        slice_fn=_slice_fn_existing_ax,
+        select_fn=_select_fn_allow_overlap,
+        num_windows=num_windows,
+        windowing_axis=windowing_axis,
+        multiplying_axis=multiplying_axis,
+        key=key,
+    )
+
+
+@document_window_fn
+def sample_overlapping_windows_new_ax(
+    tensor: Tensor,
+    window_size: int,
+    num_windows: int = 1,
+    windowing_axis: int = -1,
+    multiplying_axis: int = 0,
+    *,
+    key: 'jax.random.PRNGKey',
+) -> Tensor:
+    return _sample_window_impl(
+        tensor,
+        window_size=window_size,
+        slice_fn=_slice_fn_new_ax,
+        select_fn=_select_fn_allow_overlap,
+        num_windows=num_windows,
+        windowing_axis=windowing_axis,
+        multiplying_axis=multiplying_axis,
+        key=key,
+    )
+
+
+def sample_windows(
+    allow_overlap: bool = False,
     create_new_axis: bool = False,
 ) -> Callable:
     """
-    Sample non-overlapping windows from a tensor.
+    Sample windows from a tensor.
 
     Parameters
     ----------
-    create_new_axis : bool, optional
+    allow_overlap : bool (default: False)
+        Whether to allow windows to overlap.
+    create_new_axis : bool (default: False)
         Whether to create a new axis for the windows. Default: False.
         If this is True, the new axis will be inserted at
         ``multiplying_axis``. Otherwise, the windows will be multiplied along
@@ -158,8 +255,12 @@ def sample_nonoverlapping_windows(
     Returns
     -------
     callable
-        A function that samples non-overlapping windows from a tensor.
+        A function that samples windows from a tensor.
     """
     if create_new_axis:
+        if allow_overlap:
+            return sample_overlapping_windows_new_ax
         return sample_nonoverlapping_windows_new_ax
+    if allow_overlap:
+        return sample_overlapping_windows_existing_ax
     return sample_nonoverlapping_windows_existing_ax
