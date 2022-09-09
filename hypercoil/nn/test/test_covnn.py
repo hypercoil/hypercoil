fcea9b4216fa603e573130dab62b86e7fb23fcb0
Rastko Ciric <rastko@stanford.edu>
2022-09-08 18:45:22 -0700

    (TEST) JAX recore: covariance module family

--- hypercoil/nn/test/test_covnn.py
+++ hypercoil/nn/test/test_covnn.py
@@ -6,18 +6,25 @@ Unit tests for covariance modules
 """
 import pytest
 import numpy as np
-import torch, hypercoil
+import jax
+import distrax
+import equinox as eqx
+from hypercoil.engine.noise import (
+    StochasticParameter,
+    Diagonal, Symmetric, MatrixExponential,
+    TensorIIDAddStochasticTransform,
+    TensorIIDMulStochasticTransform,
+    ScalarIIDAddStochasticTransform,
+    ScalarIIDMulStochasticTransform,
+)
+from hypercoil.functional.cov import (
+    corr, pcorr, pairedcorr
+)
+from hypercoil.functional.utils import apply_mask
 from hypercoil.nn import (
     UnaryCovariance, UnaryCovarianceTW, UnaryCovarianceUW,
     BinaryCovariance, BinaryCovarianceTW, BinaryCovarianceUW
 )
-from hypercoil.init.base import DistributionInitialiser
-from hypercoil.init.laplace import LaplaceInit
-from hypercoil.engine.noise import (
-    DiagonalNoiseSource, DiagonalDropoutSource, BandDropoutSource
-)
-from hypercoil.functional.utils import apply_mask
-from hypercoil.init.domain import Logit
 
 
 class TestCovNN:
@@ -26,220 +33,187 @@ class TestCovNN:
     def setup_class(self):
         import os
         os.environ['KMP_DUPLICATE_LIB_OK']='True'
-        self.approx = lambda x, y: np.allclose(x.detach(), y, atol=1e-5)
-        self.approxh = lambda x, y: np.allclose(x.detach(), y, atol=1e-3)
+        self.approx = lambda x, y: np.allclose(x, y, atol=1e-5)
+        self.approxh = lambda x, y: np.allclose(x, y, atol=1e-3)
+
+        key = jax.random.PRNGKey(0)
+        xkey, ykey, srckey = jax.random.split(key, 3)
 
         self.n = 100
-        self.X = torch.rand(4, 13, self.n)
-        self.Y = torch.rand(4, 7, self.n)
-        self.dns = DiagonalNoiseSource()
-        self.dds = DiagonalDropoutSource()
-        self.bds = BandDropoutSource()
+        self.X = jax.random.uniform(key=xkey, shape=(4, 13, self.n))
+        self.Y = jax.random.uniform(key=ykey, shape=(4, 7, self.n))
 
-        if torch.cuda.is_available():
-            self.XC = self.X.clone().cuda().half()
-            self.YC = self.Y.clone().cuda().half()
+        inner_distr = distrax.Normal(3, 1)
+        distr = Diagonal(
+            src_distribution=inner_distr,
+            multiplicity=self.n,
+        )
+        self.nns = ScalarIIDAddStochasticTransform(
+            distribution=inner_distr, key=srckey)
+        self.dns = TensorIIDAddStochasticTransform(
+            distribution=distr,
+            event_axes=(-1, -2),
+            key=srckey
+        )
+
+        inner_distr = distrax.Bernoulli(probs=0.3)
+        self.bds = ScalarIIDMulStochasticTransform(
+            distribution=inner_distr, key=srckey)
+        distr = Diagonal(
+            src_distribution=inner_distr,
+            multiplicity=self.n,
+        )
+        self.dds = TensorIIDMulStochasticTransform(
+            distribution=distr,
+            event_axes=(-1, -2),
+            key=srckey
+        )
+
+        distr = Symmetric(
+            src_distribution=inner_distr,
+            multiplicity=self.n
+        )
+        distr = MatrixExponential(distr)
+        self.psds = TensorIIDMulStochasticTransform(
+            distribution=distr,
+            event_axes=(-1, -2),
+            key=srckey
+        )
 
     def test_cov_uuw(self):
-        cov = UnaryCovarianceUW(self.n, estimator=hypercoil.functional.corr)
-        out = cov(self.X)
+        key = jax.random.PRNGKey(0)
+        mkey, skey = jax.random.split(key, 2)
+        cov = UnaryCovarianceUW(
+            estimator=corr,
+            dim=self.n,
+            key=mkey,
+        )
+        out = eqx.filter_jit(cov)(self.X)
         ref = np.stack([np.corrcoef(x) for x in self.X])
         assert self.approx(out, ref)
         cov = UnaryCovarianceUW(
-            self.n, estimator=hypercoil.functional.pcorr,
-            out_channels=7, noise=self.dns) #, dropout=dds)
-        cov(self.X)
+            estimator=pcorr,
+            dim=self.n,
+            out_channels=7,
+            key=mkey,
+        )
+        weight = self.dns.sample(
+            key=skey,
+            shape=(cov.out_channels, cov.dim, cov.dim)
+        )
+        eqx.filter_jit(cov)(self.X, weight=weight)
 
     def test_cov_uuw_masked(self):
-        cov = UnaryCovarianceUW(self.n, estimator=hypercoil.functional.corr)
-        mask = torch.randn(self.n) > 0
-        out = cov(self.X, mask=mask)
-        ref = np.stack([np.corrcoef(apply_mask(x, mask, -1)) for x in self.X])
+        key = jax.random.PRNGKey(0)
+        mkey, skey = jax.random.split(key, 2)
+        cov = UnaryCovarianceUW(
+            estimator=corr,
+            dim=self.n,
+            key=mkey,
+        )
+        mask = jax.random.bernoulli(key=skey, shape=(self.n,))
+        out = eqx.filter_jit(cov)(self.X, mask=mask)
+        ref = np.stack([
+            np.corrcoef(apply_mask(x, mask, -1))
+            for x in self.X
+        ])
         assert self.approx(out, ref)
-        mask = torch.randn(4, 1, self.n) > 0
-        out = cov(self.X, mask=mask)
-        ref = np.stack([np.corrcoef(apply_mask(x, m, -1))
-                        for x, m in zip(self.X, mask)])
+        mask = jax.random.bernoulli(key=skey, shape=(4, 1, self.n))
+        out = eqx.filter_jit(cov)(self.X, mask=mask)
+        ref = np.stack([
+            np.corrcoef(apply_mask(x, m, -1))
+            for x, m in zip(self.X, mask)
+        ])
         assert self.approx(out, ref)
 
     def test_cov_utw(self):
-        cov = UnaryCovarianceTW(self.n, estimator=hypercoil.functional.corr)
-        out = cov(self.X)
+        cov = UnaryCovarianceTW(
+            estimator=corr,
+            dim=self.n,
+        )
+        out = eqx.filter_jit(cov)(self.X)
         ref = np.stack([np.corrcoef(x) for x in self.X])
         assert self.approx(out, ref)
-        init = LaplaceInit(
-            loc=(0, 0), excl_axis=[1], domain=Logit(4)
-        )
+
         cov = UnaryCovarianceTW(
-            self.n, estimator=hypercoil.functional.pcorr, max_lag=3,
-            out_channels=7, noise=self.dns, dropout=self.bds, init=init)
-        cov(self.X)
-        assert cov.prepreweight_c.size() == torch.Size([4, 7])
+            estimator=pcorr,
+            dim=self.n,
+            max_lag=3,
+            out_channels=7,
+        )
+        assert cov.weight_row.shape == (7, 4)
         assert cov.weight[5, 15, 17] == cov.weight[5, 94, 96]
-        assert cov.postweight[3, 13, 17] == 0
+        assert cov.weight[3, 13, 17] == 0
+        eqx.filter_jit(cov)(self.X)
+        cov = StochasticParameter.wrap(
+            cov, param_name='weight_col;weight_row', transform=self.nns
+        )
+        cov = StochasticParameter.wrap(
+            cov, param_name='weight_col;weight_row', transform=self.bds
+        )
+        eqx.filter_jit(cov)(self.X)
 
     def test_cov_uw(self):
-        cov = UnaryCovariance(self.n, estimator=hypercoil.functional.corr)
-        out = cov(self.X)
+        cov = UnaryCovariance(
+            estimator=corr,
+            dim=self.n,
+        )
+        out = eqx.filter_jit(cov)(self.X)
         ref = np.stack([np.corrcoef(x) for x in self.X])
         assert self.approx(out, ref)
 
-    def test_cov_uw_lag(self):
-        #TODO: this only makes sure nothing crashes in the forward pass
-        # not a test for correctness
         cov = UnaryCovariance(
-            self.n,
-            estimator=hypercoil.functional.corr,
-            max_lag=2
+            estimator=pcorr,
+            dim=self.n,
+            min_lag=1,
+            max_lag=3,
+            out_channels=7,
         )
-        out = cov(self.X)
+        eqx.filter_jit(cov)(self.X)
 
-    def test_cov_uw_domain(self):
-        #TODO: this only makes sure nothing crashes in the forward pass
-        # not a test for correctness
-        init = DistributionInitialiser(
-            distr=torch.distributions.Normal(0.5, 0.02),
-            domain=Logit()
-        )
         cov = UnaryCovariance(
-            self.n,
-            estimator=hypercoil.functional.cov,
-            init=init
+            estimator=corr,
+            dim=self.n,
+            min_lag=None,
+            max_lag=None,
         )
-        out = cov(self.X)
+        eqx.filter_jit(cov)(self.X)
 
     def test_cov_buw(self):
         cov = BinaryCovarianceUW(
-            self.n, estimator=hypercoil.functional.pairedcorr)
-        out = cov(self.X, self.Y)
+            estimator=pairedcorr,
+            dim=self.n,
+        )
+        out = eqx.filter_jit(cov)(self.X, self.Y)
         ref = np.stack([np.corrcoef(x, y)[:13, -7:]
                         for x, y in zip(self.X, self.Y)])
         assert self.approx(out, ref)
 
     def test_cov_btw(self):
         cov = BinaryCovarianceTW(
-            self.n,
-            estimator=hypercoil.functional.pairedcorr
+            estimator=pairedcorr,
+            dim=self.n,
         )
-        out = cov(self.X, self.Y)
+        out = eqx.filter_jit(cov)(self.X, self.Y)
         ref = np.stack([np.corrcoef(x, y)[:13, -7:]
                         for x, y in zip(self.X, self.Y)])
         assert self.approx(out, ref)
+
+        cov = BinaryCovarianceTW(
+            estimator=pairedcorr,
+            dim=self.n,
+            max_lag=-1,
+            min_lag=-3,
+            out_channels=2,
+        )
+        eqx.filter_jit(cov)(self.X, self.Y)
 
     def test_cov_bw(self):
         cov = BinaryCovariance(
-            self.n,
-            estimator=hypercoil.functional.pairedcorr
+            estimator=pairedcorr,
+            dim=self.n,
         )
-        out = cov(self.X, self.Y)
+        out = eqx.filter_jit(cov)(self.X, self.Y)
         ref = np.stack([np.corrcoef(x, y)[:13, -7:]
                         for x, y in zip(self.X, self.Y)])
         assert self.approx(out, ref)
-
-    @pytest.mark.cuda
-    def test_cov_uuw_cuda(self):
-        cov = UnaryCovarianceUW(
-            self.n, estimator=hypercoil.functional.corr,
-            dtype=torch.half, device='cuda')
-        out = cov(self.XC)
-        ref = np.stack([np.corrcoef(x) for x in self.X])
-        assert self.approxh(out.cpu(), ref)
-        cov = UnaryCovarianceUW(
-            self.n, estimator=hypercoil.functional.pcorr,
-            out_channels=7, noise=self.dns, device='cuda')
-        cov(self.XC.clone().float())
-
-    @pytest.mark.cuda
-    def test_cov_utw_cuda(self):
-        cov = UnaryCovarianceTW(
-            self.n, estimator=hypercoil.functional.corr,
-            dtype=torch.half, device='cuda')
-        out = cov(self.XC)
-        ref = np.stack([np.corrcoef(x) for x in self.X])
-        assert self.approxh(out.cpu(), ref)
-        init = LaplaceInit(
-            loc=(0, 0), excl_axis=[1], domain=Logit(4)
-        )
-        cov = UnaryCovarianceTW(
-            self.n, estimator=hypercoil.functional.pcorr, max_lag=3,
-            out_channels=7, noise=self.dns, dropout=self.bds, init=init,
-            device='cuda')
-        cov(self.XC.clone().float())
-        assert cov.prepreweight_c.size() == torch.Size([4, 7])
-        assert cov.weight[5, 15, 17] == cov.weight[5, 94, 96]
-        assert cov.postweight[3, 13, 17] == 0
-
-    @pytest.mark.cuda
-    def test_cov_uw_cuda(self):
-        cov = UnaryCovariance(
-            self.n, estimator=hypercoil.functional.corr,
-            dtype=torch.half, device='cuda')
-        out = cov(self.XC)
-        ref = np.stack([np.corrcoef(x) for x in self.X])
-        assert self.approxh(out.cpu(), ref)
-
-    @pytest.mark.cuda
-    def test_cov_uw_lag_cuda(self):
-        #TODO: this only makes sure nothing crashes in the forward pass
-        # not a test for correctness
-        cov = UnaryCovariance(
-            self.n,
-            estimator=hypercoil.functional.corr,
-            max_lag=2,
-            dtype=torch.half,
-            device='cuda'
-        )
-        out = cov(self.XC)
-
-    @pytest.mark.cuda
-    def test_cov_uw_domain_cuda(self):
-        #TODO: this only makes sure nothing crashes in the forward pass
-        # not a test for correctness
-        init = DistributionInitialiser(
-            distr=torch.distributions.Normal(0.5, 0.02),
-            domain=Logit()
-        )
-        cov = UnaryCovariance(
-            self.n,
-            estimator=hypercoil.functional.cov,
-            init=init,
-            dtype=torch.half,
-            device='cuda'
-        )
-        out = cov(self.XC)
-
-    @pytest.mark.cuda
-    def test_cov_buw_cuda(self):
-        cov = BinaryCovarianceUW(
-            self.n, estimator=hypercoil.functional.pairedcorr,
-            dtype=torch.half, device='cuda')
-        out = cov(self.XC, self.YC)
-        ref = np.stack([np.corrcoef(x, y)[:13, -7:]
-                        for x, y in zip(self.X, self.Y)])
-        assert self.approxh(out.cpu(), ref)
-
-    @pytest.mark.cuda
-    def test_cov_btw_cuda(self):
-        cov = BinaryCovarianceTW(
-            self.n,
-            estimator=hypercoil.functional.pairedcorr,
-            dtype=torch.half,
-            device='cuda'
-        )
-        out = cov(self.XC, self.YC)
-        ref = np.stack([np.corrcoef(x, y)[:13, -7:]
-                        for x, y in zip(self.X, self.Y)])
-        assert self.approxh(out.cpu(), ref)
-
-    @pytest.mark.cuda
-    def test_cov_bw_cuda(self):
-        cov = BinaryCovariance(
-            self.n,
-            estimator=hypercoil.functional.pairedcorr,
-            dtype=torch.half,
-            device='cuda'
-        )
-        out = cov(self.XC, self.YC)
-        ref = np.stack([np.corrcoef(x, y)[:13, -7:]
-                        for x, y in zip(self.X, self.Y)])
-        assert self.approxh(out.cpu(), ref)
