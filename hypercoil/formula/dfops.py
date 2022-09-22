0b4ae7ead1735df94a503e35ace54232559333a3
Rastko Ciric <rastko@stanford.edu>
2022-09-22 11:08:42 -0700

    (REF)(TEST) (mostly) complete confound model selection grammar

--- hypercoil/formula/dfops.py
+++ hypercoil/formula/dfops.py
@@ -6,6 +6,8 @@ DataFrames
 ~~~~~~~~~~
 Transformations and grammar for DataFrame operations.
 """
+import re
+import numpy as np
 import pandas as pd
 import equinox as eqx
 from dataclasses import field
@@ -27,6 +29,9 @@ def confound_formula_shorthand():
         'rps': 'trans_x + trans_y + trans_z + rot_x + rot_y + rot_z',
         'fd': 'framewise_displacement',
         'dv': 'std_dvars',
+        'acc': 'a_comp_cor',
+        'wcc': 'w_comp_cor',
+        'ccc': 'c_comp_cor',
     }
 
 
@@ -51,11 +56,23 @@ class ConfoundFormulaGrammar(Grammar):
             UnionNode(),
             IntersectionNode(),
             NegationNode(),
+            ScatterNode(),
+            FirstNNode(),
+            CumulativeVarianceNode(),
+            MetadataFilterNode(), # This has got to be last because other
+                                  # nodes might have a metadata filter as a
+                                  # substring of their literalisation.
         )
     )
     whitespace: bool = False
     shorthand: Dict[str, str] = field(
         default_factory = confound_formula_shorthand
+    )
+    default_interpreter: Optional[LeafInterpreter] = field(
+        default_factory = lambda: ColumnSelectInterpreter()
+    )
+    default_root_transform: Optional[TransformPrimitive] = field(
+        default_factory = lambda: DeduplicateRootNode()
     )
 
 
@@ -64,9 +81,31 @@ class ColumnSelectInterpreter(LeafInterpreter):
         def select_column(
             df: pd.DataFrame,
             meta: Optional[Any] = None,
-        ) -> pd.DataFrame:
+        ) -> Tuple[pd.DataFrame, Any]:
             return df[[leaf]], meta
         return select_column
+
+
+class DeduplicateRootNode(TransformPrimitive):
+    min_arity: int = 1
+    max_arity: int = 1
+    priority: float = float('inf')
+    associative: bool = False
+    commutative: bool = False
+    literals: Sequence[Literalisation] = ()
+
+    def __call__(self, *pparams, **params) -> Callable:
+        f = pparams[0]
+
+        def compiled(
+            arg: pd.DataFrame,
+            meta: Optional[Any] = None,
+        ) -> Tuple[pd.DataFrame, Any]:
+            arg, meta = f(arg, meta)
+            arg = arg.loc[:, ~arg.columns.duplicated()].copy()
+            return arg, meta
+
+        return compiled
 
 
 #------------------------------ Concatenation ------------------------------#
@@ -174,13 +213,16 @@ class BackwardDifferenceNode(TransformPrimitive):
             meta: Optional[Any] = None,
         ) -> Tuple[pd.DataFrame, Any]:
             arg, meta = f(arg, meta)
-            acc = [arg]
-            for o in range(max(params['order'])):
+            orig = arg
+            acc = []
+            for o in range(1, max(params['order']) + 1):
                 arg = arg.diff()
                 if o in params['order']:
                     arg.columns = [f'{c}_derivative{o}' if o != 0 else c
                                    for c in arg.columns]
                     acc.append(arg)
+            if 0 in params['order']:
+                acc = [orig] + acc
             return pd.concat(acc, axis=1), meta
 
         return backward_difference
@@ -266,7 +308,7 @@ class IndicatorPrefixLiteralisation(Literalisation):
     )
 
     def parse_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
-        threshold = float(params['order'])
+        threshold = float(params['threshold'])
         compare = params['compare']
         if compare == '>':
             params['indicator'] = lambda x: x > threshold
@@ -415,9 +457,9 @@ class NegationNode(TransformPrimitive):
             meta: Optional[Any] = None,
         ) -> Tuple[pd.DataFrame, Any]:
             arg, meta = f(arg, meta)
-            values = ~values
+            values = ~arg.values
             column = [f'negation_{c}' for c in arg.columns]
-            df = pd.DataFrame({column: values})
+            df = pd.DataFrame({c: v for c, v in zip(column, values.T)})
             return df, meta
 
         return negation
@@ -463,6 +505,15 @@ class LhsRhsNode(TransformPrimitive):
         return lhs_rhs
 
 
+#------------------------------ Dummy Coding -------------------------------#
+
+
+#------------------------------ Conditioning -------------------------------#
+
+
+#------------------------------- Expression --------------------------------#
+
+
 #------------------------------- Interaction -------------------------------#
 
 
@@ -505,16 +556,313 @@ class InteractionNode(TransformPrimitive):
 #--------------------------------- First N ---------------------------------#
 
 
+#TODO: after adding support for logical composition in metadata filters,
+#      this should use a metadata filter instead of hard-coding to match
+#      the ``Mask`` field. This might actually have some application outside
+#      of confound regression, unlike CumulVar, so changing this is probably
+#      a good idea here.
+class FirstNLeafLiteralisation(Literalisation):
+    affix : str = 'leaf'
+    regex : str = (
+        r'n_\{\{(?P<count>[0-9]+);(?P<variable>[a-zA-Z0-9_]+)'
+        r'(;)?(Mask=(?P<mask>[A-Za-z,]*))?\}\}'
+    )
+
+    def parse_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
+        shorthand = confound_formula_shorthand()
+        params['count'] = int(params['count'])
+        for k, v in shorthand.items():
+            params['variable'] = re.sub(k, v, params['variable'])
+        return params
+
+
+class FirstNNode(TransformPrimitive):
+    min_arity: int = 0
+    max_arity: int = 0
+    priority: int = 0
+    literals: Sequence[Literalisation] = (
+        FirstNLeafLiteralisation(),
+    )
+    associative: bool = False
+    commutative: bool = False
+
+    def ascend(self, *pparams, **params) -> Callable:
+
+        def first_n(
+            df: 'pd.DataFrame',
+            meta: Optional[Dict] = None
+        ) -> Tuple[pd.DataFrame, Any]:
+            n = params['count']
+            pattern = re.compile(r'^{}_[0-9]+'.format(params['variable']))
+            matches = match_metadata(pattern, meta)
+            matches.sort(key=numbered_string)
+
+            if params.get('mask', None) is None:
+                matches = matches[:n]
+            else:
+                matches_mask = []
+                masks = params['mask'].split(',')
+                for mask in masks:
+                    filt = [m for m in matches
+                            if meta[m].get('Mask') == mask]
+                    matches_mask += filt[:n]
+                matches_mask.sort(key=numbered_string)
+                matches = matches_mask
+
+            columns = tuple(
+                successive_pad_search(df, m) for m in matches
+            )
+            return pd.concat(
+                columns,
+                axis=1
+            ), meta
+
+        return first_n
+
+
 #--------------------------- Cumulative Variance ---------------------------#
 
 
-#---------------------------- Noise Components -----------------------------#
+class CumulativeVarianceLeafLiteralisation(Literalisation):
+    affix : str = 'leaf'
+    regex : str = (
+        r'v_\{\{(?P<variance>[0-9\.]+);(?P<variable>[a-zA-Z0-9_]+)'
+        r'(;)?(Mask=(?P<mask>[A-Za-z,]*))?\}\}'
+    )
+
+    def parse_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
+        shorthand = confound_formula_shorthand()
+        params['variance'] = float(params['variance']) / 100
+        for k, v in shorthand.items():
+            params['variable'] = re.sub(k, v, params['variable'])
+        return params
 
 
-#------------------------------ Dummy Coding -------------------------------#
+class CumulativeVarianceNode(TransformPrimitive):
+    min_arity: int = 0
+    max_arity: int = 0
+    priority: int = 0
+    literals: Sequence[Literalisation] = (
+        CumulativeVarianceLeafLiteralisation(),
+    )
+    associative: bool = False
+    commutative: bool = False
+
+    def ascend(self, *pparams, **params) -> Callable:
+
+        def cumulative_variance(
+            df: 'pd.DataFrame',
+            meta: Optional[Dict] = None
+        ) -> Tuple[pd.DataFrame, Any]:
+            v = params['variance']
+            pattern = re.compile(r'^{}_[0-9]+'.format(params['variable']))
+            matches = match_metadata(pattern, meta)
+
+            if params.get('mask', None) is not None:
+                matches_mask = []
+                masks = params['mask'].split(',')
+                for mask in masks:
+                    filt = [m for m in matches if meta[m].get('Mask') == mask]
+                    matches_mask += filt
+                matches = matches_mask
+
+            matches.sort(key=numbered_string)
+            out = []
+            done = False
+            for m in matches:
+                item = meta[m]
+                if done:
+                    done = item['CumulativeVarianceExplained'] > v
+                    if done: continue
+                done = item['CumulativeVarianceExplained'] > v
+                out += [m]
+
+            columns = tuple(successive_pad_search(df, o) for o in out)
+            return pd.concat(columns, axis=1), meta
+
+        return cumulative_variance
 
 
-#------------------------------- Deduplicate -------------------------------#
+#----------------------------- Metadata Filter -----------------------------#
 
 
-#------------------------------- FC-Specific -------------------------------#
+#TODO: add simple logical composition (AND, OR, NOT) to filters.
+#      Currently everything is AND, which isn't always what we want.
+class MetadataFilterLeafLiteralisation(Literalisation):
+    affix : str = 'leaf'
+    regex: str = (
+        r'\{\{(?P<variable>[a-zA-Z0-9_]+)'
+        r'(;)?(?P<filters>[a-zA-Z0-9_=;]+)\}\}'
+    )
+
+    def parse_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
+        params['filters'] = [
+            s.split('=')
+            for s in params['filters'].split(';')
+        ]
+        for filter in params['filters']:
+            try:
+                filter[1] = float(filter[1])
+            except ValueError:
+                try:
+                    filter[1] = bool(filter[1])
+                except ValueError:
+                    pass
+        params['filters'] = tuple(tuple(f) for f in params['filters'])
+        return params
+
+
+class MetadataFilterNode(TransformPrimitive):
+    min_arity: int = 0
+    max_arity: int = 0
+    priority: int = 0
+    literals: Sequence[Literalisation] = (
+        MetadataFilterLeafLiteralisation(),
+    )
+    associative: bool = False
+    commutative: bool = False
+
+    def ascend(self, *pparams, **params) -> Callable:
+
+        def filter_by_metadata(
+            df: 'pd.DataFrame',
+            meta: Optional[Dict] = None
+        ) -> Tuple[pd.DataFrame, Any]:
+            pattern = re.compile(r'^{}.*'.format(params['variable']))
+            matches = match_metadata(pattern, meta)
+
+            out = []
+            for m in matches:
+                matched = True
+                item = meta[m]
+                for k, v in params['filters']:
+                    if item.get(k) != v:
+                        matched = False
+                        break
+                if matched: out += [m]
+
+            columns = tuple(successive_pad_search(df, o) for o in out)
+            return pd.concat(columns, axis=1), meta
+
+        return filter_by_metadata
+
+
+#----------------------------- Scatter Spikes ------------------------------#
+
+
+class ScatterPrefixLiteralisation(Literalisation):
+    affix : str = 'prefix'
+    regex: str = r'\[SCATTER\]'
+
+    def parse_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
+        return params
+
+
+class ScatterNode(TransformPrimitive):
+    min_arity: int = 1
+    max_arity: int = 1
+    priority: int = 1
+    literals: Sequence[Literalisation] = (
+        ScatterPrefixLiteralisation(),
+    )
+    associative: bool = False
+    commutative: bool = False
+
+    def ascend(self, *pparams, **params) -> Callable:
+
+        f = pparams[0]
+
+        def scatter_columns(
+            arg: Any,
+            meta: Optional[Dict] = None
+        ) -> Tuple[pd.DataFrame, Any]:
+
+            arg, meta = f(arg, meta)
+            scattered = []
+            for col in arg.columns:
+                data = arg[[col]].values
+                nz = data.nonzero()
+                names = [f'{col}_spike{i}' for i in nz[0]]
+                cols = np.zeros((data.shape[0], len(nz)))
+                cols[nz[0], range(len(nz))] = data[nz]
+                scattered += [pd.DataFrame(cols, columns=names)]
+            return pd.concat(scattered, axis=1), meta
+
+        return scatter_columns
+
+
+#---------------------------- Utility functions ----------------------------#
+
+
+def numbered_string(s):
+    """
+    Convert a string whose final characters are numeric into a tuple
+    containing all characters except the final numeric ones in the first field
+    and the numeric characters, cast to an int, in the second field. This
+    permits comparison of strings of this form without regard to the number of
+    zeros in the final numeric part. It also permits strings of this form to
+    be sorted in numeric order.
+
+    Parameters
+    ----------
+    s : string
+        A string whose first characters are non-numeric and whose final
+        characters are numeric.
+
+    Returns
+    -------
+    tuple(str, int)
+        Input string split into its initial non-numeric substring and its
+        final numeric substring, cast to an int.
+    """
+    num = int(re.search('(?P<num>[0-9]+$)', s).groupdict()['num'])
+    string = re.sub('[0-9]+$', '', s)
+    return (string, num)
+
+
+def match_metadata(pattern, metadata):
+    """
+    Find all dictionary keys that match a pattern.
+
+    Parameters
+    ----------
+    pattern : compiled regular expression
+        A compiled regular expression (obtained via re.compile) that
+        represents the pattern to be matched in the metadata keys.
+    metadata : dict
+        Dictionary whose keys are to be searched for the specified pattern.
+    """
+    return list(filter(pattern.match, metadata.keys()))
+
+
+def successive_pad_search(df, key, pad=0, k=5):
+    """
+    Find the column of a DataFrame that matches a query, ignoring potential
+    zero-padding.
+
+    This function is an unfortunate artefact of an inconsistency in the way
+    that an earlier version of fMRIPrep named metadata entries and data
+    columns. The metadata entries were named without zero-padding, while the
+    data columns were named with zero-padding. This function allows the
+    metadata entries to be matched to the data columns without regard to
+    zero-padding.
+
+    Parameters
+    ----------
+    df : DataFrame
+        DataFrame whose columns are to be searched for the specified key.
+    key : str
+        Key to search for in the DataFrame.
+    pad : int (default 0)
+        Number whose padding to ignore in the search
+    k : int
+        Maximum number of pads to try before a KeyError is raised.
+    """
+    for i in range(k):
+        try:
+            return df[[key]]
+        except KeyError:
+            p = '{:' + f'{pad}{i + 1}' + '}'
+            st, nu = numbered_string(key)
+            key = f'{st}' + p.format(nu)
+    raise KeyError
---
+++ hypercoil/formula/test/test_dfops.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Unit tests for dataframe formula grammar
+"""
+import json
+import pandas as pd
+from pkg_resources import resource_filename as pkgrf
+from hypercoil.formula.dfops import (
+    ConfoundFormulaGrammar
+)
+
+
+def path_from_examples(fname):
+    return pkgrf('hypercoil', f'examples/data/{fname}')
+
+
+#TODO: remove this after we upstream the ``data`` submodule.
+def read_json(path):
+    """
+    Load JSON-formatted metadata into a python dictionary.
+
+    Parameters
+    ----------
+    path : str
+        Path to the JSON-formatted metadata file.
+
+    Returns
+    -------
+    metadata : dict
+        Python dictionary containing all metadata in the JSON file.
+    """
+    with open(path) as file:
+        metadata = json.load(file)
+    return metadata
+
+
+class TestModelSpec:
+    filenames = {
+        'confdata': 'desc-confounds_timeseries.tsv',
+        'confmeta': 'desc-confounds_timeseries.json'
+    }
+    confdata = path_from_examples(filenames['confdata'])
+    confmeta = path_from_examples(filenames['confmeta'])
+    df = pd.read_csv(confdata, sep='\t')
+    metadata = read_json(confmeta)
+
+    def spec_base(
+        self, model_formula, shape
+    ):
+        f = ConfoundFormulaGrammar().compile(model_formula)
+        out, meta = f(self.df, self.metadata)
+        assert(out.shape[1] == shape)
+        return out, meta
+
+    def test_36ev_spec(self):
+        out, _ = self.spec_base(
+            model_formula='dd1((rps + wm + csf + gsr)^^2)',
+            shape=36)
+        out[['trans_y', 'trans_y_power2', 'trans_y_derivative1']]
+        assert('trans_y_power2_derivative1' in out.columns)
+
+    def test_parens_spec(self):
+        self.spec_base(
+            model_formula='(rps)^^2 + dd1(rps)',
+            shape=18
+        )
+
+    def test_tmask_spec(self):
+        out, _ = self.spec_base(
+            model_formula='[AND](1_[<0.5](fd) + 1_[<1.5](dv))',
+            shape=1
+        )
+        assert out.values.sum() == (len(out) - 3)
+        out, _ = self.spec_base(
+            model_formula='[NOT]([OR](1_[>0.5](fd) + 1_[>1.5](dv)))',
+            shape=1
+        )
+        # Note the inconsistency because nan is always false at comparison
+        assert out.values.sum() == (len(out) - 2)
+
+    def test_spikes_spec(self):
+        out, _ = self.spec_base(
+            model_formula='[SCATTER]([OR](1_[>0.5](fd) + 1_[>1.5](dv)))',
+            shape=2
+        )
+        assert (out.values.sum(0) == 1).all()
+
+    def test_accn_spec(self):
+        out, _ = self.spec_base(
+            model_formula='d0-1(rps) + n_{{5; acc; Mask=CSF,WM}}',
+            shape=22)
+        assert('a_comp_cor_30' not in out.columns)
+        assert('a_comp_cor_02' in out.columns)
+
+    def test_acc_spec(self):
+        out, _ = self.spec_base(
+            model_formula='d1(rps) + v_{{29.9; acc; Mask=CSF,WM}}',
+            shape=23)
+        assert('trans_y' not in out.columns)
+        assert('a_comp_cor_02' in out.columns)
+
+    def test_aroma_spec(self):
+        out, _ = self.spec_base(
+            model_formula='wm + csf + {{aroma; MotionNoise=True}}',
+            shape=41)
+        assert('aroma_motion_57' in out.columns)
+        assert('aroma_motion_38' not in out.columns)
