4abddd8666fd9fe905a3ac0a843242285c35dd3e
Rastko Ciric <rastko@stanford.edu>
2022-10-02 10:58:45 -0700

    (MAINT) stash all local changes for repo migration

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""Differentiable programming for multiverse optimisation
+"""Differentiable programming for functional brain mapping
 
 This package intends to implement atoms and compositions to support writing
 differentiable programs for neuroimaging analysis. With numerous analytic
